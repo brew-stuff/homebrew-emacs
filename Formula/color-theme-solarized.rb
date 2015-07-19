317851e8af2650b28bb649b54e3617b822c2d529
Alex Dunn <dunn.alex@gmail.com>
2015-07-18 21:05:37 -0700

    color-theme-solarized: cleanup

--- Formula/color-theme-solarized.rb
+++ Formula/color-theme-solarized.rb
@@ -1,10 +1,29 @@
-require 'formula'
-
 class ColorThemeSolarized < Formula
-  homepage 'https://github.com/sellout/emacs-color-theme-solarized'
-  head 'https://github.com/sellout/emacs-color-theme-solarized.git'
+  desc "Ethan Schoonover's Solarized color scheme"
+  homepage "https://github.com/sellout/emacs-color-theme-solarized"
+  head "https://github.com/sellout/emacs-color-theme-solarized.git"
 
   def install
-    (share+'emacs/themes/solarized').install Dir['*.el']
+    (share/"emacs/site-lisp/solarized").install Dir["*.el"]
+    doc.install "README.md"
   end
+
+  def caveats; <<-EOS.undent
+    Add the following lines to your init file:
+
+      (add-to-list 'custom-theme-load-path "#{HOMEBREW_PREFIX}/share/emacs/site-lisp/solarized")
+      (load-theme 'solarized t)
+      (setq solarized-termcolors 256)
+    EOS
+  end
+
+  test do
+    (testpath/"test.el").write <<-EOS.undent
+      (add-to-list 'custom-theme-load-path "#{share}/emacs/site-lisp/solarized")
+      (load-theme 'solarized t)
+      (setq solarized-termcolors 256)
+      (add-to-list 'default-frame-alist '(background-mode . dark))
+      (print (minibuffer-prompt-width))
+    EOS
+    assert_equal "0", shell_output("emacs -batch -l #{testpath}/test.el").strip  end
 end
