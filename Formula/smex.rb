4fd876528320e8d72f5062a5cff84de52404d55d
Alex Dunn <dunn.alex@gmail.com>
2015-07-19 22:46:35 -0700

    smex 3.0 (new formula)

---
+++ Formula/smex.rb
@@ -0,0 +1,35 @@
+class Smex < Formula
+  desc "Adds Ido completions to Emacs' M-x"
+  homepage "https://github.com/nonsequitur/smex"
+  url "https://github.com/nonsequitur/smex/archive/3.0.tar.gz"
+  sha256 "96eb68ac3301d0986195a29ea03d4dad1a5f81647cb097a78de59c1cc6d78e58"
+  head "https://github.com/nonsequitur/smex.git"
+
+  depends_on :emacs => "24"
+
+  def install
+    (share/"emacs/site-lisp/smex").install "smex.el"
+    doc.install "README.markdown"
+  end
+
+  def caveats; <<-EOS.undent
+    Add the following to your init file:
+
+      (require 'smex)
+      (setq mac-option-modifier 'meta)
+      (global-set-key (kbd "M-x") 'smex)
+      (global-set-key (kbd "M-X") 'smex-major-mode-commands)
+      (global-set-key (kbd "C-x C-M") 'smex-major-mode-commands)
+    EOS
+  end
+
+  test do
+    (testpath/"test.el").write <<-EOS.undent
+      (add-to-list 'load-path "#{HOMEBREW_PREFIX}/share/emacs/site-lisp")
+      (require 'smex)
+      (smex-initialize)
+      (print (minibuffer-prompt-width))
+    EOS
+    assert_equal "0", shell_output("emacs -batch -l #{testpath}/test.el").strip
+  end
+end
