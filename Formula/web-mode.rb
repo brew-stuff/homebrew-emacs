2ff4f15f407d5bc67c8f2911223852bf9fc3375e
Alex Dunn <dunn.alex@gmail.com>
2015-07-19 19:52:50 -0700

    web-mode 11.2

--- Formula/web-mode.rb
+++ Formula/web-mode.rb
@@ -1,12 +1,24 @@
-require 'formula'
-
 class WebMode < Formula
-  homepage 'http://web-mode.org/'
-  url 'https://github.com/fxbois/web-mode/archive/v9.tar.gz'
-  sha1 '03777b95ce0564cc49d017a919e02c762bab67b4'
-  head 'https://github.com/fxbois/web-mode.git'
+  homepage "http://web-mode.org"
+  url "https://github.com/fxbois/web-mode/archive/v11.2.tar.gz"
+  sha256 "e9e91da7ee0be5f3b0aa018571f42c6963086dd2a446f2554f405a8175543b82"
+  head "https://github.com/fxbois/web-mode.git"
+
+  depends_on :emacs => "23"
 
   def install
-    (share+'emacs/site-lisp').install ['web-mode.el', 'wfs-mode.el']
+    chmod 0755, "run.sh"
+    system "./run.sh"
+    (share/"emacs/site-lisp").install ["web-mode.el"]
+    doc.install "README.md"
+  end
+
+  def caveats; <<-EOS.undent
+    Require web-mode then activate it where appropriate, e.g.:
+
+      (require 'web-mode)
+      (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+      (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+    EOS
   end
 end
