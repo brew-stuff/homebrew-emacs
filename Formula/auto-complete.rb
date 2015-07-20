b2f312c3e983b260edbba019c9578083f0ec2fa3
Alex Dunn <dunn.alex@gmail.com>
2015-07-19 21:20:56 -0700

    auto-complete 1.5.0 (new formula)

---
+++ Formula/auto-complete.rb
@@ -0,0 +1,20 @@
+class AutoComplete < Formula
+  desc "Autocompletion extension for Emacs"
+  homepage "http://auto-complete.org/"
+  url "https://github.com/auto-complete/auto-complete/archive/v1.5.0.tar.gz"
+  sha256 "a960848fcb94f438c6795070b3125c1a039bf11cc058dbd60e8668adb3cebe4c"
+  head "https://github.com/auto-complete/auto-complete.git"
+
+  depends_on :emacs => "24.1"
+  depends_on "cask"
+
+  def install
+    system "make", "test", "EMACS=#{which "emacs"}",
+                           "CASK=#{Formula["cask"].bin}/cask"
+
+    system "make", "byte-compile", "EMACS=#{which "emacs"}",
+                                   "CASK=#{Formula["cask"].bin}/cask"
+    (share/"emacs/site-lisp/auto-complete").install Dir["*.el"],
+                                                    Dir["*.elc"]
+  end
+end
