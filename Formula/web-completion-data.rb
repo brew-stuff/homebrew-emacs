529adece2aad28fe681377f6eb13d9786f74399f
Alex Dunn <dunn.alex@gmail.com>
2015-07-19 21:57:52 -0700

    web-completion-data 0.1 (new formula)

---
+++ Formula/web-completion-data.rb
@@ -0,0 +1,11 @@
+class WebCompletionData < Formula
+  desc "Completion data for ac-html and company-web"
+  homepage "https://github.com/osv/web-completion-data"
+  url "https://github.com/osv/web-completion-data/archive/v0.1.tar.gz"
+  sha256 "ae74070f2612542af24684279a8ca191da1eead2c5ba5e1bcf7153d6c42ee995"
+  head "https://github.com/osv/web-completion-data.git"
+
+  def install
+    (share/"emacs/site-lisp/web-completion-data").install Dir["*"]
+  end
+end
