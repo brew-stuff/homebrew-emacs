8a945a7c19f244b2a11bed6ed28c513d19f38ed6
Alex Dunn <dunn.alex@gmail.com>
2015-07-19 20:02:57 -0700

    php-mode 1.17.0 (new formula)

---
+++ Formula/php-mode.rb
@@ -0,0 +1,16 @@
+class PhpMode < Formula
+  desc "Major mode for editing PHP files"
+  homepage "https://github.com/ejmr/php-mode"
+  url "https://github.com/ejmr/php-mode/archive/v1.17.0.tar.gz"
+  sha256 "4393e452e7ade8b7479a3b942b985a9495f6a961b443308718756038d21c231d"
+  head "https://github.com/ejmr/php-mode.git"
+
+  depends_on :emacs => "24.1"
+
+  def install
+    system "make", "test", "EMACS=#{which "emacs"}"
+    (share/"emacs/site-lisp/php-mode").install Dir["*.el"],
+                                               Dir["*.elc"]
+    doc.install "README.md"
+  end
+end
