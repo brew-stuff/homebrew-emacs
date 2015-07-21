7616f55507887594718cc160f42fd8911bcbf478
Alex Dunn <dunn.alex@gmail.com>
2015-07-21 12:41:34 -0700

    wgrep (new formula; head-only)

---
+++ Formula/wgrep.rb
@@ -0,0 +1,26 @@
+class Wgrep < Formula
+  desc "Writeable grep buffers for Emacs"
+  homepage "https://github.com/mhayashi1120/Emacs-wgrep"
+  head "https://github.com/mhayashi1120/Emacs-wgrep.git"
+
+  depends_on :emacs => "24.1"
+
+  def install
+    system "make", "compile", "EMACS=#{which "emacs"}"
+    system "make", "check", "EMACS=#{which "emacs"}"
+
+    (share/"emacs/site-lisp/wgrep").install Dir["*.el"],
+                                            Dir["*.elc"]
+    doc.install "README.md"
+  end
+
+  test do
+    (testpath/"test.el").write <<-EOS.undent
+      (add-to-list 'load-path "#{HOMEBREW_PREFIX}/share/emacs/site-lisp")
+      (load "wgrep")
+      (wgrep-check-buffer)
+      (print (minibuffer-prompt-width))
+    EOS
+    assert_equal "0", shell_output("emacs -batch -l #{testpath}/test.el").strip
+  end
+end
