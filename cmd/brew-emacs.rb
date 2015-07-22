218a0c5ede5a4cd10a38a0d83ba328a11daf62b8
Alex Dunn <dunn.alex@gmail.com>
2015-07-22 11:49:02 -0700

    cmd/emacs: template for new formulae

---
+++ cmd/brew-emacs.rb
@@ -0,0 +1,47 @@
+raise FormulaUnspecifiedError if ARGV.empty?
+
+def template(name)
+  classname = name.split("-").collect(&:capitalize).join
+  <<-EOS.undent
+  require File.expand_path("../../Homebrew/emacs-formula", __FILE__)
+
+  class #{classname} < EmacsFormula
+    desc ""
+    homepage ""
+    url ""
+    sha256 ""
+    head ""
+
+    depends_on :emacs => "24.4"
+
+    def install
+      byte_compile "#{name}.el"
+      (share/"emacs/site-lisp/#{name}").install "#{name}.el",
+                                                "#{name}.elc"
+      doc.install "README.md"
+    end
+
+    def caveats; <<-EOS.undent
+      Add the following to your init file:
+
+      (require '#{name})
+    \EOS
+    end
+
+    test do
+      (testpath/"test.el").write <<-EOS.undent
+        (add-to-list 'load-path "\#{HOMEBREW_PREFIX}/share/emacs/site-lisp")
+        (load "#{name}")
+        (print (minibuffer-prompt-width))
+      \EOS
+      assert_equal "0", shell_output("emacs -batch -l \#{testpath}/test.el").strip
+    end
+  end
+  EOS
+end
+
+ARGV.each do |new|
+  formula_file = (HOMEBREW_LIBRARY/"Taps/dunn/homebrew-emacs/Formula/#{new}.rb")
+  formula_file.write template(new)
+  exec_editor(formula_file)
+end
