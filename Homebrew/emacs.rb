92a8ba2148899bfda91989d0d222f65c7520b701
Alex Dunn <dunn.alex@gmail.com>
2015-07-22 10:18:16 -0700

    Homebrew/emacs: O_O

--- Homebrew/emacs.rb
+++ Homebrew/emacs.rb
@@ -6,23 +6,23 @@ module Emacs
   end
 
   def self.compile(*args)
-    emacs_args = %w[emacs --batch -Q]
-
-    # FIXME: lol
-    formula_path = caller.first.gsub(/:.*/, "")
-    f = Formulary.factory(formula_path)
-    if f.deps.any?
-      f.recursive_dependencies do |_, dep|
-        Dir["#{dep.to_formula.opt_share}/emacs/site-lisp/**/*"].each do |x|
-          x = Pathname.new(x)
-          emacs_args << "--directory #{x}" if x.directory?
-        end
-      end
-    end
-    emacs_args << %w[-f batch-byte-compile]
-
     lisps = args.flatten
     lisps.each do |file|
+      emacs_args = %w[emacs --batch -Q]
+
+      # FIXME: lol
+      formula_path = caller.first.gsub(/:.*/, "")
+      f = Formulary.factory(formula_path)
+      if f.deps.any?
+        f.recursive_dependencies do |_, dep|
+          Dir["#{dep.to_formula.opt_share}/emacs/site-lisp/**/*"].each do |x|
+            x = Pathname.new(x)
+            emacs_args << "--directory #{x}" if x.directory?
+          end
+        end
+      end
+      emacs_args << %w[-f batch-byte-compile]
+
       ohai "Byte compiling #{file}"
       emacs_args << file
       # TODO: why doesn't `system "emacs", *emacs_args` work here
