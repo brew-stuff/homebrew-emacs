24e2c22db68747368998592d3bed31ab82c979f5
Alex Dunn <dunn.alex@gmail.com>
2015-07-21 23:06:51 -0700

    smex: compile

--- Formula/smex.rb
+++ Formula/smex.rb
@@ -1,3 +1,5 @@
+require File.expand_path("../../emacs", __FILE__)
+
 class Smex < Formula
   desc "Adds Ido completions to Emacs' M-x"
   homepage "https://github.com/nonsequitur/smex"
@@ -5,22 +7,23 @@ class Smex < Formula
   sha256 "96eb68ac3301d0986195a29ea03d4dad1a5f81647cb097a78de59c1cc6d78e58"
   head "https://github.com/nonsequitur/smex.git"
 
-  depends_on :emacs => "24"
+  depends_on :emacs => "24.1"
 
   def install
-    (share/"emacs/site-lisp/smex").install "smex.el"
+    Emacs.compile "smex.el"
+    (share/"emacs/site-lisp/smex").install "smex.el", "smex.elc"
     doc.install "README.markdown"
   end
 
   def caveats; <<-EOS.undent
     Add the following to your init file:
 
-      (require 'smex)
-      (setq mac-option-modifier 'meta)
-      (global-set-key (kbd "M-x") 'smex)
-      (global-set-key (kbd "M-X") 'smex-major-mode-commands)
-      (global-set-key (kbd "C-x C-M") 'smex-major-mode-commands)
-    EOS
+    (require 'smex)
+    (setq mac-option-modifier 'meta)
+    (global-set-key (kbd "M-x") 'smex)
+    (global-set-key (kbd "M-X") 'smex-major-mode-commands)
+    (global-set-key (kbd "C-x C-M") 'smex-major-mode-commands)
+  EOS
   end
 
   test do
