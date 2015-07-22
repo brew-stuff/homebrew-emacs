d6a0e415d97bbeb8f5c1c363f7d789395dc091d2
Alex Dunn <dunn.alex@gmail.com>
2015-07-21 17:51:22 -0700

    xml-rpc: add HEAD

--- Formula/xml-rpc.rb
+++ Formula/xml-rpc.rb
@@ -1,12 +1,11 @@
-require 'formula'
-
 class XmlRpc < Formula
-  homepage 'https://launchpad.net/xml-rpc-el'
-  url 'https://launchpad.net/xml-rpc-el/trunk/1.6.8/+download/xml-rpc.el', :using => :curl
-  version '1.6.8'
-  sha1 '5addce61674bd08c5649085185bb2d9a22d91875'
+  homepage "https://launchpad.net/xml-rpc-el"
+  url "https://launchpad.net/xml-rpc-el/trunk/1.6.8/+download/xml-rpc.el"
+  version "1.6.8"
+  sha256 "ebd4e582f66df200a01344cd89af428d1ae75878c2057c76c65c66e841721045"
+  head "lp:xml-rpc-el", :using => :bzr
 
   def install
-    (share+'emacs/site-lisp').install 'xml-rpc.el'
+    (share/"emacs/site-lisp/xml-rpc").install "xml-rpc.el"
   end
 end
