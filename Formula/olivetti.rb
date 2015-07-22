87718b24ff2a0a120df6ffe65729b68a446dd86f
Alex Dunn <dunn.alex@gmail.com>
2015-07-22 14:12:21 -0700

    olivetti: needs 24.4+

--- Formula/olivetti.rb
+++ Formula/olivetti.rb
@@ -5,8 +5,9 @@ class Olivetti < EmacsFormula
   homepage "https://github.com/rnkn/olivetti"
   url "https://github.com/rnkn/olivetti/archive/v1.2.0.tar.gz"
   sha256 "e94a65f43e22f42be477a1fae028d4192837195b4f2e496a4c62925d47dc4d7d"
-
   head "https://github.com/rnkn/olivetti.git"
+
+  depends_on :emacs => "24.4"
 
   def install
     byte_compile "olivetti.el"
