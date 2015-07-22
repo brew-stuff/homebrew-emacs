423a9b91233e79c0d36428e01635ca925232eebe
Alex Dunn <dunn.alex@gmail.com>
2015-07-21 23:05:54 -0700

    scss-mode: compile

--- Formula/scss-mode.rb
+++ Formula/scss-mode.rb
@@ -1,3 +1,5 @@
+require File.expand_path("../../emacs", __FILE__)
+
 class ScssMode < Formula
   desc "Major mode for editing .scss files in Emacs"
   homepage "https://github.com/antonj/scss-mode"
@@ -5,16 +7,20 @@ class ScssMode < Formula
   sha256 "765b06cd6f93763c5dc5903b1645d98ea9ea6f75f5d6bc96b68d979224f90812"
   head "https://github.com/antonj/scss-mode.git"
 
+  depends_on :emacs => "22.1"
+
   def install
-    (share/"emacs/site-lisp/scss-mode").install "scss-mode.el"
+    Emacs.compile "scss-mode.el"
+    (share/"emacs/site-lisp/scss-mode").install "scss-mode.el",
+                                                "scss-mode.elc"
     doc.install "README.org"
   end
 
   def caveats; <<-EOS.undent
     Add the following to your init file:
 
-      (require 'scss-mode)
-      (add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
-    EOS
+    (require 'scss-mode)
+    (add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
+  EOS
   end
 end
