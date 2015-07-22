3a1c9a7a625a1f91a34dbd177e3fdf504d8b7bb8
Alex Dunn <dunn.alex@gmail.com>
2015-07-21 21:57:33 -0700

    color-theme-solarized: compile during install

--- Formula/color-theme-solarized.rb
+++ Formula/color-theme-solarized.rb
@@ -1,20 +1,24 @@
+require File.expand_path("../../emacs", __FILE__)
+
 class ColorThemeSolarized < Formula
-  desc "Ethan Schoonover's Solarized color scheme"
+  desc "Ethan Schoonover's Solarized color scheme for Emacs"
   homepage "https://github.com/sellout/emacs-color-theme-solarized"
   head "https://github.com/sellout/emacs-color-theme-solarized.git"
 
   def install
-    (share/"emacs/site-lisp/solarized").install Dir["*.el"]
+    Emacs.compile Dir["*.el"]
+    (share/"emacs/site-lisp/solarized").install Dir["*.el"],
+                                                Dir["*.elc"]
     doc.install "README.md"
   end
 
   def caveats; <<-EOS.undent
     Add the following lines to your init file:
 
-      (add-to-list 'custom-theme-load-path "#{HOMEBREW_PREFIX}/share/emacs/site-lisp/solarized")
-      (load-theme 'solarized t)
-      (setq solarized-termcolors 256)
-    EOS
+    (add-to-list 'custom-theme-load-path "#{HOMEBREW_PREFIX}/share/emacs/site-lisp/solarized")
+    (load-theme 'solarized t)
+    (setq solarized-termcolors 256)
+  EOS
   end
 
   test do
