0dc800104690b6c033ee279d9a4495aa88e3ddf2
Alex Dunn <dunn.alex@gmail.com>
2015-07-21 17:25:10 -0700

    metaweblog: depends_on xml-rpc

--- Formula/metaweblog.rb
+++ Formula/metaweblog.rb
@@ -5,6 +5,8 @@ class Metaweblog < Formula
   sha256 "14ac8029fc685417d419f28f2f04acb38ddfa269669b0c862d4ba6ca1fffb906"
   head "https://github.com/punchagan/metaweblog.git"
 
+  depends_on "dunn/emacs/xml-rpc"
+
   def install
     (share/"emacs/site-lisp/metaweblog").install "metaweblog.el"
   end
