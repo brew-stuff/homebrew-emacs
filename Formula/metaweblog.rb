fc14c11bfc747032fb50e9e19d90f3fb26c01541
Alex Dunn <dunn.alex@gmail.com>
2015-07-21 17:17:27 -0700

    metaweblog 0.1.1

--- Formula/metaweblog.rb
+++ Formula/metaweblog.rb
@@ -1,10 +1,11 @@
-require 'formula'
-
 class Metaweblog < Formula
-  homepage 'https://github.com/punchagan/metaweblog'
-  head 'https://github.com/punchagan/metaweblog.git'
+  desc "Emacs library to access metaweblog-based weblogs"
+  homepage "https://github.com/punchagan/metaweblog"
+  url "https://github.com/punchagan/metaweblog/archive/v0.1.1.tar.gz"
+  sha256 "14ac8029fc685417d419f28f2f04acb38ddfa269669b0c862d4ba6ca1fffb906"
+  head "https://github.com/punchagan/metaweblog.git"
 
   def install
-    (share+'emacs/site-lisp').install 'metaweblog.el'
+    (share/"emacs/site-lisp/metaweblog").install "metaweblog.el"
   end
 end
