e64686b6e9c2e8f3d016234abc35b467579b80c7
Alex Dunn <dunn.alex@gmail.com>
2015-07-21 22:54:05 -0700

    rainbow mode: add test, and compile during install

--- Formula/rainbow-mode.rb
+++ Formula/rainbow-mode.rb
@@ -1,3 +1,5 @@
+require File.expand_path("../../emacs", __FILE__)
+
 class RainbowMode < Formula
   desc "Minor mode for highlighting color-strings"
   homepage "http://elpa.gnu.org/packages/rainbow-mode.html"
@@ -5,19 +7,34 @@ class RainbowMode < Formula
   sha256 "72ed06fb4f2f3ab1d5e2a9aac10864bce1d45f1923e52ee9a8e6d8f2930e16a5"
   head "http://git.savannah.gnu.org/cgit/emacs/elpa.git/plain/packages/rainbow-mode/rainbow-mode.el"
 
+  depends_on :emacs => "22.1"
+
   def install
-    version_string = build.stable? ? "-#{version}" : ""
-    (share/"emacs/site-lisp/rainbow-mode").install "rainbow-mode#{version_string}.el" => "rainbow-mode.el"
+    mv "rainbow-mode-#{version}.el", "rainbow-mode.el" if build.stable?
+
+    Emacs.compile "rainbow-mode.el"
+    (share/"emacs/site-lisp/rainbow-mode").install "rainbow-mode.el",
+                                                   "rainbow-mode.elc"
   end
 
   def caveats; <<-EOS.undent
     Add hooks to activate rainbow-mode where desired, like so:
 
-      (require 'rainbow-mode)
-      (add-hook 'scss-mode-hook 'scss-rainbow-hook)
-      (defun scss-rainbow-hook ()
-        "Colorize color strings."
-        (rainbow-mode 1))
+    (require 'rainbow-mode)
+    (add-hook 'scss-mode-hook 'scss-rainbow-hook)
+    (defun scss-rainbow-hook ()
+      "Colorize color strings."
+      (rainbow-mode 1))
+  EOS
+  end
+
+  test do
+    (testpath/"test.el").write <<-EOS.undent
+      (add-to-list 'load-path "#{HOMEBREW_PREFIX}/share/emacs/site-lisp")
+      (load "rainbow-mode")
+      (rainbow-mode 1)
+      (print (minibuffer-prompt-width))
     EOS
+    assert_equal "0", shell_output("emacs -batch -l #{testpath}/test.el").strip
   end
 end
