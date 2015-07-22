0c8c06f38a35fb03d58cfc957b260d0db3a4422d
Alex Dunn <dunn.alex@gmail.com>
2015-07-21 18:38:01 -0700

    Emacs: batch byte compiling

--- emacs.rb
+++ emacs.rb
@@ -2,4 +2,11 @@ module Emacs
   def self.version
     Utils.popen_read("emacs", "--batch", "--eval", "(princ emacs-version)").to_f
   end
+
+  def self.compile(*args)
+    # TODO: load directories of dependencies
+    args.each do |file|
+      system "emacs", "--batch", "-Q", "-f", "batch-byte-compile", *file
+    end
+  end
 end
