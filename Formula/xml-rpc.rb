5f57b49ba03c753b604c6d5094ba941771f6f6c7
Alex Dunn <dunn.alex@gmail.com>
2015-07-22 00:02:43 -0700

    xml-rpc: compile

--- Formula/xml-rpc.rb
+++ Formula/xml-rpc.rb
@@ -1,11 +1,18 @@
+require File.expand_path("../../Homebrew/emacs", __FILE__)
+
 class XmlRpc < Formula
+  desc "XML-RPC client for Emacs"
   homepage "https://launchpad.net/xml-rpc-el"
   url "https://launchpad.net/xml-rpc-el/trunk/1.6.8/+download/xml-rpc.el"
   version "1.6.8"
   sha256 "ebd4e582f66df200a01344cd89af428d1ae75878c2057c76c65c66e841721045"
   head "lp:xml-rpc-el", :using => :bzr
 
+  depends_on :emacs => "22.1"
+
   def install
-    (share/"emacs/site-lisp/xml-rpc").install "xml-rpc.el"
+    Emacs.compile "xml-rpc.el"
+    (share/"emacs/site-lisp/xml-rpc").install "xml-rpc.el",
+                                              "xml-rpc.elc"
   end
 end
