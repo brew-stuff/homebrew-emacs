9a43dd0208c9a9f87997db2627c6061e374d4e77
Alex Dunn <dunn.alex@gmail.com>
2015-07-22 15:34:48 -0700

    auto-complete: github is a better homepage

--- Formula/auto-complete.rb
+++ Formula/auto-complete.rb
@@ -1,6 +1,8 @@
+require File.expand_path("../../Homebrew/emacs-formula", __FILE__)
+
 class AutoComplete < EmacsFormula
   desc "Autocompletion extension for Emacs"
-  homepage "http://auto-complete.org/"
+  homepage "https://github.com/auto-complete/auto-complete"
   url "https://github.com/auto-complete/auto-complete/archive/v1.5.0.tar.gz"
   sha256 "a960848fcb94f438c6795070b3125c1a039bf11cc058dbd60e8668adb3cebe4c"
   head "https://github.com/auto-complete/auto-complete.git"
