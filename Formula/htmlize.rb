c8e276e8e8983e55087b70ad816277f67583ba4b
Alex Dunn <dunn.alex@gmail.com>
2015-07-23 13:56:56 -0700

    htmlize: cleanup, add test

--- Formula/htmlize.rb
+++ Formula/htmlize.rb
@@ -1,11 +1,28 @@
-require 'formula'
+require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 
-class Htmlize < Formula
-  homepage 'http://www.emacswiki.org/emacs/Htmlize' # couldn't find a proper homepage
-  url 'http://fly.srk.fer.hr/~hniksic/emacs/htmlize.git', :tag => "release/1.47"
-  head 'http://fly.srk.fer.hr/~hniksic/emacs/htmlize.git'
+class Htmlize < EmacsFormula
+  desc "Emacs package for converting text to HTML"
+  homepage "http://www.us.xemacs.org/People/hrvoje.niksic/"
+  url "http://fly.srk.fer.hr/~hniksic/emacs/htmlize.git",
+      :tag => "release/1.47",
+      :revision => "aa6e2f6dba6fdfa200c7c55efe29ff63380eac8f"
+
+  head "http://fly.srk.fer.hr/~hniksic/emacs/htmlize.git"
+
+  depends_on :emacs => "22.1"
 
   def install
-    (share+'emacs/site-lisp').install 'htmlize.el'
+    byte_compile "htmlize.el"
+    (share/"emacs/site-lisp/htmlize").install "htmlize.el", "htmlize.elc"
+    doc.install "NEWS"
+  end
+
+  test do
+    (testpath/"test.el").write <<-EOS.undent
+      (add-to-list 'load-path "#{HOMEBREW_PREFIX}/share/emacs/site-lisp")
+      (load "htmlize")
+      (print htmlize-version)
+    EOS
+    assert_match version.to_s, shell_output("emacs -batch -l #{testpath}/test.el").strip
   end
 end
