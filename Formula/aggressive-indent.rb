fe8103e89b1a04ca3fbfe653bdf9f2febedff34a
Alex Dunn <dunn.alex@gmail.com>
2015-07-23 00:28:32 -0700

    aggressive-indent 1.1 (new formula)

---
+++ Formula/aggressive-indent.rb
@@ -0,0 +1,39 @@
+require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
+
+class AggressiveIndent < EmacsFormula
+  desc "Emacs minor mode to keep code always indented"
+  homepage "https://github.com/Malabarba/aggressive-indent-mode"
+  url "https://github.com/Malabarba/aggressive-indent-mode/archive/1.1.tar.gz"
+  sha256 "80c4f681800506cf48606a3db017eff8c33ce763f36bdea4e2e8ba6e7888f030"
+  head "https://github.com/Malabarba/aggressive-indent-mode.git"
+
+  depends_on :emacs => "24.1"
+  depends_on "dunn/emacs/names"
+  depends_on "dunn/emacs/cl-lib" if Emacs.version < 24.3
+
+  def install
+    byte_compile "aggressive-indent.el"
+    (share/"emacs/site-lisp/aggressive-indent").install Dir["*.el"],
+                                                        Dir["*.elc"]
+    doc.install "README.md"
+  end
+
+  def caveats; <<-EOS.undent
+    Add the following to your init file, modified to your tastes:
+
+    (require 'aggressive-indent)
+    (global-aggressive-indent-mode 1)
+    (add-to-list 'aggressive-indent-excluded-modes 'html-mode)
+  EOS
+  end
+
+  test do
+    (testpath/"test.el").write <<-EOS.undent
+      (add-to-list 'load-path "#{HOMEBREW_PREFIX}/share/emacs/site-lisp")
+      (load "aggressive-indent")
+      (global-aggressive-indent-mode 1)
+      (print (minibuffer-prompt-width))
+    EOS
+    assert_equal "0", shell_output("emacs -batch -l #{testpath}/test.el").strip
+  end
+end
