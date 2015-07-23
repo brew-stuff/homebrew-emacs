d615b413db62141388b1ed230e5b87490b3cac33
Alex Dunn <dunn.alex@gmail.com>
2015-07-23 00:41:05 -0700

    all-emacs 1.0 (new formula)

---
+++ Formula/all-emacs.rb
@@ -0,0 +1,33 @@
+require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
+
+class AllEmacs < EmacsFormula
+  desc "Emacs package to edit all lines matching a given regexp"
+  homepage "http://elpa.gnu.org/packages/all.html"
+  url "http://elpa.gnu.org/packages/all-1.0.el"
+  sha256 "23c200460cb8ebcbc03e781562277e550b8c6bb075593de0d70840dbc165049e"
+  head "http://git.savannah.gnu.org/cgit/emacs/elpa.git/plain/packages/all/all.el"
+
+  depends_on :emacs
+
+  def install
+    mv "all-#{version}.el", "all.el" if build.stable?
+    byte_compile "all.el"
+    (share/"emacs/site-lisp/all").install "all.el", "all.elc"
+  end
+
+  def caveats; <<-EOS.undent
+    Add the following to your init file:
+
+    (require 'all)
+  EOS
+  end
+
+  test do
+    (testpath/"test.el").write <<-EOS.undent
+      (add-to-list 'load-path "#{HOMEBREW_PREFIX}/share/emacs/site-lisp")
+      (load "all")
+      (print (minibuffer-prompt-width))
+    EOS
+    assert_equal "0", shell_output("emacs -batch -l #{testpath}/test.el").strip
+  end
+end
