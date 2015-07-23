4709d8e8853371b53f9198c331b30503f5053ad1
Alex Dunn <dunn.alex@gmail.com>
2015-07-22 23:44:53 -0700

    ada-mode: build with reference manual by default

--- Formula/ada-mode.rb
+++ Formula/ada-mode.rb
@@ -6,7 +6,14 @@ class AdaMode < EmacsFormula
   url "http://stephe-leake.org/emacs/ada-mode/org.emacs.ada-mode-5.1.8.tar.gz"
   sha256 "361b5e4f3bd8cb48a236245ce65093c361175e4ae782d0233555bd9bd294aa91"
 
+  option "without-reference", "Build without the reference manual"
+
   depends_on :emacs => "24.2"
+
+  resource "reference" do
+    url "http://stephe-leake.org/ada/arm_info-2012.2.tar.gz"
+    sha256 "4e734e37b82a5db34656c6cff9bfee0b11f9ebbcac1cc6716d80541a25359970"
+  end
 
   resource "mode-keys" do
     url "http://stephe-leake.org/emacs/ada-mode/ada-mode-keys.el"
@@ -19,6 +26,9 @@ class AdaMode < EmacsFormula
     (share/"emacs/site-lisp/ada-mode").install Dir["*.el"],
                                                Dir["*.elc"]
     info.install Dir["*.info"]
+    if build.with? "reference"
+      resource("reference").stage { info.install Dir["*"] }
+    end
 
     resource("mode-keys").stage do
       byte_compile "ada-mode-keys.el"
