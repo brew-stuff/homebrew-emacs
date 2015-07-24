aa1b8ec0820e1bda0b8d813acf8a643cf97c21c3
Alex Dunn <dunn.alex@gmail.com>
2015-07-23 22:24:15 -0700

    enwc 1.0 (new formula)

---
+++ Formula/enwc.rb
@@ -0,0 +1,42 @@
+require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
+
+class Enwc < EmacsFormula
+  desc "The Emacs Network Client"
+  homepage "http://elpa.gnu.org/packages/enwc.html"
+  url "http://elpa.gnu.org/packages/enwc-1.0.tar"
+  sha256 "1eedd7238722c71d6686afd636d36daabbbf2ecf5ee9562affcf33651f9bb2a6"
+  head "http://bzr.savannah.nongnu.org/r/enwc/trunk", :using => :bzr
+
+  depends_on :emacs
+  depends_on "dunn/emacs/cl-lib" if Emacs.version < 24.3
+
+  def install
+    if build.stable?
+      byte_compile Dir["*.el"]
+      (share/"emacs/site-lisp/enwc").install Dir["*.el"], Dir["*.elc"]
+    else
+      # currently fails:
+      # enwc-wicd.el:378:1:Error: Unknown upattern `(quote addr)'
+      system "make"
+      system "make", "install", "PREFIX=#{prefix}"
+    end
+    doc.install Dir["doc/*"]
+  end
+
+  def caveats; <<-EOS.undent
+    Add the following to your init file:
+
+    (require 'enwc-setup)
+    (enwc-setup)
+  EOS
+  end
+
+  test do
+    (testpath/"test.el").write <<-EOS.undent
+      (add-to-list 'load-path "#{HOMEBREW_PREFIX}/share/emacs/site-lisp")
+      (load "enwc-setup")
+      (print (minibuffer-prompt-width))
+    EOS
+    assert_equal "0", shell_output("emacs -batch -l #{testpath}/test.el").strip
+  end
+end
