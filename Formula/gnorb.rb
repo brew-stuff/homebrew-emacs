6b87b8b378ef6bfe3483a96f2e71784155d86537
Alex Dunn <dunn.alex@gmail.com>
2015-07-24 00:43:07 -0700

    gnorb 1.1.1 (new formula)

---
+++ Formula/gnorb.rb
@@ -0,0 +1,36 @@
+require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
+
+class Gnorb < EmacsFormula
+  desc "Glue code between Gnus, Org, and BBDB in Emacs"
+  homepage "https://github.com/girzel/gnorb"
+  url "http://elpa.gnu.org/packages/gnorb-1.1.1.tar"
+  sha256 "aadf76a504dcf0acfa0884178376dc6006162d4257c9e447bca9b66b2d7b22da"
+  head "https://github.com/girzel/gnorb.git"
+
+  depends_on :emacs
+  depends_on "dunn/emacs/cl-lib" if Emacs.version < 24.3
+
+  def install
+    byte_compile Dir["*.el"]
+    (share/"emacs/site-lisp/gnorb").install Dir["*.el"],
+                                            Dir["*.elc"]
+    doc.install "README.org", "gnorb.texi"
+    info.install "gnorb.info"
+  end
+
+  def caveats; <<-EOS.undent
+    Add the following to your init file:
+
+    (require 'gnorb)
+  EOS
+  end
+
+  test do
+    (testpath/"test.el").write <<-EOS.undent
+      (add-to-list 'load-path "#{HOMEBREW_PREFIX}/share/emacs/site-lisp")
+      (load "gnorb")
+      (print (minibuffer-prompt-width))
+    EOS
+    assert_equal "0", shell_output("emacs -batch -l #{testpath}/test.el").strip
+  end
+end
