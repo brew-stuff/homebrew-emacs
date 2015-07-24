ec2c2a8a18343264d0be39201bf87aafa40db19a
Alex Dunn <dunn.alex@gmail.com>
2015-07-24 01:03:14 -0700

    gnugo-emacs 3.0.0 (new formula)

---
+++ Formula/gnugo-emacs.rb
@@ -0,0 +1,36 @@
+require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
+
+class GnugoEmacs < EmacsFormula
+  desc "Emacs interface to GNU Go"
+  homepage "http://www.gnuvola.org/software/gnugo/"
+  url "http://elpa.gnu.org/packages/gnugo-3.0.0.tar"
+  sha256 "fd082e88944bf607c6bf115753d0670f6c21a5bc33595c271f02e4d8f19a242d"
+
+  depends_on :emacs
+  depends_on "homebrew/games/gnu-go"
+  depends_on "dunn/emacs/ascii-art-to-unicode"
+  depends_on "dunn/emacs/xpm-emacs"
+  depends_on "dunn/emacs/cl-lib" if Emacs.version < 24.3
+
+  def install
+    byte_compile Dir["*.el"]
+    (share/"emacs/site-lisp/gnugo").install Dir["*.el"], Dir["*.elc"]
+    doc.install "README", "HACKING"
+  end
+
+  def caveats; <<-EOS.undent
+    Add the following to your init file:
+
+    (require 'gnugo)
+  EOS
+  end
+
+  test do
+    (testpath/"test.el").write <<-EOS.undent
+      (add-to-list 'load-path "#{HOMEBREW_PREFIX}/share/emacs/site-lisp")
+      (load "gnugo")
+      (print (minibuffer-prompt-width))
+    EOS
+    assert_equal "0", shell_output("emacs -batch -l #{testpath}/test.el").strip
+  end
+end
