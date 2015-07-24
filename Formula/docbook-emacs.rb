74e5aaff9eea2e9c210aa4d450e94f70cda15c94
Alex Dunn <dunn.alex@gmail.com>
2015-07-23 21:26:01 -0700

    docbook-emacs 0.1 (new formula)

---
+++ Formula/docbook-emacs.rb
@@ -0,0 +1,33 @@
+require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
+
+class DocbookEmacs < EmacsFormula
+  desc "Emacs package for viewing DocBook files"
+  homepage "http://elpa.gnu.org/packages/docbook.html"
+  url "http://elpa.gnu.org/packages/docbook-0.1.el"
+  sha256 "668706d1d52744295cce5c850f4de273eb256eea109b59d3fdb5180e1b7aa007"
+
+  depends_on :emacs
+
+  def install
+    mv "docbook-#{version}.el", "docbook.el"
+    byte_compile "docbook.el"
+    (share/"emacs/site-lisp/docbook").install "docbook.el",
+                                              "docbook.elc"
+  end
+
+  def caveats; <<-EOS.undent
+    Add the following to your init file:
+
+    (require 'docbook)
+  EOS
+  end
+
+  test do
+    (testpath/"test.el").write <<-EOS.undent
+      (add-to-list 'load-path "#{HOMEBREW_PREFIX}/share/emacs/site-lisp")
+      (load "docbook")
+      (print (minibuffer-prompt-width))
+    EOS
+    assert_equal "0", shell_output("emacs -batch -l #{testpath}/test.el").strip
+  end
+end
