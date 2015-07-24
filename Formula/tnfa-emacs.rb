e3e45e21e2a9a78c680abd5ce84f0f972fc39efc
Alex Dunn <dunn.alex@gmail.com>
2015-07-23 21:03:52 -0700

    tnfa-emacs 0.1.1 (new formula)

---
+++ Formula/tnfa-emacs.rb
@@ -0,0 +1,26 @@
+require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
+
+class TnfaEmacs < EmacsFormula
+  desc "Tagged non-deterministic finite-state automata, in Emacs"
+  homepage "http://www.dr-qubit.org/emacs.php"
+  url "http://www.dr-qubit.org/download.php?file=predictive/tNFA-0.1.1.el"
+  sha256 "e61159c523166252709855ab5c71ea3aebea381d50e269c2dbc769b4d02d45ec"
+
+  depends_on :emacs
+  depends_on "dunn/emacs/queue-emacs"
+
+  def install
+    mv "tNFA-#{version}.el", "tNFA.el"
+    byte_compile "tNFA.el"
+    (share/"emacs/site-lisp/tNFA").install "tNFA.el", "tNFA.elc"
+  end
+
+  test do
+    (testpath/"test.el").write <<-EOS.undent
+      (add-to-list 'load-path "#{HOMEBREW_PREFIX}/share/emacs/site-lisp")
+      (load "tNFA")
+      (print (minibuffer-prompt-width))
+    EOS
+    assert_equal "0", shell_output("emacs -batch -l #{testpath}/test.el").strip
+  end
+end
