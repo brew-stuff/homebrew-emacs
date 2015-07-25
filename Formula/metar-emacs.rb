2a67f9a9e587935014bdedeeaa4720954210ebdf
Alex Dunn <dunn.alex@gmail.com>
2015-07-24 18:51:22 -0700

    metar-emacs 0.1 (new formula)

---
+++ Formula/metar-emacs.rb
@@ -0,0 +1,34 @@
+require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
+
+class MetarEmacs < EmacsFormula
+  desc "Emacs package for accessing METAR weather information"
+  homepage "http://elpa.gnu.org/packages/metar.html"
+  url "http://elpa.gnu.org/packages/metar-0.1.el"
+  sha256 "28c4e562ee7a98eaa0b57f92c34b1f095dafb7e68b9cd2552f5008a0eff73f69"
+
+  depends_on :emacs
+  depends_on "dunn/emacs/cl-lib" if Emacs.version < 24.3
+
+  def install
+    mv "metar-#{version}.el", "metar.el"
+    byte_compile "metar.el"
+    (share/"emacs/site-lisp/metar").install "metar.el",
+                                            "metar.elc"
+  end
+
+  def caveats; <<-EOS.undent
+    Add the following to your init file:
+
+    (require 'metar)
+  EOS
+  end
+
+  test do
+    (testpath/"test.el").write <<-EOS.undent
+      (add-to-list 'load-path "#{HOMEBREW_PREFIX}/share/emacs/site-lisp")
+      (load "metar")
+      (print (metar-get-record "SARC"))
+    EOS
+    assert_match "SARC", shell_output("emacs -batch -l #{testpath}/test.el").strip
+  end
+end
