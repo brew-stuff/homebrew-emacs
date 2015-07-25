86ce8f3f89c3d3241c69e7af982fb1e3449cbe48
Alex Dunn <dunn.alex@gmail.com>
2015-07-24 17:00:02 -0700

    jgraph-mode 1.1 (new formula)

---
+++ Formula/jgraph-mode.rb
@@ -0,0 +1,34 @@
+require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
+
+class JgraphMode < EmacsFormula
+  desc "Emacs major mode for editing Jgraph files"
+  homepage "http://elpa.gnu.org/packages/jgraph-mode.html"
+  url "http://elpa.gnu.org/packages/jgraph-mode-1.1.el"
+  sha256 "1469d74235c64e6baa2e48183b1167bb6afc029d8e0efaace9e9e4f2658ee910"
+
+  depends_on :emacs
+  depends_on "dunn/emacs/cl-lib" if Emacs.version < 24.3
+
+  def install
+    mv "jgraph-mode-#{version}.el", "jgraph-mode.el"
+    byte_compile "jgraph-mode.el"
+    (share/"emacs/site-lisp/jgraph-mode").install "jgraph-mode.el",
+                                                  "jgraph-mode.elc"
+  end
+
+  def caveats; <<-EOS.undent
+    Add the following to your init file:
+
+    (require 'jgraph-mode)
+  EOS
+  end
+
+  test do
+    (testpath/"test.el").write <<-EOS.undent
+      (add-to-list 'load-path "#{HOMEBREW_PREFIX}/share/emacs/site-lisp")
+      (load "jgraph-mode")
+      (print jgraph-commands)
+    EOS
+    assert_match "epilogue", shell_output("emacs -batch -l #{testpath}/test.el").strip
+  end
+end
