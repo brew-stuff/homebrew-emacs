5c77edbb11b7cc361e3d45ee1ab4496a06799eac
Alex Dunn <dunn.alex@gmail.com>
2015-07-24 23:08:07 -0700

    dom-emacs (new formula; head-only)
    is this the canonical dom.el? ¯\_(ツ)_/¯

---
+++ Formula/dom-emacs.rb
@@ -0,0 +1,26 @@
+require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
+
+class DomEmacs < EmacsFormula
+  desc "Emacs library for the Document Object Model"
+  homepage "https://github.com/toroidal-code/dom.el"
+  head "https://github.com/toroidal-code/dom.el.git"
+
+  depends_on :emacs
+  depends_on "dunn/emacs/cl-lib" if Emacs.version < 24.3
+
+  def install
+    byte_compile "dom.el"
+    (share/"emacs/site-lisp/dom").install "dom.el",
+                                          "dom.elc"
+    doc.install "README.md"
+  end
+
+  test do
+    (testpath/"test.el").write <<-EOS.undent
+      (add-to-list 'load-path "#{HOMEBREW_PREFIX}/share/emacs/site-lisp")
+      (load "dom")
+      (print (minibuffer-prompt-width))
+    EOS
+    assert_equal "0", shell_output("emacs -batch -l #{testpath}/test.el").strip
+  end
+end
