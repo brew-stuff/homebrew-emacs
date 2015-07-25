cf08e27bac5577e5af1435480ecd43e5aebe022b
Alex Dunn <dunn.alex@gmail.com>
2015-07-24 17:39:45 -0700

    lex-emacs 1.1 (new formula)

---
+++ Formula/lex-emacs.rb
@@ -0,0 +1,33 @@
+require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
+
+class LexEmacs < EmacsFormula
+  desc "Lexical analyser construction"
+  homepage "http://elpa.gnu.org/packages/lex.html"
+  url "http://elpa.gnu.org/packages/lex-1.1.tar"
+  sha256 "672dfebb43ea57cf05718b3bd9a72ce7967d5b3dc6f36aa18dcf8a25e688d9c4"
+
+  depends_on :emacs
+  depends_on "dunn/emacs/cl-lib" if Emacs.version < 24.3
+
+  def install
+    byte_compile Dir["*.el"]
+    (share/"emacs/site-lisp/lex").install Dir["*.el"],
+                                          Dir["*.elc"]
+  end
+
+  def caveats; <<-EOS.undent
+    Add the following to your init file:
+
+    (require 'lex)
+  EOS
+  end
+
+  test do
+    (testpath/"test.el").write <<-EOS.undent
+      (add-to-list 'load-path "#{HOMEBREW_PREFIX}/share/emacs/site-lisp")
+      (load "lex")
+      (print (minibuffer-prompt-width))
+    EOS
+    assert_equal "0", shell_output("emacs -batch -l #{testpath}/test.el").strip
+  end
+end
