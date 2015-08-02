d940f71ed90b5f5872adb339df0d5c1f651ba7bb
Alex Dunn <dunn.alex@gmail.com>
2015-08-02 12:21:07 -0700

    epl/pkg-info: fix tests

--- Formula/epl.rb
+++ Formula/epl.rb
@@ -20,10 +20,10 @@ class Epl < EmacsFormula
 
   test do
     (testpath/"test.el").write <<-EOS.undent
-      (add-to-list 'load-path "#{HOMEBREW_PREFIX}/share/emacs/site-lisp")
-      (require 'elp)
+      (add-to-list 'load-path "#{share}/emacs/site-lisp/epl")
+      (load "elp")
       (print (minibuffer-prompt-width))
     EOS
-    assert_equal "0", shell_output("emacs -batch -l #{testpath}/test.el").strip
+    assert_equal "0", shell_output("emacs -Q --batch -l #{testpath}/test.el").strip
   end
 end
