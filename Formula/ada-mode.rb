436ec33e62f9acbabf708d3a1d8a21c1c0cb8f79
Alex Dunn <dunn.alex@gmail.com>
2015-08-02 10:57:28 -0700

    ada-mode: fix test

--- Formula/ada-mode.rb
+++ Formula/ada-mode.rb
@@ -48,10 +48,10 @@ class AdaMode < EmacsFormula
 
   test do
     (testpath/"test.el").write <<-EOS.undent
-      (add-to-list 'load-path "#{HOMEBREW_PREFIX}/share/emacs/site-lisp")
+      (add-to-list 'load-path "#{share}/emacs/site-lisp/ada-mode")
       (load "ada-mode")
       (print (ada-mode-version))
     EOS
-    assert_match version.to_s, shell_output("emacs -batch -l #{testpath}/test.el").strip
+    assert_match version.to_s, shell_output("emacs -Q --batch -l #{testpath}/test.el").strip
   end
 end
