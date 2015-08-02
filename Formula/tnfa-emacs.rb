89e25e3d7309474ab1712a6dd64a653b05a38e49
Alex Dunn <dunn.alex@gmail.com>
2015-08-02 12:04:28 -0700

    tnfa-emacs: fix test

--- Formula/tnfa-emacs.rb
+++ Formula/tnfa-emacs.rb
@@ -17,10 +17,11 @@ class TnfaEmacs < EmacsFormula
 
   test do
     (testpath/"test.el").write <<-EOS.undent
-      (add-to-list 'load-path "#{HOMEBREW_PREFIX}/share/emacs/site-lisp")
+      (add-to-list 'load-path "#{share}/emacs/site-lisp/tnfa")
+      (add-to-list 'load-path "#{Formula["queue-emacs"].share}/emacs/site-lisp/queue")
       (load "tNFA")
       (print (minibuffer-prompt-width))
     EOS
-    assert_equal "0", shell_output("emacs -batch -l #{testpath}/test.el").strip
+    assert_equal "0", shell_output("emacs -Q --batch -l #{testpath}/test.el").strip
   end
 end
