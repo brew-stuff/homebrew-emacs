58e28ba9e8ada96793eacf6ffe255329ebe91875
Alex Dunn <dunn.alex@gmail.com>
2015-08-02 11:53:42 -0700

    dict-tree: fix test

--- Formula/dict-tree.rb
+++ Formula/dict-tree.rb
@@ -21,10 +21,14 @@ class DictTree < EmacsFormula
 
   test do
     (testpath/"test.el").write <<-EOS.undent
-      (add-to-list 'load-path "#{HOMEBREW_PREFIX}/share/emacs/site-lisp")
+      (add-to-list 'load-path "#{share}/emacs/site-lisp/dict-tree")
+      (add-to-list 'load-path "#{Formula["trie-emacs"].share}/emacs/site-lisp/trie")
+      (add-to-list 'load-path "#{Formula["heap-emacs"].share}/emacs/site-lisp/heap")
+      (add-to-list 'load-path "#{Formula["tnfa-emacs"].share}/emacs/site-lisp/tNFA")
+      (add-to-list 'load-path "#{Formula["queue-emacs"].share}/emacs/site-lisp/queue")
       (load "dict-tree")
       (print (minibuffer-prompt-width))
     EOS
-    assert_equal "0", shell_output("emacs -batch -l #{testpath}/test.el").strip
+    assert_equal "0", shell_output("emacs -Q --batch -l #{testpath}/test.el").strip
   end
 end
