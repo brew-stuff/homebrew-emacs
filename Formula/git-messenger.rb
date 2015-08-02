892e9f690b7b868fc7c7254880af6d6a3919fbb8
Alex Dunn <dunn.alex@gmail.com>
2015-08-02 12:31:28 -0700

    git-messenger: fix test

--- Formula/git-messenger.rb
+++ Formula/git-messenger.rb
@@ -28,12 +28,13 @@ class GitMessenger < EmacsFormula
 
   test do
     (testpath/"test.el").write <<-EOS.undent
-      (add-to-list 'load-path "#{HOMEBREW_PREFIX}/share/emacs/site-lisp")
+      (add-to-list 'load-path "#{share}/emacs/site-lisp/git-messenger")
+      (add-to-list 'load-path "#{Formula["popup"].share}/emacs/site-lisp/popup")
       (load "git-messenger")
       (git-messenger:execute-command 'git '("init") t)
       (print (minibuffer-prompt-width))
     EOS
-    assert_equal "0", shell_output("emacs -batch -l #{testpath}/test.el").strip
+    assert_equal "0", shell_output("emacs -Q --batch -l #{testpath}/test.el").strip
     (testpath/".git").exist?
   end
 end
