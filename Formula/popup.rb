587873cd16ec43b37842c1f964a5c8a0e9f85e10
Alex Dunn <dunn.alex@gmail.com>
2015-08-03 14:12:44 -0700

    popup 0.5.3

--- Formula/popup.rb
+++ Formula/popup.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class Popup < EmacsFormula
   desc "Visual popup interface library for Emacs"
   homepage "https://github.com/auto-complete/popup-el"
-  url "https://github.com/auto-complete/popup-el/archive/v0.5.2.tar.gz"
-  sha256 "19714326dd5166def540c3f1c47a27e67ffdb3a94c91271c88a8500a15846c0b"
+  url "https://github.com/auto-complete/popup-el/archive/v0.5.3.tar.gz"
+  sha256 "8035782f642f346ba768fa408e4120a9368b0c86f43e3ecd62b527e391742ffb"
   head "https://github.com/auto-complete/popup-el.git"
 
   # only tested on 24
@@ -13,11 +13,19 @@ class Popup < EmacsFormula
   depends_on "homebrew/emacs/cl-lib" if Emacs.version < 24.3
 
   def install
-    # non-interactive test
     system "make", "travis-ci", "CASK=#{Formula["cask"].bin}/cask"
 
     byte_compile "popup.el"
     (share/"emacs/site-lisp/popup").install "popup.el", "popup.elc"
     doc.install "README.md"
+  end
+
+  test do
+    (testpath/"test.el").write <<-EOS.undent
+      (add-to-list 'load-path "#{share}/emacs/site-lisp/popup")
+      (load "popup")
+      (print popup-version)
+    EOS
+    assert_equal "\"#{version}\"", shell_output("emacs -Q --batch -l #{testpath}/test.el").strip
   end
 end
