d4166437c1b19ffe0e74b9c237ea22a6ecbeedf8
Alex Dunn <dunn.alex@gmail.com>
2015-08-03 08:33:47 -0700

    clojure-mode: add with-inf option

--- Formula/clojure-mode.rb
+++ Formula/clojure-mode.rb
@@ -7,8 +7,15 @@ class ClojureMode < EmacsFormula
   sha256 "05105cbeb0c1db47c0be0835de9afde6be76a83ea32e06bbcc07dcd4ac72cb25"
   head "https://github.com/clojure-emacs/clojure-mode.git"
 
+  option "with-inf", "Build with the \"inferior\" REPL"
+
   depends_on :emacs => "24.3"
   depends_on "cask"
+
+  resource "inf" do
+    url "https://github.com/clojure-emacs/inf-clojure/archive/v1.2.0.tar.gz"
+    sha256 "1dd45cee8d263051c09428e07b6c2cb010914b00ff493cdf532dd28d7832ac1c"
+  end
 
   def install
     system "make", "test", "CASK=#{Formula["cask"].bin}/cask"
@@ -16,6 +23,12 @@ class ClojureMode < EmacsFormula
     (share/"emacs/site-lisp/clojure-mode").install Dir["*.el"],
                                                    Dir["*.elc"]
     doc.install "README.md"
+
+    if build.with? "inf"
+      resource("inf").stage do
+        (share/"emacs/site-lisp/clojure-mode").install "inf-clojure.el"
+      end
+    end
   end
 
   def caveats; <<-EOS.undent
