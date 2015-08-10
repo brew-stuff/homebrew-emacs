fcbd1db8478806fc34b2de97c9eb64fb41211701
Alex Dunn <dunn.alex@gmail.com>
2015-08-10 13:32:40 -0700

    circe 2.0
    Closes #8.

--- Formula/circe.rb
+++ Formula/circe.rb
@@ -3,27 +3,18 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class Circe < EmacsFormula
   desc "Emacs IRC client"
   homepage "https://github.com/jorgenschaefer/circe"
-  url "https://github.com/jorgenschaefer/circe/archive/v1.5.tar.gz"
-  sha256 "a67fc53113de9af919ba1da5906d86eecd5418fd3e53793c266a44d4f214a284"
-
-  head do
-    url "https://github.com/jorgenschaefer/circe.git"
-    depends_on "cask"
-  end
+  url "https://github.com/jorgenschaefer/circe/archive/v2.0.tar.gz"
+  sha256 "c35ca0016a1da8f8c08cc20439a24af82b937b95250711d1c4b2944a3caec45d"
+  head "https://github.com/jorgenschaefer/circe.git"
 
   depends_on :emacs
+  depends_on "cask"
   depends_on "homebrew/emacs/cl-lib" if Emacs.version < 24.3
 
   def install
     doc.install "README.md", "LICENSE"
 
-    if build.stable?
-      Dir.chdir "lisp"
-      byte_compile Dir["*.el"]
-    else
-      inreplace "scripts/compile", "find -name", "find . -name"
-      system "scripts/compile"
-    end
+    system "scripts/compile"
     (share/"emacs/site-lisp/circe").install Dir["*.el"], Dir["*.elc"]
   end
 
