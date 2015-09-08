ad3f823441a25654f0fbd42639c6bc75a4a7230a
Alex Dunn <dunn.alex@gmail.com>
2015-09-08 11:24:08 -0700

    emacs_requirement: fix no-version hack
    I guess I forgot @version is the minimum version required, not the version of the installed Emacs.  As a result Homebrew would always try to install Emacs when `depends_on :emacs` was given.

--- Homebrew/emacs_requirement.rb
+++ Homebrew/emacs_requirement.rb
@@ -2,9 +2,9 @@ class EmacsRequirement < Requirement
   def initialize(tags)
     @version = tags.shift if /\d+\.*\d*/ === tags.first
     # patch EmacsRequirement to allow no version:
-    # because we can't redefine the satisfaction block, just bump the
-    # version really high if it's not given after `depends_on :emacs`
-    @version ||= "1000"
+    # because we can't redefine the satisfaction block, just floor the
+    # version if it's not given after `depends_on :emacs`
+    @version ||= "0"
     super
   end
 end
