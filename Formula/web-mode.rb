47f732647d5c0f0aaef8768cd9b8a9f6d55472cf
Alex Dunn <dunn.alex@gmail.com>
2015-09-13 11:16:15 -0700

    web-mode 12.2

--- Formula/web-mode.rb
+++ Formula/web-mode.rb
@@ -1,28 +1,18 @@
 require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 
 class WebMode < EmacsFormula
+  desc "Emacs major mode for editing web templates"
   homepage "http://web-mode.org"
-  url "https://github.com/fxbois/web-mode/archive/v12.tar.gz"
-  sha256 "3a43ce2230d46118576c69297df2409f9fdb62875c369f4f9b3385bcfa259c42"
+  url "https://github.com/fxbois/web-mode/archive/v12.2.tar.gz"
+  sha256 "ed05fcbd77c102d489421279796e1781c8f5f2eba0c495946d9655a14b3ff3a7"
   head "https://github.com/fxbois/web-mode.git"
 
-  # not tested below 23
   depends_on :emacs => "23.1"
 
   def install
     system "/bin/bash", "./run.sh"
     byte_compile "web-mode.el"
     (share/"emacs/site-lisp/web-mode").install "web-mode.el", "web-mode.elc"
-    doc.install "README.md", "LICENSE"
-  end
-
-  def caveats; <<-EOS.undent
-    Require web-mode in your init file, then activate it where appropriate, e.g.:
-
-    (require 'web-mode)
-    (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-    (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-  EOS
   end
 
   test do
