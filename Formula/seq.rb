0b5cb9fdb9db477f1fbfbd0a84aa8bdcc079421e
Alex Dunn <dunn.alex@gmail.com>
2015-09-29 18:11:11 -0700

    seq 1.9

--- Formula/seq.rb
+++ Formula/seq.rb
@@ -3,15 +3,14 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class Seq < EmacsFormula
   desc "Sequence manipulation library for Emacs"
   homepage "http://elpa.gnu.org/packages/seq.html"
-  url "http://elpa.gnu.org/packages/seq-1.8.el"
-  sha256 "9c2cd997a5d71c4b2661a28f4a53cce67f6cc9bb3a961abc726453813fc92b15"
+  url "http://elpa.gnu.org/packages/seq-1.9.el"
+  sha256 "08be71299d883c0a547444dc5fbfd7a55f0b74eb037dba32998e936e9ffb2219"
   head "http://git.savannah.gnu.org/cgit/emacs/elpa.git/plain/packages/seq/seq.el"
 
   depends_on :emacs
 
   def install
     mv "seq-#{version}.el", "seq.el" if build.stable?
-
     byte_compile "seq.el"
     (share/"emacs/site-lisp/seq").install "seq.el", "seq.elc"
   end
