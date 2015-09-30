1c46eb0ad3ecf78088959ea90bf21ba987356476
Alex Dunn <dunn.alex@gmail.com>
2015-09-29 18:37:23 -0700

    ahungry-emacs 1.0.10

--- Formula/ahungry-emacs.rb
+++ Formula/ahungry-emacs.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class AhungryEmacs < EmacsFormula
   desc "Bright and bold color theme for Emacs"
   homepage "https://github.com/ahungry/color-theme-ahungry"
-  url "http://elpa.gnu.org/packages/ahungry-theme-1.0.9.tar"
-  sha256 "8c5701ec1ef6f1df244005289322ac51f8543102bd266a89fd69aff99442b1e2"
+  url "http://elpa.gnu.org/packages/ahungry-theme-1.0.10.tar"
+  sha256 "3640f108251de652434af54650c56bf1284eca8bf5eaaa28140c8d68d155352a"
   head "https://github.com/ahungry/color-theme-ahungry.git"
 
   depends_on :emacs => "24.1"
@@ -13,15 +13,6 @@ class AhungryEmacs < EmacsFormula
     byte_compile Dir["*.el"]
     (share/"emacs/site-lisp/ahungry").install Dir["*.el"],
                                               Dir["*.elc"]
-    doc.install "README.md"
-  end
-
-  def caveats; <<-EOS.undent
-    Add the following to your init file:
-
-    (add-to-list 'custom-theme-load-path "#{share}/emacs/site-lisp/ahungry/")
-    (load-theme 'ahungry t)
-  EOS
   end
 
   test do
