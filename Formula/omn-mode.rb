b08516744d08f977e5b0033c469c9d9d378285b8
Alex Dunn <dunn.alex@gmail.com>
2015-09-29 18:18:52 -0700

    omn-mode 1.2

--- Formula/omn-mode.rb
+++ Formula/omn-mode.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class OmnMode < EmacsFormula
   desc "Emacs major mode for the OWL Manchester Notation"
   homepage "http://elpa.gnu.org/packages/omn-mode.html"
-  url "http://elpa.gnu.org/packages/omn-mode-1.0.el"
-  sha256 "65d1293e6ffdc53cc8f617be9a84d2ee1a3aa4725aea93dac2c4b55ea3606a12"
+  url "http://elpa.gnu.org/packages/omn-mode-1.2.el"
+  sha256 "829c291ddd7f3208e5142cce450e93d5ac808f5d919e9f7011c6b2b614aef45c"
 
   depends_on :emacs
 
@@ -13,13 +13,6 @@ class OmnMode < EmacsFormula
     byte_compile "omn-mode.el"
     (share/"emacs/site-lisp/omn-mode").install "omn-mode.el",
                                                "omn-mode.elc"
-  end
-
-  def caveats; <<-EOS.undent
-    Add the following to your init file:
-
-    (require 'omn-mode)
-  EOS
   end
 
   test do
