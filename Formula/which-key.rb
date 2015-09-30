ace042b9b690cd98d7f5f1faac981b0b0186c8bd
Alex Dunn <dunn.alex@gmail.com>
2015-09-29 17:59:10 -0700

    which-key 0.6.1

--- Formula/which-key.rb
+++ Formula/which-key.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class WhichKey < EmacsFormula
   desc "Emacs package for displaying available keybindings"
   homepage "https://github.com/justbur/emacs-which-key"
-  url "https://github.com/justbur/emacs-which-key/archive/v0.5.2.tar.gz"
-  sha256 "1c8975ba7721c678f4f0f4892b0a8e4f4b7b1f9f1a594a2a61600d337d753086"
+  url "https://github.com/justbur/emacs-which-key/archive/v0.6.1.tar.gz"
+  sha256 "06dc0b0cd10f4e5058ce09bfab641562c60899581f108e1084d7080208a36045"
   head "https://github.com/justbur/emacs-which-key.git"
 
   depends_on :emacs => "24.3"
@@ -15,14 +15,6 @@ class WhichKey < EmacsFormula
     byte_compile "which-key.el"
     (share/"emacs/site-lisp/which-key").install "which-key.el",
                                                 "which-key.elc"
-  end
-
-  def caveats; <<-EOS.undent
-    Add the following to your init file:
-
-    (require 'which-key)
-    (which-key-mode)
-  EOS
   end
 
   test do
