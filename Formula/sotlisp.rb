b6e1594d634153f8fb028598c022cf6d1a6372e0
Alex Dunn <dunn.alex@gmail.com>
2015-09-29 18:04:00 -0700

    sotlisp 1.4

--- Formula/sotlisp.rb
+++ Formula/sotlisp.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class Sotlisp < EmacsFormula
   desc "Emacs minor mode for writing Emacs Lisp faster"
   homepage "https://github.com/Malabarba/speed-of-thought-lisp"
-  url "https://github.com/Malabarba/speed-of-thought-lisp/archive/1.1.tar.gz"
-  sha256 "1150b4478a323e7332c665a9eddb390f10719a5363562d4bde75f1f6bf537dd4"
+  url "https://github.com/Malabarba/speed-of-thought-lisp/archive/1.4.tar.gz"
+  sha256 "23b33570d72039ca431f777c2ea94ab95c54df47dac5b40e54fd9c223fe96a89"
   head "https://github.com/Malabarba/speed-of-thought-lisp.git"
 
   depends_on :emacs => "24.1"
@@ -13,14 +13,6 @@ class Sotlisp < EmacsFormula
     byte_compile "sotlisp.el"
     (share/"emacs/site-lisp/sotlisp").install "sotlisp.el",
                                               "sotlisp.elc"
-    doc.install "README.md"
-  end
-
-  def caveats; <<-EOS.undent
-    Add the following to your init file:
-
-    (require 'sotlisp)
-  EOS
   end
 
   test do
