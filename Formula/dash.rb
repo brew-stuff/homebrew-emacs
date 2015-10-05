61e8d05abb98d3f89debf2376d03a510477744bd
Alex Dunn <dunn.alex@gmail.com>
2015-10-04 18:37:26 -0700

    dash 2.12.0

--- Formula/dash.rb
+++ Formula/dash.rb
@@ -3,12 +3,10 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class Dash < EmacsFormula
   desc "A modern list library for Emacs"
   homepage "https://github.com/magnars/dash.el"
-  url "https://github.com/magnars/dash.el/archive/2.11.0.tar.gz"
-  sha256 "d888d34b9b86337c5740250f202e7f2efc3bf059b08a817a978bf54923673cde"
+  url "https://github.com/magnars/dash.el/archive/2.12.0.tar.gz"
+  sha256 "c6db3a237747b02d20382a761397563d813b306c020ae28ce25a1c3915fac60f"
   head "https://github.com/magnars/dash.el.git"
 
-  # the tests might not pass otherwise since it detects version 23 by
-  # checking if $EMACS = "emacs23" which it probably won't be on OS X
   depends_on :emacs => "24.1"
 
   def install
@@ -16,7 +14,16 @@ class Dash < EmacsFormula
     system "./create-docs.sh"
     (share/"emacs/site-lisp/dash").install Dir["*.el"],
                                            Dir["*.elc"]
-    doc.install "README.md", Dir["*.texi"]
+    doc.install Dir["*.texi"]
     info.install "dash.info"
+  end
+
+  test do
+    (testpath/"test.el").write <<-EOS.undent
+      (add-to-list 'load-path "#{share}/emacs/site-lisp/dash")
+      (load "dash")
+      (print (--map (* it it) '(1 2 3 4)))
+    EOS
+    assert_equal "(1 4 9 16)", shell_output("emacs -Q --batch -l #{testpath}/test.el").strip
   end
 end
