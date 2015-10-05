1df09050c79a88b95724f6385f138fecd9d16daf
Alex Dunn <dunn.alex@gmail.com>
2015-10-04 18:42:35 -0700

    aggressive-indent 1.4

--- Formula/aggressive-indent.rb
+++ Formula/aggressive-indent.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class AggressiveIndent < EmacsFormula
   desc "Emacs minor mode to keep code always indented"
   homepage "https://github.com/Malabarba/aggressive-indent-mode"
-  url "https://github.com/Malabarba/aggressive-indent-mode/archive/1.2.tar.gz"
-  sha256 "58eb60bab6b368b68c6a4d78e9ca8d8dd4a9eea76eeab5b8fa2f366e69c7d0a5"
+  url "https://github.com/Malabarba/aggressive-indent-mode/archive/1.4.tar.gz"
+  sha256 "cbc4920e46342c5cbd307b5b0b7b9e4cd43ea9009e7378dce1fd9cae89882db4"
   head "https://github.com/Malabarba/aggressive-indent-mode.git"
 
   depends_on :emacs => "24.1"
@@ -15,15 +15,6 @@ class AggressiveIndent < EmacsFormula
     byte_compile "aggressive-indent.el"
     (share/"emacs/site-lisp/aggressive-indent").install Dir["*.el"],
                                                         Dir["*.elc"]
-  end
-
-  def caveats; <<-EOS.undent
-    Add something like the following to your init file:
-
-    (require 'aggressive-indent)
-    (global-aggressive-indent-mode 1)
-    (add-to-list 'aggressive-indent-excluded-modes 'html-mode)
-  EOS
   end
 
   test do
