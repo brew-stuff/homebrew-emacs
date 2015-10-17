a4f98b39ea232c784a5b5a809b9f8a62e1ae6ac7
Alex Dunn <dunn.alex@gmail.com>
2015-10-16 20:42:35 -0700

    anzu-mode 0.59

--- Formula/anzu-mode.rb
+++ Formula/anzu-mode.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class AnzuMode < EmacsFormula
   desc "Emacs minor mode for numbering search results"
   homepage "https://github.com/syohex/emacs-anzu"
-  url "https://github.com/syohex/emacs-anzu/archive/0.57.tar.gz"
-  sha256 "1146166916738fb1284f233f3a6a917ef24fc2fc842b66300e0dcf4ccd030f99"
+  url "https://github.com/syohex/emacs-anzu/archive/0.59.tar.gz"
+  sha256 "c5342b30bbd81eff9dfde0e5f90831fefa3948d37d8388cf34489c6ff7e74fbf"
   head "https://github.com/syohex/emacs-anzu.git"
 
   depends_on :emacs => "24.1"
