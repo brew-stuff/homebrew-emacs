9b5a636a6b3b82f0c6b8dd2820500da28c700a6d
Alex Dunn <dunn.alex@gmail.com>
2015-10-25 14:43:29 -0700

    emoji-cheat-sheet-plus: add cocoa
    apparently required for png support? see https://travis-ci.org/Homebrew/homebrew-emacs/jobs/88944172

--- Formula/emoji-cheat-sheet-plus.rb
+++ Formula/emoji-cheat-sheet-plus.rb
@@ -7,28 +7,18 @@ class EmojiCheatSheetPlus < EmacsFormula
   sha256 "7f8592a2a54e8fd58689f8f6a6d118b9511e48ce56fa84f294fa6623333c7d5a"
   head "https://github.com/syl20bnr/emacs-emoji-cheat-sheet-plus.git"
 
-  # due to helm
-  depends_on :emacs => "24.3"
+  depends_on :emacs => ["24.3", "with-cocoa"]
   depends_on "homebrew/emacs/helm"
 
   def install
     byte_compile "emoji-cheat-sheet-plus.el"
-    doc.install "README.md", "LICENSE"
-    (share/"emacs/site-lisp/emoji-cheat-sheet-plus").install Dir["*"]
-  end
-
-  def caveats; <<-EOS.undent
-    Add the following to your init file:
-
-    (require 'emoji-cheat-sheet-plus)
-    (global-set-key (kbd "C-c e") 'emoji-cheat-sheet-plus-insert)))
-  EOS
+    elisp.install "emoji-cheat-sheet", Dir["*.el"], Dir["*.elc"]
   end
 
   test do
     (testpath/"test.el").write <<-EOS.undent
-      (add-to-list 'load-path "#{share}/emacs/site-lisp/emoji-cheat-sheet-plus")
-      (add-to-list 'load-path "#{Formula["homebrew/emacs/helm"].share}/emacs/site-lisp/helm")
+      (add-to-list 'load-path "#{elisp}")
+      (add-to-list 'load-path "#{Formula["homebrew/emacs/helm"].opt_elisp}")
       (load "helm")
       (load "emoji-cheat-sheet-plus")
       (emoji-cheat-sheet-plus--create-cache)
