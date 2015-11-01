f026941055b0cd72c64d6d9a0149552f1ae85f26
Alex Dunn <dunn.alex@gmail.com>
2015-10-31 20:19:35 -0700

    beacon-mode 0.5.1 (new formula)
    Closes #44.

---
+++ Formula/beacon-mode.rb
@@ -0,0 +1,29 @@
+require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
+
+class BeaconMode < EmacsFormula
+  desc "Highlight the cursor when an Emacs window scrolls"
+  homepage "https://github.com/Malabarba/beacon"
+  url "http://elpa.gnu.org/packages/beacon-0.5.1.el"
+  sha256 "44ea8964d19e3c68a1d9ce4494fb8de80cecadca8a0736699f43749a2fbefef7"
+  head "https://github.com/Malabarba/beacon.git"
+
+  depends_on :emacs
+  depends_on "homebrew/emacs/seq"
+
+  def install
+    mv "beacon-#{version}.el", "beacon.el" if build.stable?
+
+    byte_compile "beacon.el"
+    (share/"emacs/site-lisp/beacon-mode").install "beacon.el", "beacon.elc"
+  end
+
+  test do
+    (testpath/"test.el").write <<-EOS.undent
+      (add-to-list 'load-path "#{share}/emacs/site-lisp/beacon-mode")
+      (add-to-list 'load-path "#{Formula["homebrew/emacs/seq"].opt_share}/emacs/site-lisp/seq")
+      (load "beacon")
+      (print (minibuffer-prompt-width))
+    EOS
+    assert_equal "0", shell_output("emacs -Q --batch -l #{testpath}/test.el").strip
+  end
+end
