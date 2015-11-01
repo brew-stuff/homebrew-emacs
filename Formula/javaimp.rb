0c1b5ad71d8ca861c7c7a897ff0e84915d8dbadf
Alex Dunn <dunn.alex@gmail.com>
2015-10-31 18:51:04 -0700

    javaimp 0.6
    Closes #42.

--- Formula/javaimp.rb
+++ Formula/javaimp.rb
@@ -1,25 +1,17 @@
 require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 
 class Javaimp < EmacsFormula
-  desc "Emacs functions for managing Java imports in Maven projects"
+  desc "Emacs functions for Java imports in Maven projects"
   homepage "http://elpa.gnu.org/packages/javaimp.html"
-  url "http://elpa.gnu.org/packages/javaimp-0.5.el"
-  sha256 "6d85bae143e632144157bbe5a5654f143b8f5da77dab9c6323112af7220d4ff5"
+  url "http://elpa.gnu.org/packages/javaimp-0.6.el"
+  sha256 "10df463d9ba48a18116bea94fcff30c4e96c8c3a7397a08228fe2f9eb63c4301"
 
   depends_on :emacs
 
   def install
     mv "javaimp-#{version}.el", "javaimp.el"
     byte_compile "javaimp.el"
-    (share/"emacs/site-lisp/javaimp").install "javaimp.el",
-                                              "javaimp.elc"
-  end
-
-  def caveats; <<-EOS.undent
-    Add the following to your init file:
-
-    (require 'javaimp)
-  EOS
+    (share/"emacs/site-lisp/javaimp").install "javaimp.el", "javaimp.elc"
   end
 
   test do
