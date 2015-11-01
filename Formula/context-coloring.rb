4f4df58a8b7c53d706752f94a2b2f777b995bc29
Alex Dunn <dunn.alex@gmail.com>
2015-10-31 19:10:52 -0700

    context-coloring 7.1.0

--- Formula/context-coloring.rb
+++ Formula/context-coloring.rb
@@ -1,10 +1,10 @@
 require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 
 class ContextColoring < EmacsFormula
-  desc "Emacs mode to highlight JS and Elisp code by scope"
+  desc "Emacs mode to color JS and Elisp code by scope"
   homepage "https://github.com/jacksonrayhamilton/context-coloring"
-  url "https://github.com/jacksonrayhamilton/context-coloring/archive/v7.0.0.tar.gz"
-  sha256 "d321bff4dd6a9be795ca701ee3adab88258b62f6d4f7d5eef3167c818aca1c84"
+  url "https://github.com/jacksonrayhamilton/context-coloring/archive/v7.1.0.tar.gz"
+  sha256 "1b7726e06b6050d910b2312e1400e68b6ce511f93d9d784b31f65dc0c31592dd"
   head "https://github.com/jacksonrayhamilton/context-coloring.git"
 
   depends_on :emacs => "24.3"
@@ -16,20 +16,6 @@ class ContextColoring < EmacsFormula
     system "make", "compile", "CASK=#{Formula["cask"].bin}/cask"
     (share/"emacs/site-lisp/context-coloring").install "context-coloring.el",
                                                        "context-coloring.elc"
-    doc.install "README.md"
-  end
-
-  def caveats; <<-EOS.undent
-    Add the following to your init file:
-
-    (require 'context-coloring)
-    (add-hook 'emacs-lisp-mode-hook #'context-coloring-mode)
-    (add-hook 'minibuffer-setup-hook #'context-coloring-mode)
-
-    ;; JavaScript highlighting requires js2-mode
-    (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-    (add-hook 'js2-mode-hook #'context-coloring-mode)
-  EOS
   end
 
   test do
