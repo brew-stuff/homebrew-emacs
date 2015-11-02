230fb56f00687b648953c7594866f4ce05dc2e30
Alex Dunn <dunn.alex@gmail.com>
2015-11-01 17:57:29 -0800

    wisi-emacs: cleanup, audit

--- Formula/wisi-emacs.rb
+++ Formula/wisi-emacs.rb
@@ -1,7 +1,7 @@
 require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 
 class WisiEmacs < EmacsFormula
-  desc "Indentation and navigation in Emacs using LALR parsers"
+  desc "Indentation/navigation in Emacs using LALR parsers"
   homepage "http://elpa.gnu.org/packages/wisi.html"
   url "http://elpa.gnu.org/packages/wisi-1.1.1.tar"
   sha256 "797845efd2358d76d5281f45c19e521a7c7121549128eb43250b3d3b4f8e7791"
@@ -11,20 +11,12 @@ class WisiEmacs < EmacsFormula
 
   def install
     byte_compile Dir["*.el"]
-    (share/"emacs/site-lisp/wisi").install Dir["*.el"], Dir["*.elc"]
-    doc.install "README"
-  end
-
-  def caveats; <<-EOS.undent
-    Add the following to your init file:
-
-    (require 'wisi)
-  EOS
+    elisp.install Dir["*.el"], Dir["*.elc"]
   end
 
   test do
     (testpath/"test.el").write <<-EOS.undent
-      (add-to-list 'load-path "#{share}/emacs/site-lisp/wisi")
+      (add-to-list 'load-path "#{elisp}")
       (load "wisi")
       (print (minibuffer-prompt-width))
     EOS
