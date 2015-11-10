42d490efec0078b06c18a6fc4e28cfa3902b0b5e
Alex Dunn <dunn.alex@gmail.com>
2015-11-09 22:28:43 -0800

    gist-emacs 1.3.1 (new formula)

---
+++ Formula/gist-emacs.rb
@@ -0,0 +1,29 @@
+require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
+
+class GistEmacs < EmacsFormula
+  desc "Emacs integration for gist.github.com"
+  homepage "https://github.com/defunkt/gist.el"
+  url "https://github.com/defunkt/gist.el/archive/v1.3.1.tar.gz"
+  sha256 "a060bc828aceba8a8c9aa850b5157cb85a8fe83987096bcc7500ae0ba86bb54b"
+  head "https://github.com/defunkt/gist.el.git"
+
+  depends_on :emacs => "24.1"
+  depends_on "homebrew/emacs/gh-emacs"
+
+  def install
+    byte_compile "gist.el"
+    elisp.install "gist.el", "gist.elc"
+  end
+
+  test do
+    (testpath/"test.el").write <<-EOS.undent
+      (add-to-list 'load-path "#{elisp}")
+      (add-to-list 'load-path "#{Formula["homebrew/emacs/gh-emacs"].opt_elisp}")
+      (add-to-list 'load-path "#{Formula["homebrew/emacs/logito"].opt_elisp}")
+      (add-to-list 'load-path "#{Formula["homebrew/emacs/pcache"].opt_elisp}")
+      (load "gist")
+      (print (minibuffer-prompt-width))
+    EOS
+    assert_equal "0", shell_output("emacs -Q --batch -l #{testpath}/test.el").strip
+  end
+end
