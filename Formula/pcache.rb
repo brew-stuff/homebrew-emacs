645de4da331a9e58fa5e18006db9c3af5b0b6341
Alex Dunn <dunn.alex@gmail.com>
2015-11-09 19:33:16 -0800

    pcache 0.3.2
    Closes #61.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/pcache.rb
+++ Formula/pcache.rb
@@ -3,22 +3,9 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class Pcache < EmacsFormula
   desc "Persistent caching for Emacs"
   homepage "https://github.com/sigma/pcache"
+  url "https://github.com/sigma/pcache/archive/v0.3.2.tar.gz"
+  sha256 "5acef749bf2255186b98cfa4f3656037db4b516a2622b94b2acc72b8a0532094"
   head "https://github.com/sigma/pcache.git"
-
-  stable do
-    url "https://github.com/sigma/pcache/archive/v0.3.1.tar.gz"
-    sha256 "7fb9445e943cc9771f16e5853b9f131928a2cfdfc1a5f54454eb5ab415db018b"
-
-    patch do
-      url "https://github.com/sigma/pcache/commit/a3b8e77bc2ae36a1c87ceddbdfec59efe7eda668.diff"
-      sha256 "3ca4317a1addabda7d8a81dd6be999f97808747aa073e97a35fefaa6cd002359"
-    end
-
-    patch do
-      url "https://github.com/sigma/pcache/commit/04ec841cb8430fa022e2c24aa835c2eb6bb5533a.diff"
-      sha256 "49df3934c0cfd95f7e033428c7f4ce5c094b0b65cee248300f0ff66b00fc9a7a"
-    end
-  end
 
   depends_on :emacs
 
