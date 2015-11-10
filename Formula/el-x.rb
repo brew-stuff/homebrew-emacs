ceb1f21d954f192e80552900a4ae823e27366235
Alex Dunn <dunn.alex@gmail.com>
2015-11-09 20:25:30 -0800

    el-x 0.3.1 (new formula)
    Closes #66.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

---
+++ Formula/el-x.rb
@@ -0,0 +1,26 @@
+require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
+
+class ElX < EmacsFormula
+  desc "Emacs Lisp extensions"
+  homepage "https://github.com/sigma/el-x"
+  url "https://github.com/sigma/el-x/archive/v0.3.1.tar.gz"
+  sha256 "734e654b26ae699c41bb8f691fb4ec4d6064c156b02ddf3c2fa98b7dc16a527e"
+  head "https://github.com/sigma/el-x.git"
+
+  depends_on :emacs
+  depends_on "homebrew/emacs/cl-lib" if Emacs.version < 24.3
+
+  def install
+    system "make"
+    elisp.install Dir["lisp/*"]
+  end
+
+  test do
+    (testpath/"test.el").write <<-EOS.undent
+      (add-to-list 'load-path "#{elisp}")
+      (load "el-x")
+      (print (minibuffer-prompt-width))
+    EOS
+    assert_equal "0", shell_output("emacs -Q --batch -l #{testpath}/test.el").strip
+  end
+end
