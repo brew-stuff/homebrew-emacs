ea18215c84543ba4a35a2ff28acf984ea4234458
Alex Dunn <dunn.alex@gmail.com>
2015-11-14 14:23:34 -0800

    el-mock 1.25.1 (new formula)
    Closes #84.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

---
+++ Formula/el-mock.rb
@@ -0,0 +1,25 @@
+require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
+
+class ElMock < EmacsFormula
+  desc "Tiny mock and stub framework in Emacs Lisp"
+  homepage "https://github.com/rejeep/el-mock.el"
+  url "https://github.com/rejeep/el-mock.el/archive/v1.25.1.tar.gz"
+  sha256 "0b8db563fbf9d8a4793afa57c9f59eb44ca07f5de248fff61fb677542c49bc9b"
+  head "https://github.com/rejeep/el-mock.el.git"
+
+  depends_on :emacs
+
+  def install
+    byte_compile "el-mock.el"
+    elisp.install "el-mock.el", "el-mock.elc"
+  end
+
+  test do
+    (testpath/"test.el").write <<-EOS.undent
+      (add-to-list 'load-path "#{elisp}")
+      (load "el-mock")
+      (print (minibuffer-prompt-width))
+    EOS
+    assert_equal "0", shell_output("emacs -Q --batch -l #{testpath}/test.el").strip
+  end
+end
