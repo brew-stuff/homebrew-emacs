cee1a240298b9da98008dbe5e49006eec812e112
Alex Dunn <dunn.alex@gmail.com>
2015-11-18 21:50:49 -0800

    olivetti 1.3.0

--- Formula/olivetti.rb
+++ Formula/olivetti.rb
@@ -3,16 +3,15 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class Olivetti < EmacsFormula
   desc "Emacs minor mode for distraction-free writing"
   homepage "https://github.com/rnkn/olivetti"
-  url "https://github.com/rnkn/olivetti/archive/v1.2.0.tar.gz"
-  sha256 "e94a65f43e22f42be477a1fae028d4192837195b4f2e496a4c62925d47dc4d7d"
+  url "https://github.com/rnkn/olivetti/archive/v1.3.0.tar.gz"
+  sha256 "916f2579ed8d0c06b46ad4c7305ef966cd12c8ee04042eafe9183de4c3bec264"
   head "https://github.com/rnkn/olivetti.git"
 
   depends_on :emacs => "24.4"
 
   def install
     byte_compile "olivetti.el"
-    (share/"emacs/site-lisp/olivetti").install "olivetti.el",
-                                               "olivetti.elc"
+    elisp.install "olivetti.el", "olivetti.elc"
   end
 
   test do
