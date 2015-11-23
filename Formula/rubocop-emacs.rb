680ee555e699a9a7eab3f360a092ed9f409578bb
Alex Dunn <dunn.alex@gmail.com>
2015-11-23 13:51:18 -0800

    rubocop 0.4.0

--- Formula/rubocop-emacs.rb
+++ Formula/rubocop-emacs.rb
@@ -3,6 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class RubocopEmacs < EmacsFormula
   desc "Emacs interface for RuboCop"
   homepage "https://github.com/bbatsov/rubocop-emacs"
+  url "https://github.com/bbatsov/rubocop-emacs/archive/v0.4.0.tar.gz"
+  sha256 "b3dadd72c7f06607576949cbf388c2c62acc42313b02a088a308e6f36807e43a"
   head "https://github.com/bbatsov/rubocop-emacs.git"
 
   depends_on :emacs => "24.1"
@@ -15,6 +17,7 @@ class RubocopEmacs < EmacsFormula
 
   test do
     (testpath/"test.el").write <<-EOS.undent
+      (add-to-list 'load-path "#{Formula["dash-emacs"].opt_elisp}")
       (add-to-list 'load-path "#{elisp}")
       (load "rubocop")
       (print (minibuffer-prompt-width))
