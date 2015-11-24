2d46dea88512b13f4bf3e9adfd445763eaa30305
Alex Dunn <dunn.alex@gmail.com>
2015-11-23 20:00:33 -0800

    async-emacs 1.6

--- Formula/async-emacs.rb
+++ Formula/async-emacs.rb
@@ -3,11 +3,12 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class AsyncEmacs < EmacsFormula
   desc "Emacs library for asynchronous processing"
   homepage "https://github.com/jwiegley/emacs-async"
-  url "https://github.com/jwiegley/emacs-async/archive/v1.5.tar.gz"
-  sha256 "3126a6960796aa1bebe8a169ea5430df6ae5e18a34816c01b578f7c37c4bc216"
+  url "https://github.com/jwiegley/emacs-async/archive/v1.6.tar.gz"
+  sha256 "9962c7bced1fe663ba741a2e85ac9e71a4206ad5c7e47486d75e98e0b7faa7eb"
   head "https://github.com/jwiegley/emacs-async.git"
 
   depends_on :emacs
+  depends_on "homebrew/emacs/cl-lib" if Emacs.version < 24.3
 
   def install
     byte_compile Dir["*.el"]
@@ -18,8 +19,12 @@ class AsyncEmacs < EmacsFormula
     (testpath/"test.el").write <<-EOS.undent
       (add-to-list 'load-path "#{elisp}")
       (load "async")
-      (print (minibuffer-prompt-width))
+      (let ((fut (async-start
+             (lambda () (sleep-for 2) "mkdir brew"))))
+        (shell-command (async-get fut)))
     EOS
-    assert_equal "0", shell_output("emacs -Q --batch -l #{testpath}/test.el").strip
+    system "emacs", "-Q", "--batch", "-l", "#{testpath}/test.el"
+    sleep 3
+    (testpath/"brew").directory?
   end
 end
