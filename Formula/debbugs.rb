ced1cc78b5a4c19d642bff67b5306696a1d65f3b
Alex Dunn <dunn.alex@gmail.com>
2015-11-23 18:55:32 -0800

    debbugs: add test

--- Formula/debbugs.rb
+++ Formula/debbugs.rb
@@ -1,34 +1,26 @@
 require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 
 class Debbugs < EmacsFormula
-  desc "Emacs library for accessing the GNU Bug Tracker"
+  desc "Emacs library for accessing Debbugs SOAP servers"
   homepage "http://elpa.gnu.org/packages/debbugs.html"
   url "http://elpa.gnu.org/packages/debbugs-0.7.tar"
   sha256 "e2ec926e0f6dbdb3569542263f4c561c510e8893e4d1febe6211297547a76f5d"
 
   depends_on :emacs => "22.1"
-  depends_on "homebrew/emacs/soap-client-emacs" => "HEAD" if Emacs.version < 24.1
+  depends_on "homebrew/emacs/soap-client-emacs" if Emacs.version < 24.1
 
   def install
     byte_compile Dir["*.el"]
-    (share/"emacs/site-lisp/debbugs").install Dir["*.el"],
-                                              Dir["*.elc"],
-                                              "Debbugs.wsdl"
-    doc.install "README"
+    elisp.install Dir["*.el"], Dir["*.elc"], "Debbugs.wsdl"
     info.install "debbugs.info"
   end
 
-  def caveats; <<-EOS.undent
-    Add the following to your init file:
-
-    (require 'debbugs-gnu)
-)
-    ;; If you have org-mode, you may want to add instead:
-
-    (require 'debbugs-org)
-    (autoload 'debbugs-org "debbugs-org" "" 'interactive)
-    (autoload 'debbugs-org-search "debbugs-org" "" 'interactive)
-    (autoload 'debbugs-org-bugs "debbugs-org" "" 'interactive)
-  EOS
+  test do
+    (testpath/"test.el").write <<-EOS.undent
+      (add-to-list 'load-path "#{elisp}")
+      (load "debbugs")
+      (print (car debbugs-servers))
+    EOS
+    assert_match "gnu.org", shell_output("emacs -Q --batch -l #{testpath}/test.el").strip
   end
 end
