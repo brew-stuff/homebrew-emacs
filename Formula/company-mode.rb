a4c581f204ce0e2c19825aa32b70fad28b14aa3e
Alex Dunn <dunn.alex@gmail.com>
2015-11-29 17:15:03 -0800

    company-mode: incl. stats-mode as resource, audit

--- Formula/company-mode.rb
+++ Formula/company-mode.rb
@@ -8,6 +8,7 @@ class CompanyMode < EmacsFormula
   head "https://github.com/company-mode/company-mode.git"
 
   option "with-emoji", "Install company-emoji"
+  option "with-statistics", "Include statistical ranking minor mode"
   option "with-web", "Install company-web"
 
   depends_on :emacs => "24.1"
@@ -23,6 +24,11 @@ class CompanyMode < EmacsFormula
     sha256 "51f5c3c43ab6fcb79ea88115b0e773269cc02d56a8dbaec1f83f7fbe3e5f34f8"
   end
 
+  resource "statistics" do
+    url "https://github.com/company-mode/company-statistics/archive/0.2.2.tar.gz"
+    sha256 "31c7f68324f492cbe658f0733841e4de64149e86f821c878c390edb0e840e420"
+  end
+
   resource "web" do
     url "https://github.com/osv/company-web/archive/v0.9.tar.gz"
     sha256 "c5e26ae5eb9f7c57684b24ad9a960fa86df5ab2e2dac14b7b1068f3853a98622"
@@ -32,28 +38,32 @@ class CompanyMode < EmacsFormula
     if build.with? "emoji"
       resource("emoji").stage do
         byte_compile "company-emoji.el"
-        (share/"emacs/site-lisp/company/emoji").install Dir["*.el"],
-                                                        Dir["*.elc"]
+        (elisp/"emoji").install Dir["*.el"], Dir["*.elc"]
+      end
+    end
+
+    if build.with? "statistics"
+      resource("statistics").stage do
+        byte_compile "company-statistics.el"
+        (elisp/"statistics").install Dir["*.el"], Dir["*.elc"]
       end
     end
 
     if build.with? "web"
       resource("web").stage do
         byte_compile Dir["*.el"]
-        (share/"emacs/site-lisp/company/web").install Dir["*.el"],
-                                                      Dir["*.elc"]
+        (elisp/"web").install Dir["*.el"], Dir["*.elc"]
       end
     end
 
     system "make", "test-batch"
     system "make", "compile"
-    (share/"emacs/site-lisp/company").install Dir["company*.el"],
-                                              Dir["company*.elc"]
+    elisp.install Dir["company*.el"], Dir["company*.elc"]
   end
 
   test do
     (testpath/"test.el").write <<-EOS.undent
-      (add-to-list 'load-path "#{share}/emacs/site-lisp/company")
+      (add-to-list 'load-path "#{elisp}")
       (load "company")
       (print (minibuffer-prompt-width))
     EOS
