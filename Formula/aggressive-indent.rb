d7de9e39dff3b5057c05d2f509f11603f0c43034
Alex Dunn <dunn.alex@gmail.com>
2015-12-01 20:34:57 -0800

    aggressive-indent 1.4.2

--- Formula/aggressive-indent.rb
+++ Formula/aggressive-indent.rb
@@ -3,18 +3,16 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class AggressiveIndent < EmacsFormula
   desc "Emacs minor mode to keep code always indented"
   homepage "https://github.com/Malabarba/aggressive-indent-mode"
-  url "https://github.com/Malabarba/aggressive-indent-mode/archive/1.4.1.tar.gz"
-  sha256 "e29f35ee77f421686c50352142a59eeaceaaaf28999691940a47a78732dc1bfe"
+  url "https://github.com/Malabarba/aggressive-indent-mode/archive/1.4.2.tar.gz"
+  sha256 "27791e4cd8ebd7f585ad385bf3e6f87ce9a0f62330affe5a83ed73f2a2f0a542"
   head "https://github.com/Malabarba/aggressive-indent-mode.git"
 
   depends_on :emacs => "24.1"
-  depends_on "homebrew/emacs/names"
   depends_on "homebrew/emacs/cl-lib" if Emacs.version < 24.3
 
   def install
     byte_compile "aggressive-indent.el"
-    (share/"emacs/site-lisp/aggressive-indent").install Dir["*.el"],
-                                                        Dir["*.elc"]
+    elisp.install Dir["*.el"], Dir["*.elc"]
   end
 
   test do
