d41c700c38d42cb725e1d059824eaf4c028e165a
Alex Dunn <dunn.alex@gmail.com>
2015-12-01 18:46:04 -0800

    names 20151201.0

--- Formula/names.rb
+++ Formula/names.rb
@@ -3,19 +3,16 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class Names < EmacsFormula
   desc "Namespacing for Emacs Lisp"
   homepage "https://github.com/Malabarba/names"
-  url "https://github.com/Malabarba/names/archive/20150723.0.tar.gz"
-  sha256 "353c5840828b298e826c3ebac267bcd72fd18988fccc2f50840b4df9022d0691"
+  url "http://elpa.gnu.org/packages/names-20151201.0.tar"
+  sha256 "4d84c4cf981b0d3597f0e94668e1e31352f477b08fd2fdef699ef43480d3558f"
   head "https://github.com/Malabarba/names.git"
 
   depends_on :emacs => "24.1"
   depends_on "homebrew/emacs/cl-lib" if Emacs.version < 24.3
 
   def install
-    # tests don't pass
-    # `make build` compiles in strict mode and so fails
     byte_compile Dir["*.el"]
-    (share/"emacs/site-lisp/names").install Dir["*.el"], Dir["*.elc"]
-    doc.install Dir["*.org"]
+    elisp.install Dir["*.el"], Dir["*.elc"]
   end
 
   test do
