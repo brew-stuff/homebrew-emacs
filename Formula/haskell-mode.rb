90067384358dcbfaf859a7fd168efe20f5356d54
Alex Dunn <dunn.alex@gmail.com>
2015-12-01 20:45:35 -0800

    haskell-mode 13.16
    Closes #120.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/haskell-mode.rb
+++ Formula/haskell-mode.rb
@@ -3,34 +3,26 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class HaskellMode < EmacsFormula
   desc "Emacs major mode for Haskell"
   homepage "https://github.com/haskell/haskell-mode"
-  url "https://github.com/haskell/haskell-mode/archive/v13.14.2.tar.gz"
-  sha256 "5b2577b60be74147af09d90d93bacfac295ad881c8e490761322ac8da417accf"
+  url "https://github.com/haskell/haskell-mode/archive/v13.16.tar.gz"
+  sha256 "6a846e9fb0b4ba354d6d42452cf298d548a0457271cd73fb792c0bf677a13d59"
   head "https://github.com/haskell/haskell-mode.git"
 
-  option "with-html", "Build HTML documentation"
+  deprecated_option "with-html" => "with-docs"
+  option "with-docs", "Build HTML documentation"
 
   depends_on :emacs => "23.1"
-  depends_on "texinfo" if build.with? "html"
+  depends_on "texinfo" if build.with? "docs"
 
   def install
     system "make"
     system "make", "check"
-    (share/"emacs/site-lisp/haskell-mode").install Dir["*.el"],
-                                                   Dir["*.elc"]
-    doc.install "README.md"
+    elisp.install Dir["*.el"], Dir["*.elc"]
     info.install Dir["*.info"]
 
-    if build.with? "html"
+    if build.with? "docs"
       system "make", "doc/html"
       doc.install "doc/html"
     end
-  end
-
-  def caveats; <<-EOS.undent
-    Add the following to your init file:
-
-    (require 'haskell-mode)
-  EOS
   end
 
   test do
