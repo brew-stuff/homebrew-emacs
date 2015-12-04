0f8048c03da826fae24cab12b523e0271cc65fb4
Alex Dunn <dunn.alex@gmail.com>
2015-12-03 17:01:51 -0800

    cider 0.10.0
    Closes #121.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/cider.rb
+++ Formula/cider.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class Cider < EmacsFormula
   desc "Clojure IDE for Emacs"
   homepage "https://github.com/clojure-emacs/cider"
-  url "https://github.com/clojure-emacs/cider/archive/v0.9.1.tar.gz"
-  sha256 "e2ac1e26a936fe7942ebabb96c720d792b8e77a692d7ab445b13da2fa058b233"
+  url "https://github.com/clojure-emacs/cider/archive/v0.10.0.tar.gz"
+  sha256 "517e788977e001f9f6b007c9260d6f6e9a92d4684e9ece0ae909f6564ebc91f7"
   head "https://github.com/clojure-emacs/cider.git"
 
   depends_on :emacs => "24.3"
@@ -23,9 +23,12 @@ class Cider < EmacsFormula
   test do
     (testpath/"test.el").write <<-EOS.undent
       (add-to-list 'load-path "#{elisp}")
-      (add-to-list 'load-path "#{Formula["homebrew/emacs/dash-emacs"].opt_elisp}")
       (add-to-list 'load-path "#{Formula["homebrew/emacs/clojure-mode"].opt_elisp}")
+      (add-to-list 'load-path "#{Formula["homebrew/emacs/dash-emacs"].opt_elisp}")
+      (add-to-list 'load-path "#{Formula["homebrew/emacs/pkg-info"].opt_elisp}")
       (add-to-list 'load-path "#{Formula["homebrew/emacs/queue-emacs"].opt_elisp}")
+      (add-to-list 'load-path "#{Formula["homebrew/emacs/seq"].opt_elisp}")
+      (add-to-list 'load-path "#{Formula["homebrew/emacs/spinner-emacs"].opt_elisp}")
       (load "cider")
       (print cider-version)
     EOS
