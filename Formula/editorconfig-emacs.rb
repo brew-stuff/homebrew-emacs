13ab14e79cf4c0b8f2b5789ff0a52eea8adbe355
Alex Dunn <dunn.alex@gmail.com>
2015-12-06 11:45:23 -0800

    editorconfig-emacs 0.6.1
    Includes Emacs Lisp implementation of core.

--- Formula/editorconfig-emacs.rb
+++ Formula/editorconfig-emacs.rb
@@ -3,14 +3,20 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class EditorconfigEmacs < EmacsFormula
   desc "EditorConfig plugin for emacs"
   homepage "http://editorconfig.org/"
-  url "https://github.com/editorconfig/editorconfig-emacs/archive/v0.5.tar.gz"
-  sha256 "7f4d7876109e2e25ee1beac071f85ed3687f69a1608b49adedbd3207f1357ae2"
+  url "https://github.com/editorconfig/editorconfig-emacs.git",
+      :tag => "v0.6.1",
+      :revision => "3d1e4797ea3f5a1bb6d0ec296f04ce05e6e368b4"
   head "https://github.com/editorconfig/editorconfig-emacs.git"
 
-  depends_on "editorconfig"
+  option "without-editorconfig", "Use the Emacs Lisp implementation of EditorConfig Core"
+
   depends_on :emacs => "24.1"
+  depends_on "cmake" => :build
+  depends_on "editorconfig" => :recommended
+  depends_on "homebrew/emacs/cl-lib" if Emacs.version < 24.3
 
   def install
+    ENV["LC_ALL"] = "en_US.UTF-8"
     system "make"
     system "make", "test"
     elisp.install "editorconfig.el", "editorconfig.elc"
@@ -20,6 +26,7 @@ class EditorconfigEmacs < EmacsFormula
     (testpath/"test.el").write <<-EOS.undent
       (add-to-list 'load-path "#{elisp}")
       (load "editorconfig")
+      (editorconfig-mode 1)
       (editorconfig-set-indentation "space")
       (print (minibuffer-prompt-width))
     EOS
