cf8e57f6aa3207c27fb449e437e7897370e2d52a
Alex Dunn <dunn.alex@gmail.com>
2015-12-08 18:23:11 -0800

    tiny-emacs 0.1.1
    Closes #290.
    
    Signed-off-by: Alex Dunn <adunn@ucsb.edu>

--- Formula/tiny-emacs.rb
+++ Formula/tiny-emacs.rb
@@ -3,31 +3,30 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class TinyEmacs < EmacsFormula
   desc "Emacs package for generating linear ranges"
   homepage "https://github.com/abo-abo/tiny"
-  url "http://elpa.gnu.org/packages/tiny-0.1.tar"
-  sha256 "38e09e8e5f09073c1770508968692312c114a66ab8f3728a9aa4da277f8b3e12"
   head "https://github.com/abo-abo/tiny.git"
 
-  depends_on :emacs
-  depends_on "cask"
+  stable do
+    url "https://elpa.gnu.org/packages/tiny-0.1.1.tar"
+    sha256 "c77a8fc0ee7ccb7cb0a1367031975afd202d8e54ae7620f929c4365cce400fda"
 
-  def install
-    system "make", "test"
-    system "make", "compile"
-    (share/"emacs/site-lisp/tiny").install Dir["*.el"], Dir["*.elc"]
-    doc.install "README.md"
+    patch do
+      url "https://github.com/abo-abo/tiny/commit/d775201a6e14aae03dda032132aa288ca93bddc4.diff"
+      sha256 "ea486e83a803f2c3e0b2eee1fb809e9584b2b8e09a400cc1f6e0ef6efd043ac5"
+    end
   end
 
-  def caveats; <<-EOS.undent
-    Add the following to your init file:
+  depends_on :emacs
+  depends_on "homebrew/emacs/undercover" => :build
 
-    (require 'tiny)
-    (global-set-key (kbd "C-;") 'tiny-expand)
-  EOS
+  def install
+    ert_run_tests "tiny-test.el"
+    system "make", "compile"
+    elisp.install Dir["*.el"], Dir["*.elc"]
   end
 
   test do
     (testpath/"test.el").write <<-EOS.undent
-      (add-to-list 'load-path "#{share}/emacs/site-lisp/tiny")
+      (add-to-list 'load-path "#{elisp}")
       (load "tiny")
       (print (tiny-tokenize "+x2"))
     EOS
