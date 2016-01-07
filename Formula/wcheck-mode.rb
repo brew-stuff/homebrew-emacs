2f3af9c0788d5b23ef77b47359745525de312061
Alex Dunn <dunn.alex@gmail.com>
2016-01-06 18:04:40 -0800

    wcheck-mode 2016.1.5
    Closes #147.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/wcheck-mode.rb
+++ Formula/wcheck-mode.rb
@@ -3,20 +3,26 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class WcheckMode < EmacsFormula
   desc "General-purpose text-checker interface for Emacs"
   homepage "https://github.com/tlikonen/wcheck-mode"
-  url "https://github.com/tlikonen/wcheck-mode/archive/2015.11.29.tar.gz"
-  sha256 "8292ce9a83b5af4be9d568c3f9fcc5be017da522b7c8b7197ab70c9ebb207f33"
+  url "http://elpa.gnu.org/packages/wcheck-mode-2016.1.5.el"
+  sha256 "6349832400070833d697c2baf251450a452a54414f32edc9ef57ddc0b518ad2b"
   head "https://github.com/tlikonen/wcheck-mode.git"
 
   depends_on :emacs
 
   def install
-    system "make", "elc"
+    if build.stable?
+      mv "wcheck-mode-#{version}.el", "wcheck-mode.el"
+      byte_compile "wcheck-mode.el"
+    else
+      system "make", "elc"
+    end
+
     elisp.install "wcheck-mode.el", "wcheck-mode.elc"
   end
 
   test do
     (testpath/"test.el").write <<-EOS.undent
-      (add-to-list 'load-path "#{share}/emacs/site-lisp/wcheck-mode")
+      (add-to-list 'load-path "#{elisp}")
       (load "wcheck-mode")
       (print (minibuffer-prompt-width))
     EOS
