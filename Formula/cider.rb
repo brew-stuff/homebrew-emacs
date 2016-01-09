7772ed91c55c5e604e875702e55cff74eb04f343
Alex Dunn <dunn.alex@gmail.com>
2016-01-09 07:32:40 -0800

    cider 0.10.1

--- Formula/cider.rb
+++ Formula/cider.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class Cider < EmacsFormula
   desc "Clojure IDE for Emacs"
   homepage "https://github.com/clojure-emacs/cider"
-  url "https://github.com/clojure-emacs/cider/archive/v0.10.0.tar.gz"
-  sha256 "517e788977e001f9f6b007c9260d6f6e9a92d4684e9ece0ae909f6564ebc91f7"
+  url "https://github.com/clojure-emacs/cider/archive/v0.10.1.tar.gz"
+  sha256 "3891d16d2eeea5653d23e64cecdb644f486bef282ab57dd21a5fdfdc71c729d9"
   head "https://github.com/clojure-emacs/cider.git"
 
   depends_on :emacs => "24.3"
