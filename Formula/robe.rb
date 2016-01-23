4805750a2181c97a69106e6145d1d8c9cf010043
Alex Dunn <adunn@ucsb.edu>
2016-01-22 16:20:12 -0800

    robe 0.7.9

--- Formula/robe.rb
+++ Formula/robe.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class Robe < EmacsFormula
   desc "Navigation, documentation lookup and completion for Ruby"
   homepage "https://github.com/dgutov/robe"
-  url "https://github.com/dgutov/robe/archive/0.7.8.tar.gz"
-  sha256 "ed63fcff0cde46d91780f90577bca26d15d93b8905a5ce8c716a69289eca76f8"
+  url "https://github.com/dgutov/robe/archive/0.7.9.tar.gz"
+  sha256 "f6b471dd4af841c3b79c14a0db57ba0a87209d8ca7d9845bbcfc82b20e9a79f4"
   head "https://github.com/dgutov/robe.git"
 
   depends_on :emacs => "24.3"
