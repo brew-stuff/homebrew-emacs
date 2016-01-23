757b0f57a938be9b644ead533a0f28da98d8f611
Alex Dunn <dunn.alex@gmail.com>
2016-01-22 18:28:02 -0800

    ada-mode 5.1.9

--- Formula/ada-mode.rb
+++ Formula/ada-mode.rb
@@ -3,12 +3,13 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class AdaMode < EmacsFormula
   desc "Emacs major mode for editing Ada sources"
   homepage "http://stephe-leake.org/emacs/ada-mode/emacs-ada-mode.html"
-  url "http://stephe-leake.org/emacs/ada-mode/org.emacs.ada-mode-5.1.8.tar.gz"
-  sha256 "da22247244cafe9395189fcca13667448fbc6a0fd52fb0cbcd8c3b0545aaf092"
+  url "http://elpa.gnu.org/packages/ada-mode-5.1.9.tar"
+  sha256 "878eb97393f497d17f7bd1d93d55a85736b9955f2d1231d7dd8409e16ff21c12"
 
   option "without-reference", "Build without the reference manual"
 
   depends_on :emacs => "24.2"
+  depends_on "homebrew/emacs/wisi-emacs"
 
   resource "reference" do
     url "http://stephe-leake.org/ada/arm_info-2012.2.tar.gz"
@@ -23,32 +24,22 @@ class AdaMode < EmacsFormula
   def install
     # there's `make byte-compile` but it uses package.el
     byte_compile Dir["*.el"]
-    (share/"emacs/site-lisp/ada-mode").install Dir["*.el"],
-                                               Dir["*.elc"]
+    elisp.install Dir["*.el"], Dir["*.elc"]
+
     info.install Dir["*.info"]
     if build.with? "reference"
       resource("reference").stage { info.install Dir["*"] }
     end
 
     resource("mode-keys").stage do
-      byte_compile "ada-mode-keys.el"
-      (prefix/"contrib").install "ada-mode-keys.el",
-                                 "ada-mode-keys.elc"
+      (prefix/"contrib").install "ada-mode-keys.el"
     end
-  end
-
-  def caveats; <<-EOS.undent
-    Example settings have been installed in #{prefix}/contrib
-
-    Add the following to your init file:
-
-    (require 'ada-mode)
-  EOS
   end
 
   test do
     (testpath/"test.el").write <<-EOS.undent
-      (add-to-list 'load-path "#{share}/emacs/site-lisp/ada-mode")
+      (add-to-list 'load-path "#{elisp}")
+      (add-to-list 'load-path "#{Formula["wisi-emacs"].opt_elisp}")
       (load "ada-mode")
       (print (ada-mode-version))
     EOS
