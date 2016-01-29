cdf7e5a052272521edd42058d8504a6931649a06
Alex Dunn <adunn@ucsb.edu>
2016-01-29 14:30:37 -0800

    slime 2.16

--- Formula/slime.rb
+++ Formula/slime.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class Slime < EmacsFormula
   desc "Emacs package for interactive programming in Lisp"
   homepage "http://common-lisp.net/project/slime/"
-  url "https://github.com/slime/slime/archive/v2.15.tar.gz"
-  sha256 "bad1851ce99b7f40b0593dc91842b261b526e0af797e660184159dda05355f50"
+  url "https://github.com/slime/slime/archive/v2.16.tar.gz"
+  sha256 "5390434c87d789f49c5976270c50078613b957a93aaa2ce962bc8d45247dafff"
   head "https://github.com/slime/slime.git"
 
   depends_on :emacs => "23.4"
@@ -14,14 +14,13 @@ class Slime < EmacsFormula
     system "make"
     system "make", "compile-swank"
     system "make", "contrib-compile"
-    (share/"emacs/site-lisp/slime").install Dir["*.lisp"],
-                                            Dir["*.el"], Dir["*.elc"],
-                                            "lib", "swank", "contrib"
+    elisp.install Dir["*.lisp"], Dir["*.el"], Dir["*.elc"],
+                  "lib", "swank", "contrib"
   end
 
   test do
     (testpath/"test.el").write <<-EOS.undent
-      (add-to-list 'load-path "#{share}/emacs/site-lisp/slime")
+      (add-to-list 'load-path "#{elisp}")
       (load "slime-autoloads")
       (print (minibuffer-prompt-width))
     EOS
