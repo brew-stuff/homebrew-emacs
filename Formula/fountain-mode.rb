9c2cf1bfe3c56a7175684c87c0e9020551e14fed
Alex Dunn <dunn.alex@gmail.com>
2016-02-07 19:18:03 -0800

    fountain-mode 1.5.1

--- Formula/fountain-mode.rb
+++ Formula/fountain-mode.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class FountainMode < EmacsFormula
   desc "Major mode for the Fountain screenwriting syntax"
   homepage "https://github.com/rnkn/fountain-mode"
-  url "https://github.com/rnkn/fountain-mode/archive/v1.5.0.tar.gz"
-  sha256 "433f41332f33265a9fe30215f8e18419c9c11aa640326b0c1d5b09cdb35b1c4f"
+  url "https://github.com/rnkn/fountain-mode/archive/v1.5.1.tar.gz"
+  sha256 "2b2862bb5234d3e81c1c45e67e9908803e5bc36a6fc12304b2bde60c99f50b65"
   head "https://github.com/rnkn/fountain-mode.git"
 
   depends_on :emacs => "24.4"
@@ -12,27 +12,17 @@ class FountainMode < EmacsFormula
 
   def install
     byte_compile "fountain-mode.el"
-    (share/"emacs/site-lisp/fountain-mode").install "fountain-mode.el",
-                                                    "fountain-mode.elc"
-    doc.install "README.md"
-  end
-
-  def caveats; <<-EOS.undent
-    Add the following to your init file:
-
-    (require 'fountain-mode)
-    (add-to-list 'auto-mode-alist '("\\.fountain$" . fountain-mode))
-  EOS
+    elisp.install "fountain-mode.el", "fountain-mode.elc"
   end
 
   test do
     (testpath/"test.el").write <<-EOS.undent
-      (add-to-list 'load-path "#{share}/emacs/site-lisp/fountain-mode")
-      (add-to-list 'load-path "#{Formula["homebrew/emacs/s"].opt_share}/emacs/site-lisp/s")
+      (add-to-list 'load-path "#{elisp}")
+      (add-to-list 'load-path "#{Formula["homebrew/emacs/s"].opt_elisp}")
       (load "fountain-mode")
       (print (fountain-export-bold "Homebrew: **the** Brewing"))
     EOS
-    assert_equal "\"Homebrew: <strong>the</strong> Brewing\"",
+    assert_equal '"Homebrew: <strong>the</strong> Brewing"',
                  shell_output("emacs -Q --batch -l #{testpath}/test.el").strip
   end
 end
