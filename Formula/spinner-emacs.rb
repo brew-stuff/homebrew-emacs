f8da1bd74cd313ddc41de40982e68f98e18ab250
Alex Dunn <adunn@ucsb.edu>
2016-02-09 13:05:16 -0800

    spinner-emacs 1.7

--- Formula/spinner-emacs.rb
+++ Formula/spinner-emacs.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class SpinnerEmacs < EmacsFormula
   desc "Emacs library for spinners and progress bars"
   homepage "https://github.com/Malabarba/spinner.el"
-  url "http://elpa.gnu.org/packages/spinner-1.5.el"
-  sha256 "17d581df5a8c3fbdce97627a37e6d4275de0203d1d054a2b1f59b13b084f4d0f"
+  url "http://elpa.gnu.org/packages/spinner-1.7.el"
+  sha256 "0e2af4e02d10647d7d0d3c8a989a91730d7e26dc4d0c8881ebe4c46a449ee8f8"
   head "https://github.com/Malabarba/spinner.el.git"
 
   depends_on :emacs
