b54cf2b13bd72ea5285eab5a7c944c87d2589ddd
Alex Dunn <dunn.alex@gmail.com>
2016-02-17 07:01:58 -0800

    fountain-mode 2.0.0

--- Formula/fountain-mode.rb
+++ Formula/fountain-mode.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class FountainMode < EmacsFormula
   desc "Major mode for the Fountain screenwriting syntax"
   homepage "https://github.com/rnkn/fountain-mode"
-  url "https://github.com/rnkn/fountain-mode/archive/v1.5.1.tar.gz"
-  sha256 "2b2862bb5234d3e81c1c45e67e9908803e5bc36a6fc12304b2bde60c99f50b65"
+  url "https://github.com/rnkn/fountain-mode/archive/v2.0.0.tar.gz"
+  sha256 "9bb7193c6648a519461aa53f9ca6b118f60f57f9b7843cc87fb5c97e24b14f79"
   head "https://github.com/rnkn/fountain-mode.git"
 
   depends_on :emacs => "24.4"
@@ -20,9 +20,8 @@ class FountainMode < EmacsFormula
       (add-to-list 'load-path "#{elisp}")
       (add-to-list 'load-path "#{Formula["homebrew/emacs/s"].opt_elisp}")
       (load "fountain-mode")
-      (print (fountain-export-bold "Homebrew: **the** Brewing"))
+      (print fountain-version)
     EOS
-    assert_equal '"Homebrew: <strong>the</strong> Brewing"',
-                 shell_output("emacs -Q --batch -l #{testpath}/test.el").strip
+    assert_equal "\"#{version}\"", shell_output("emacs -Q --batch -l #{testpath}/test.el").strip
   end
 end
