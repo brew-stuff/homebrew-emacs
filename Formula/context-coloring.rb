cec840ecd5aa230369ee720d44517df6b589b379
Alex Dunn <dunn.alex@gmail.com>
2016-02-24 17:57:50 -0800

    context-coloring 7.2.1

--- Formula/context-coloring.rb
+++ Formula/context-coloring.rb
@@ -3,17 +3,17 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class ContextColoring < EmacsFormula
   desc "Emacs mode to color JS and Elisp code by scope"
   homepage "https://github.com/jacksonrayhamilton/context-coloring"
-  url "https://github.com/jacksonrayhamilton/context-coloring/archive/v7.2.0.tar.gz"
-  sha256 "e16a890a070addfc857e5d6ea3b60cb9bd7d447cb123e82a34126f948168beec"
+  url "https://github.com/jacksonrayhamilton/context-coloring/archive/v7.2.1.tar.gz"
+  sha256 "81265c908086cfd429b11e7a92f009dd77fa49c4437485c1c7e4ee8c333010ad"
   head "https://github.com/jacksonrayhamilton/context-coloring.git"
 
   depends_on :emacs => "24.3"
-  depends_on "cask"
   depends_on "homebrew/emacs/js2-mode"
+  depends_on "homebrew/emacs/undercover" => :build
 
   def install
-    system "make", "test", "CASK=#{Formula["cask"].bin}/cask"
-    system "make", "compile", "CASK=#{Formula["cask"].bin}/cask"
+    ert_run_tests Dir["test/*.el"]
+    byte_compile Dir["*.el"]
     elisp.install "context-coloring.el", "context-coloring.elc"
   end
 
