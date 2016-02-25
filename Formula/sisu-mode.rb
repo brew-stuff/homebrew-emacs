c407c8b1ed3d335ff8d0799eb39b8eb078bf0510
Alex Dunn <dunn.alex@gmail.com>
2016-02-24 18:00:23 -0800

    sisu-mode 7.1.8

--- Formula/sisu-mode.rb
+++ Formula/sisu-mode.rb
@@ -3,28 +3,20 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class SisuMode < EmacsFormula
   desc "Emacs major mode for editing SiSU markup"
   homepage "http://elpa.gnu.org/packages/sisu-mode.html"
-  url "http://elpa.gnu.org/packages/sisu-mode-3.0.3.el"
-  sha256 "7968afd4acddd75a79cff197fe494b86e7fe5b3840005fd370a3f4d1a383c92b"
+  url "http://elpa.gnu.org/packages/sisu-mode-7.1.8.el"
+  sha256 "d282c04aa6cdee1e7726e5c0b3f002d22507181f90272adb69e1d99f8937fa8b"
 
   depends_on :emacs
 
   def install
     mv "sisu-mode-#{version}.el", "sisu-mode.el"
     byte_compile "sisu-mode.el"
-    (share/"emacs/site-lisp/sisu-mode").install "sisu-mode.el",
-                                                "sisu-mode.elc"
-  end
-
-  def caveats; <<-EOS.undent
-    Add the following to your init file:
-
-    (require 'sisu-mode)
-  EOS
+    elisp.install "sisu-mode.el", "sisu-mode.elc"
   end
 
   test do
     (testpath/"test.el").write <<-EOS.undent
-      (add-to-list 'load-path "#{share}/emacs/site-lisp/sisu-mode")
+      (add-to-list 'load-path "#{elisp}")
       (load "sisu-mode")
       (sisu-mode)
       (print (minibuffer-prompt-width))
