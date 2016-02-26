2952fb0243de5d10cb3a579bc2999630d19a5c6a
Alex Dunn <adunn@ucsb.edu>
2016-02-26 15:23:55 -0800

    circe 2.2

--- Formula/circe.rb
+++ Formula/circe.rb
@@ -3,16 +3,15 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class Circe < EmacsFormula
   desc "Emacs IRC client"
   homepage "https://github.com/jorgenschaefer/circe"
-  url "https://github.com/jorgenschaefer/circe/archive/v2.1.tar.gz"
-  sha256 "b70b550f6fcc01616e0ca9f9884b6726a07c4a0359f7dbdbd86ce963eb375718"
+  url "https://github.com/jorgenschaefer/circe/archive/v2.2.tar.gz"
+  sha256 "bc756d4adea7a50d7d570ae8fa893c47cd1bfde55c8f91b95213f184f0f2fc08"
   head "https://github.com/jorgenschaefer/circe.git"
 
   depends_on :emacs
-  depends_on "cask"
   depends_on "homebrew/emacs/cl-lib" if Emacs.version < 24.3
 
   def install
-    system "scripts/compile"
+    byte_compile Dir["*.el"]
     elisp.install Dir["*.el"], Dir["*.elc"]
   end
 
