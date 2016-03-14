26b41d83c9af06e0d2a5d5ed63344bebaac0495a
Alex Dunn <adunn@ucsb.edu>
2016-03-14 14:22:32 -0700

    elisp-bug-hunter 1.1
    Closes #233.

--- Formula/elisp-bug-hunter.rb
+++ Formula/elisp-bug-hunter.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class ElispBugHunter < EmacsFormula
   desc "Emacs library for debugging configuration files"
   homepage "https://github.com/Malabarba/elisp-bug-hunter"
-  url "http://elpa.gnu.org/packages/bug-hunter-1.0.1.el"
-  sha256 "2d797e2a064e990af05d1dcf71a208920d79850162f656c40c2c012f48791730"
+  url "https://github.com/Malabarba/elisp-bug-hunter/archive/1.1.tar.gz"
+  sha256 "ddc1704f8c5f84f270138c85e710ad81e7d3b1424719bdcf5c3e95a250560ce8"
   head "https://github.com/Malabarba/elisp-bug-hunter.git"
 
   depends_on :emacs => "24.1"
@@ -12,8 +12,7 @@ class ElispBugHunter < EmacsFormula
   depends_on "homebrew/emacs/cl-lib" if Emacs.version < 24.3
 
   def install
-    mv "bug-hunter-#{version}.el", "bug-hunter.el" if build.stable?
-    ert_run_tests "bug-hunter-test.el" if build.head?
+    ert_run_tests "bug-hunter-test.el"
 
     byte_compile "bug-hunter.el"
     elisp.install "bug-hunter.el", "bug-hunter.elc"
