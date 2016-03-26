fbd4c4b94a1d08bc2a61269165a3e998920917f5
Alex Dunn <dunn.alex@gmail.com>
2016-03-26 07:40:36 -0700

    metar-emacs 0.2

--- Formula/metar-emacs.rb
+++ Formula/metar-emacs.rb
@@ -1,10 +1,10 @@
 require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 
 class MetarEmacs < EmacsFormula
-  desc "Emacs package for accessing METAR weather information"
-  homepage "http://elpa.gnu.org/packages/metar.html"
-  url "http://elpa.gnu.org/packages/metar-0.1.el"
-  sha256 "28c4e562ee7a98eaa0b57f92c34b1f095dafb7e68b9cd2552f5008a0eff73f69"
+  desc "Emacs package for accessing METAR weather info"
+  homepage "https://elpa.gnu.org/packages/metar.html"
+  url "https://elpa.gnu.org/packages/metar-0.2.el"
+  sha256 "6e6a05b4a0324cc59fba8860bddd5e2a77a88546f6ca2900efd1404ad3c1df65"
 
   depends_on :emacs
   depends_on "homebrew/emacs/cl-lib" if Emacs.version < 24.3
@@ -12,20 +12,12 @@ class MetarEmacs < EmacsFormula
   def install
     mv "metar-#{version}.el", "metar.el"
     byte_compile "metar.el"
-    (share/"emacs/site-lisp/metar").install "metar.el",
-                                            "metar.elc"
-  end
-
-  def caveats; <<-EOS.undent
-    Add the following to your init file:
-
-    (require 'metar)
-  EOS
+    elisp.install "metar.el", "metar.elc"
   end
 
   test do
     (testpath/"test.el").write <<-EOS.undent
-      (add-to-list 'load-path "#{share}/emacs/site-lisp/metar")
+      (add-to-list 'load-path "#{elisp}")
       (load "metar")
       (print (metar-get-record "SARC"))
     EOS
