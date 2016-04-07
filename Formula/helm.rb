98ea619425d53d1c76a4d55c1996775baba2027a
Alex Dunn <dunn.alex@gmail.com>
2016-04-07 12:34:08 -0700

    helm 1.9.4
    Closes #253.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/helm.rb
+++ Formula/helm.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class Helm < EmacsFormula
   desc "Emacs completion and selection narrowing framework"
   homepage "https://emacs-helm.github.io/helm/"
-  url "https://github.com/emacs-helm/helm/archive/v1.9.2.tar.gz"
-  sha256 "7dda135eeb3e19b0bbeebbc10a3532afe8b4773e5d18d75cc5c3afa660d86e12"
+  url "https://github.com/emacs-helm/helm/archive/v1.9.4.tar.gz"
+  sha256 "653c28fcc7e811366c4b06a537dbecb1adff90805da64fe02be5622ebbac4fba"
   head "https://github.com/emacs-helm/helm.git"
 
   depends_on :emacs => "24.3"
