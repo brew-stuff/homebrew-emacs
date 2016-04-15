1bd209e990a9f78fb827fb14e9bd24bb9f0b4da4
Alex Dunn <dunn.alex@gmail.com>
2016-04-15 12:04:49 -0700

    coffee-mode 0.6.3

--- Formula/coffee-mode.rb
+++ Formula/coffee-mode.rb
@@ -3,19 +3,17 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class CoffeeMode < EmacsFormula
   desc "Emacs major mode for CoffeeScript"
   homepage "https://github.com/defunkt/coffee-mode"
-  url "https://github.com/defunkt/coffee-mode/archive/v0.6.2.tar.gz"
-  sha256 "caedf2077e89a7ea3d3984f7a15df6b193a0fe939678fd55d6e2fc7f101046f6"
+  url "https://github.com/defunkt/coffee-mode/archive/v0.6.3.tar.gz"
+  sha256 "7aa8e5858f8aa1de77ca6d2bf829d1f0b7db656905108426e53d03d8db4104fc"
   head "https://github.com/defunkt/coffee-mode.git"
 
   depends_on :emacs => "24.1"
-  depends_on "cask"
   depends_on "homebrew/emacs/cl-lib" if Emacs.version < 24.3
 
   def install
-    system "make", "test"
+    ert_run_tests Dir["test/*.el"]
     byte_compile "coffee-mode.el"
-    (share/"emacs/site-lisp/coffee-mode").install "coffee-mode.el",
-                                                  "coffee-mode.elc"
+    elisp.install "coffee-mode.el", "coffee-mode.elc"
     doc.install "examples"
   end
 
