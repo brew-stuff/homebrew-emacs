c7c0517bfc2da966078c49a64e0f76942fdf8c5b
Alex Dunn <dunn.alex@gmail.com>
2016-04-15 11:57:41 -0700

    buttercup 1.5
    Closes #256.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/buttercup.rb
+++ Formula/buttercup.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class Buttercup < EmacsFormula
   desc "Behavior-driven Emacs Lisp testing"
   homepage "https://github.com/jorgenschaefer/emacs-buttercup/"
-  url "https://github.com/jorgenschaefer/emacs-buttercup/archive/v1.4.tar.gz"
-  sha256 "415e8f41e30d9f2fd08a4f3f04cd257efec7e112f973c4cdc616b9124fd54f9e"
+  url "https://github.com/jorgenschaefer/emacs-buttercup/archive/v1.5.tar.gz"
+  sha256 "d694a00de0828b4c8c554ed394076e37ba32e67e8bf6afb779abd7fc4baa2757"
   head "https://github.com/jorgenschaefer/emacs-buttercup.git"
 
   depends_on :emacs
