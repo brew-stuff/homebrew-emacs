ee7d3f0f8cb90f41aa0f50adbde8e8740ff7f958
Alex Dunn <dunn.alex@gmail.com>
2016-04-22 16:55:33 -0700

    csv-mode 1.6
    Closes #262.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/csv-mode.rb
+++ Formula/csv-mode.rb
@@ -2,9 +2,9 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 
 class CsvMode < EmacsFormula
   desc "Emacs major mode for editing delimited-field files"
-  homepage "https://sites.google.com/site/fjwcentaur/emacs"
-  url "http://elpa.gnu.org/packages/csv-mode-1.5.el"
-  sha256 "ac872ed07b18869165a2848c43b06b36fa73dce5f199ac79123a55b3f232acb6"
+  homepage "https://elpa.gnu.org/packages/csv-mode.html"
+  url "https://elpa.gnu.org/packages/csv-mode-1.6.el"
+  sha256 "caac29cb7a1abe9da08bd6be32155acbd1bb3c8e721a777529d95e1f94c506ed"
   head "http://git.savannah.gnu.org/cgit/emacs/elpa.git/plain/packages/csv-mode/csv-mode.el"
 
   depends_on :emacs
@@ -13,17 +13,7 @@ class CsvMode < EmacsFormula
     mv "csv-mode-#{version}.el", "csv-mode.el" if build.stable?
 
     byte_compile "csv-mode.el"
-    (share/"emacs/site-lisp/csv-mode").install Dir["*.el"],
-                                               Dir["*.elc"]
-  end
-
-  def caveats; <<-EOS.undent
-    Add the following to your init file:
-
-      (add-to-list 'auto-mode-alist '("\\.[Cc][Ss][Vv]\\'" . csv-mode))
-      (autoload 'csv-mode "csv-mode"
-        "Major mode for editing comma-separated value files." t)
-    EOS
+    elisp.install Dir["*.el"], Dir["*.elc"]
   end
 
   test do
