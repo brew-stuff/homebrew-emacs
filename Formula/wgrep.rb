00582580ae87cfbc04cf0098c8019102d08c3a74
Alex Dunn <dunn.alex@gmail.com>
2016-06-25 16:32:18 -0700

    wgrep 2.1.10

--- Formula/wgrep.rb
+++ Formula/wgrep.rb
@@ -3,6 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class Wgrep < EmacsFormula
   desc "Writeable grep buffers for Emacs"
   homepage "https://github.com/mhayashi1120/Emacs-wgrep"
+  url "https://github.com/mhayashi1120/Emacs-wgrep/archive/2.1.10.tar.gz"
+  sha256 "819219bafe76fa8b5f3e5eb50c46e23c0d47ce0c5d84ae9b791d9cacaebf4be4"
   head "https://github.com/mhayashi1120/Emacs-wgrep.git"
 
   depends_on :emacs => "22.1"
@@ -11,9 +13,7 @@ class Wgrep < EmacsFormula
     system "make", "compile"
     system "make", "check"
 
-    (share/"emacs/site-lisp/wgrep").install Dir["*.el"],
-                                            Dir["*.elc"]
-    doc.install "README.md"
+    elisp.install Dir["*.el"], Dir["*.elc"]
   end
 
   test do
