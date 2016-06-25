dc1f70ec4b8e1716ab3c22086b8fe09049f02844
Alex Dunn <dunn.alex@gmail.com>
2016-06-25 16:36:19 -0700

    xcscope 1.2
    Closes #332.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/xcscope.rb
+++ Formula/xcscope.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class Xcscope < EmacsFormula
   desc "cscope interface for (X)Emacs"
   homepage "https://github.com/dkogan/xcscope.el"
-  url "https://github.com/dkogan/xcscope.el/archive/v1.1.tar.gz"
-  sha256 "f4458437d9cfe02299eeca56c760238c6909a4c0eab4677a578596920c02be6c"
+  url "https://github.com/dkogan/xcscope.el/archive/v1.2.tar.gz"
+  sha256 "47e05cf03e9f460426de6d1bec6871bec90853d5f599edbdb0e3f0428b59a197"
   head "https://github.com/dkogan/xcscope.el.git"
 
   depends_on :emacs
@@ -12,17 +12,7 @@ class Xcscope < EmacsFormula
 
   def install
     byte_compile "xcscope.el"
-    (share/"emacs/site-lisp/xcscope").install "xcscope.el",
-                                              "xcscope.elc"
-    doc.install "README.org"
-  end
-
-  def caveats; <<-EOS.undent
-    Add the following to your init file:
-
-    (require 'xcscope)
-    (cscope-setup)
-  EOS
+    elisp.install "xcscope.el", "xcscope.elc"
   end
 
   test do
