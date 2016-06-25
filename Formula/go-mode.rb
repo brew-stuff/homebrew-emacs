eddb8af4ef62cc1b525a4ae48cff98ed192abc49
Alex Dunn <dunn.alex@gmail.com>
2016-06-25 15:35:47 -0700

    go-mode 1.4.0

--- Formula/go-mode.rb
+++ Formula/go-mode.rb
@@ -3,28 +3,22 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class GoMode < EmacsFormula
   desc "Emacs major mode for editing Go"
   homepage "https://github.com/dominikh/go-mode.el"
-  url "https://github.com/dominikh/go-mode.el/archive/v1.3.1.tar.gz"
-  sha256 "5a6dc7e6c2129ab7eaa205286ada46b0eba2206829461c0a667f256b2fb35570"
+  url "https://github.com/dominikh/go-mode.el/archive/v1.4.0.tar.gz"
+  sha256 "31365aa8b220ca67e6fb24fa197e99121a6db815690df731df2f56915805935d"
   head "https://github.com/dominikh/go-mode.el"
 
   depends_on :emacs
+  depends_on "homebrew/emacs/cl-lib" if Emacs.version < 24.3
 
   def install
     byte_compile Dir["*.el"]
-    (share/"emacs/site-lisp/go-mode").install Dir["*.el"], Dir["*.elc"]
-  end
-
-  def caveats; <<-EOS.undent
-    Add the following to your init file:
-
-    (require 'go-mode-autoloads)
-  EOS
+    elisp.install Dir["*.el"], Dir["*.elc"]
   end
 
   test do
     (testpath/"test.el").write <<-EOS.undent
-      (add-to-list 'load-path "#{share}/emacs/site-lisp/go-mode")
-      (load "go-mode-autoloads")
+      (add-to-list 'load-path "#{elisp}")
+      (load "go-mode")
       (print (minibuffer-prompt-width))
     EOS
     assert_equal "0", shell_output("emacs -Q --batch -l #{testpath}/test.el").strip
