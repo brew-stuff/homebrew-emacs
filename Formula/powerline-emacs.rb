2287b7c84693da5ad83e676d19b0f2da0ea8ed0d
Alex Dunn <dunn.alex@gmail.com>
2016-07-03 12:27:00 -0700

    powerline-emacs 2.4 (new formula)
    Closes #343.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

---
+++ Formula/powerline-emacs.rb
@@ -0,0 +1,27 @@
+require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
+
+class PowerlineEmacs < EmacsFormula
+  desc "Emacs version of the Vim powerline"
+  homepage "https://github.com/milkypostman/powerline"
+  url "https://github.com/milkypostman/powerline/archive/2.4.tar.gz"
+  sha256 "cb4fff38648913f865c7085c9ab9708468940c3d05a8aee1b1d77fb96630c0c5"
+  head "https://github.com/milkypostman/powerline.git"
+
+  depends_on :emacs
+  depends_on "homebrew/emacs/cl-lib" if Emacs.version < 24.3
+
+  def install
+    byte_compile Dir["*.el"]
+    elisp.install Dir["*.el"], Dir["*.elc"]
+  end
+
+  test do
+    (testpath/"test.el").write <<-EOS.undent
+      (add-to-list 'load-path "#{elisp}")
+      (load "powerline")
+      (powerline-default-theme)
+      (print (minibuffer-prompt-width))
+    EOS
+    assert_equal "0", shell_output("emacs -Q --batch -l #{testpath}/test.el").strip
+  end
+end
