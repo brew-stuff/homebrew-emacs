e8b0bc3a7f9d0b52883907a1db6d0190d4a92a0e
Alhadis <gardnerjohng@gmail.com>
2016-07-06 16:23:46 +1000

    Fix escaped backslashes in rust-mode's caveat code

--- Formula/rust-mode.rb
+++ Formula/rust-mode.rb
@@ -20,7 +20,7 @@ class RustMode < EmacsFormula
     Add the following to your init file:
 
     (autoload 'rust-mode "rust-mode" nil t)
-    (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+    (add-to-list 'auto-mode-alist '("\\\\.rs\\\\'" . rust-mode))
   EOS
   end
 
