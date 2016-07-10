020b88502e38a0b4065dfa4690895216c96559c5
Alex Dunn <dunn.alex@gmail.com>
2016-07-10 10:50:49 -0700

    oauth2-emacs 0.11

--- Formula/oauth2-emacs.rb
+++ Formula/oauth2-emacs.rb
@@ -2,22 +2,21 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 
 class Oauth2Emacs < EmacsFormula
   desc "Emacs library for OAuth 2.0"
-  homepage "http://elpa.gnu.org/packages/oauth2.html"
-  url "http://elpa.gnu.org/packages/oauth2-0.10.el"
-  sha256 "ca7ae1ca38cc2ea6c541e555a651308596dd2de8ad969638f6e03684d6aa9d66"
+  homepage "https://elpa.gnu.org/packages/oauth2.html"
+  url "https://elpa.gnu.org/packages/oauth2-0.11.el"
+  sha256 "b76a4c5792292d3e3b5616e4f38735601e2f0ac5f59006dd5ecbeaaf6462b379"
 
   depends_on :emacs
 
   def install
     mv "oauth2-#{version}.el", "oauth2.el"
     byte_compile "oauth2.el"
-    (share/"emacs/site-lisp/oauth2").install "oauth2.el",
-                                             "oauth2.elc"
+    elisp.install "oauth2.el", "oauth2.elc"
   end
 
   test do
     (testpath/"test.el").write <<-EOS.undent
-      (add-to-list 'load-path "#{share}/emacs/site-lisp/oauth2")
+      (add-to-list 'load-path "#{elisp}")
       (load "oauth2")
       (print (minibuffer-prompt-width))
     EOS
