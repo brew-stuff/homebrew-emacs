24c93f14122855c2d8a487c63543732a72d538d3
Alex Dunn <dunn.alex@gmail.com>
2016-07-11 17:58:51 -0700

    fountain-mode 2.1.1

--- Formula/fountain-mode.rb
+++ Formula/fountain-mode.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class FountainMode < EmacsFormula
   desc "Major mode for the Fountain screenwriting syntax"
   homepage "https://github.com/rnkn/fountain-mode"
-  url "https://github.com/rnkn/fountain-mode/archive/v2.1.0.tar.gz"
-  sha256 "08586da698f48b00937b7a9a43a33cb9e7079166608b29749d206f8a60e7edc0"
+  url "https://github.com/rnkn/fountain-mode/archive/v2.1.1.tar.gz"
+  sha256 "2932c8fcd093f35f0670835ab48c10e0c05722b02a12fc0f4d4a8e435bd94b58"
   head "https://github.com/rnkn/fountain-mode.git"
 
   depends_on :emacs => "24.4"
