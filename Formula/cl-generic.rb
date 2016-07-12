ec39c7a46c3ba39e94d13c81189f9143e388a75e
Alex Dunn <dunn.alex@gmail.com>
2016-07-12 06:36:51 -0700

    cl-generic 0.3

--- Formula/cl-generic.rb
+++ Formula/cl-generic.rb
@@ -2,9 +2,9 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 
 class ClGeneric < EmacsFormula
   desc "Forward compatibility library for Emacs 24 and below"
-  homepage "http://elpa.gnu.org/packages/cl-generic.html"
-  url "http://elpa.gnu.org/packages/cl-generic-0.2.el"
-  sha256 "e3261768c03d5cbc43cc9f6fc05d4c155fb357db0eac09cba4cd91e048085e2c"
+  homepage "https://elpa.gnu.org/packages/cl-generic.html"
+  url "https://elpa.gnu.org/packages/cl-generic-0.3.el"
+  sha256 "f893845a425e414d5a5769670b958b36cb5c3ef1b63f0c8ccb565f09171a636d"
   head "http://git.savannah.gnu.org/cgit/emacs/elpa.git/plain/packages/cl-generic/cl-generic.el"
 
   depends_on :emacs
@@ -13,13 +13,12 @@ class ClGeneric < EmacsFormula
     mv "cl-generic-#{version}.el", "cl-generic.el"
 
     byte_compile "cl-generic.el"
-    (share/"emacs/site-lisp/cl-generic").install "cl-generic.el",
-                                                 "cl-generic.elc"
+    elisp.install "cl-generic.el", "cl-generic.elc"
   end
 
   test do
     (testpath/"test.el").write <<-EOS.undent
-      (add-to-list 'load-path "#{share}/emacs/site-lisp/cl-generic")
+      (add-to-list 'load-path "#{elisp}")
       (load "cl-generic")
       (print (minibuffer-prompt-width))
     EOS
