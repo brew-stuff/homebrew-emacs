4bcf115bbc0215cd76288c49dc646f9c566b33be
Alex Dunn <dunn.alex@gmail.com>
2016-07-22 06:54:13 -0700

    fountain-mode 2.1.3
    Closes #360.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/fountain-mode.rb
+++ Formula/fountain-mode.rb
@@ -3,12 +3,11 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class FountainMode < EmacsFormula
   desc "Major mode for the Fountain screenwriting syntax"
   homepage "https://github.com/rnkn/fountain-mode"
-  url "https://github.com/rnkn/fountain-mode/archive/v2.1.2.tar.gz"
-  sha256 "b75b6fd33457d4445130638377ae4718c83ff6f9d76642f6638a6b4065e84b9f"
+  url "https://github.com/rnkn/fountain-mode/archive/v2.1.3.tar.gz"
+  sha256 "99aec3adb0c0d6b56feebd958f2b9e6fa2e3bb54311042b9ed1c0323029af111"
   head "https://github.com/rnkn/fountain-mode.git"
 
   depends_on :emacs => "24.4"
-  depends_on "homebrew/emacs/s-emacs"
 
   def install
     byte_compile "fountain-mode.el"
@@ -18,7 +17,6 @@ class FountainMode < EmacsFormula
   test do
     (testpath/"test.el").write <<-EOS.undent
       (add-to-list 'load-path "#{elisp}")
-      (add-to-list 'load-path "#{Formula["homebrew/emacs/s-emacs"].opt_elisp}")
       (load "fountain-mode")
       (print fountain-version)
     EOS
