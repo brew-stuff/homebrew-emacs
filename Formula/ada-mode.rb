a2df6f6821eeee00b40f0da3a30a5c560ae3f10d
Alex Dunn <dunn.alex@gmail.com>
2016-07-27 19:50:32 -0700

    ada-mode: remove ada-mode-keys.el
    It's not a versioned URL so that SHA is changing.
    
    Closes #372.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/ada-mode.rb
+++ Formula/ada-mode.rb
@@ -16,11 +16,6 @@ class AdaMode < EmacsFormula
     sha256 "e861098b37faf38289b0ada9543bb2f846285080512d86e052a4154d0a800e61"
   end
 
-  resource "mode-keys" do
-    url "http://stephe-leake.org/emacs/ada-mode/ada-mode-keys.el"
-    sha256 "51fcdc58780ed2f63a6ccd956c3a349d34da93e4271986b7c3cc2e42e11dd6bd"
-  end
-
   def install
     # there's `make byte-compile` but it uses package.el
     byte_compile Dir["*.el"]
@@ -29,10 +24,6 @@ class AdaMode < EmacsFormula
     info.install Dir["*.info"]
     if build.with? "reference"
       resource("reference").stage { info.install Dir["*"] }
-    end
-
-    resource("mode-keys").stage do
-      (prefix/"contrib").install "ada-mode-keys.el"
     end
   end
 
