47ebbeca18a5c98fc8dfe6dade46b397b5a27c06
Alex Dunn <dunn.alex@gmail.com>
2016-07-31 14:23:44 -0700

    debbugs 0.10
    Closes #377.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/debbugs.rb
+++ Formula/debbugs.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class Debbugs < EmacsFormula
   desc "Emacs library for accessing Debbugs SOAP servers"
   homepage "https://elpa.gnu.org/packages/debbugs.html"
-  url "https://elpa.gnu.org/packages/debbugs-0.9.7.tar"
-  sha256 "189f14f7c60c925c57fbae34ee9b0020a0e34e0ae08e2d601c02564a5c1c871f"
+  url "https://elpa.gnu.org/packages/debbugs-0.10.tar"
+  sha256 "36de2919e59327c34dcc410cbfe2c69d8013b8a21fa78c03c0fa4603c1513a6f"
 
   depends_on :emacs => "22.1"
   depends_on "homebrew/emacs/soap-client-emacs" if Emacs.version < Version.create("24.1")
