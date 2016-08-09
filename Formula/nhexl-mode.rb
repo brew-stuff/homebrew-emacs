da1efb0a76878c725088b63d861ad9cee14a901c
Alex Dunn <dunn.alex@gmail.com>
2016-08-08 18:09:30 -0700

    nhexl-mode 0.2

--- Formula/nhexl-mode.rb
+++ Formula/nhexl-mode.rb
@@ -2,29 +2,21 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 
 class NhexlMode < EmacsFormula
   desc "Emacs minor mode for editing hex dumps"
-  homepage "http://elpa.gnu.org/packages/nhexl-mode.html"
-  url "http://elpa.gnu.org/packages/nhexl-mode-0.1.el"
-  sha256 "3271fe2fbf215c599f44781d15c9997363d65c5264d7dc3be12ac98c5aa19340"
+  homepage "https://elpa.gnu.org/packages/nhexl-mode.html"
+  url "https://elpa.gnu.org/packages/nhexl-mode-0.2.el"
+  sha256 "a097eebd37ec42776595f54e54987829f3aef653457a33c25822f2d6fbbc3f63"
 
   depends_on :emacs
 
   def install
     mv "nhexl-mode-#{version}.el", "nhexl-mode.el"
     byte_compile "nhexl-mode.el"
-    (share/"emacs/site-lisp/nhexl-mode").install "nhexl-mode.el",
-                                                 "nhexl-mode.elc"
-  end
-
-  def caveats; <<-EOS.undent
-    Add the following to your init file:
-
-    (require 'nhexl-mode)
-  EOS
+    elisp.install "nhexl-mode.el", "nhexl-mode.elc"
   end
 
   test do
     (testpath/"test.el").write <<-EOS.undent
-      (add-to-list 'load-path "#{share}/emacs/site-lisp/nhexl-mode")
+      (add-to-list 'load-path "#{elisp}")
       (load "nhexl-mode")
       (nhexl-mode)
       (print (minibuffer-prompt-width))
