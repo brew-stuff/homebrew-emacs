1be4bb8af9014dbb6eea8163dfb23f47f292e087
ilovezfs <ilovezfs@icloud.com>
2016-08-20 22:30:17 -0700

    helm 2.0

--- Formula/helm.rb
+++ Formula/helm.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class Helm < EmacsFormula
   desc "Emacs completion and selection narrowing framework"
   homepage "https://emacs-helm.github.io/helm/"
-  url "https://github.com/emacs-helm/helm/archive/v1.9.9.tar.gz"
-  sha256 "79f79079fb42a0c775ba2015df333ca4dcf898768b25513c1b146a3566484205"
+  url "https://github.com/emacs-helm/helm/archive/v2.0.tar.gz"
+  sha256 "cb0b9954ed08222dd83170093c796d05f79e05a481a59ce4921ce6998760a499"
   head "https://github.com/emacs-helm/helm.git"
 
   depends_on :emacs => "24.3"
