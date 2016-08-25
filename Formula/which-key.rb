5079318cc3dcd616bbfc837b10a13b1193afa31f
ilovezfs <ilovezfs@icloud.com>
2016-08-25 09:08:31 -0700

    which-key 1.1.15

--- Formula/which-key.rb
+++ Formula/which-key.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class WhichKey < EmacsFormula
   desc "Emacs package for displaying available keybindings"
   homepage "https://github.com/justbur/emacs-which-key"
-  url "https://github.com/justbur/emacs-which-key/archive/v1.1.14.tar.gz"
-  sha256 "fa7bbd26f80e3b9c5999010ba6866a2f28e589d0de30678bf1ae84418f2110f9"
+  url "https://github.com/justbur/emacs-which-key/archive/v1.1.15.tar.gz"
+  sha256 "51def459380ca7d46f19b2ad421ed2d8b5de6cc277bd22c72e09baecb27c1b9c"
   head "https://github.com/justbur/emacs-which-key.git"
 
   depends_on :emacs => "24.3"
