c788edd802a1750605fb0d5bb766f8f5da85a1d3
Alex Dunn <dunn.alex@gmail.com>
2016-08-24 17:41:32 -0700

    htmlize: switch to temporary mirror
    See https://github.com/melpa/melpa/issues/4125
    
    Closes #404.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/htmlize.rb
+++ Formula/htmlize.rb
@@ -3,23 +3,22 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class Htmlize < EmacsFormula
   desc "Emacs package for converting text to HTML"
   homepage "http://www.us.xemacs.org/People/hrvoje.niksic/"
-  url "http://fly.srk.fer.hr/~hniksic/emacs/htmlize.git",
+  url "https://github.com/dunn/htmlize-mirror.git",
       :tag => "release/1.47",
       :revision => "aa6e2f6dba6fdfa200c7c55efe29ff63380eac8f"
 
-  head "http://fly.srk.fer.hr/~hniksic/emacs/htmlize.git"
+  head "https://github.com/dunn/htmlize-mirror.git"
 
   depends_on :emacs => "22.1"
 
   def install
     byte_compile "htmlize.el"
-    (share/"emacs/site-lisp/htmlize").install "htmlize.el", "htmlize.elc"
-    doc.install "NEWS"
+    elisp.install "htmlize.el", "htmlize.elc"
   end
 
   test do
     (testpath/"test.el").write <<-EOS.undent
-      (add-to-list 'load-path "#{share}/emacs/site-lisp/htmlize")
+      (add-to-list 'load-path "#{elisp}")
       (load "htmlize")
       (print htmlize-version)
     EOS
