835fe603919997f2775de1df54b272f95d213bb7
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-10 02:15:12 +0100

    csharp-mode: add 0.9.0 bottle.

--- Formula/csharp-mode.rb
+++ Formula/csharp-mode.rb
@@ -7,6 +7,13 @@ class CsharpMode < EmacsFormula
   sha256 "4232fe0fc4528be543ca1e338ce3e839814f2a5b43e480c16b7b4f1ea1bca3c5"
   head "https://github.com/josteink/csharp-mode"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "6bb3152b225fc120087933417d5779524832b40e122bbab026154d04e0d977d2" => :el_capitan
+    sha256 "42d63e7cf2281186389dfa824888eec5ad2f8c9cb0989b9a6e9acf02b24f4293" => :yosemite
+    sha256 "42d63e7cf2281186389dfa824888eec5ad2f8c9cb0989b9a6e9acf02b24f4293" => :mavericks
+  end
+
   depends_on :emacs => "22.1"
 
   def install
