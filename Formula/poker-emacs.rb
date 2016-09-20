f095b136ea90891774ef11bf770a1b373963576d
Alex Dunn <dunn.alex@gmail.com>
2016-09-19 21:12:31 -0700

    poker-emacs: remove hash rockets

--- Formula/poker-emacs.rb
+++ Formula/poker-emacs.rb
@@ -6,7 +6,7 @@ class PokerEmacs < EmacsFormula
   url "https://elpa.gnu.org/packages/poker-0.2.el"
   sha256 "00b8a4953460c753e853dc093cf215ff836140de2fc41d3de4fbdb5de3d5336a"
 
-  depends_on :emacs => "24.1"
+  depends_on emacs: "24.1"
   depends_on "homebrew/emacs/cl-lib" if Emacs.version < Version.create("24.3")
 
   def install
