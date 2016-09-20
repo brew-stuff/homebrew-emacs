c35c3bbf9bf8db099671cd8441cc89a1c3b35a1c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-20 03:46:23 +0100

    queue-emacs: add 0.1.1 bottle.

--- Formula/queue-emacs.rb
+++ Formula/queue-emacs.rb
@@ -6,6 +6,13 @@ class QueueEmacs < EmacsFormula
   url "https://elpa.gnu.org/packages/queue-0.1.1.el"
   sha256 "210daf744931abd007e2ad0caae0990ecfa09b6509402b9c6238398bbb23824b"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b642d609277da3a3c6c55bab87ea97d2e31de8ba553cb968f4816748fd7ef6b6" => :sierra
+    sha256 "b642d609277da3a3c6c55bab87ea97d2e31de8ba553cb968f4816748fd7ef6b6" => :el_capitan
+    sha256 "b642d609277da3a3c6c55bab87ea97d2e31de8ba553cb968f4816748fd7ef6b6" => :yosemite
+  end
+
   depends_on :emacs
 
   def install
