472a7b29af69f819c49ccc19068b52ce1f076be0
Alex Dunn <dunn.alex@gmail.com>
2016-09-19 19:26:11 -0700

    ada-mode: use new homepage

--- Formula/ada-mode.rb
+++ Formula/ada-mode.rb
@@ -2,13 +2,13 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 
 class AdaMode < EmacsFormula
   desc "Emacs major mode for editing Ada sources"
-  homepage "http://stephe-leake.org/emacs/ada-mode/emacs-ada-mode.html"
+  homepage "http://www.nongnu.org/ada-mode/"
   url "https://elpa.gnu.org/packages/ada-mode-5.2.0.tar"
   sha256 "19dfd93c3c466cf4a246fae3c0123051490f6640bd3cefa791e54f5f17498ec8"
 
   option "without-reference", "Build without the reference manual"
 
-  depends_on :emacs => "24.2"
+  depends_on emacs: "24.2"
   depends_on "homebrew/emacs/wisi-emacs"
 
   resource "reference" do
