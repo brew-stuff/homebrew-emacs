d6fd0ee1a8cdfed488f67d0ec597107b2b73ed8b
Alex Dunn <dunn.alex@gmail.com>
2016-09-19 21:41:21 -0700

    trie-emacs: update homepage, use secure url

--- Formula/trie-emacs.rb
+++ Formula/trie-emacs.rb
@@ -2,27 +2,27 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 
 class TrieEmacs < EmacsFormula
   desc "Emacs implementation of the trie data structure"
-  homepage "http://www.dr-qubit.org/emacs.php"
-  url "http://elpa.gnu.org/packages/trie-0.2.6.el"
+  homepage "http://www.dr-qubit.org/Emacs_data_structure_packages.html"
+  url "https://elpa.gnu.org/packages/trie-0.2.6.el"
   sha256 "3112d165951b4bb84e64214373ae7f19eebf8948b0ee8b6158839582610b71e0"
 
   # for the avl-tree package
-  depends_on :emacs => "24.1"
+  depends_on emacs: "24.1"
   depends_on "homebrew/emacs/heap-emacs"
   depends_on "homebrew/emacs/tnfa-emacs"
 
   def install
     mv "trie-#{version}.el", "trie.el"
     byte_compile "trie.el"
-    (share/"emacs/site-lisp/trie").install "trie.el", "trie.elc"
+    elisp.install "trie.el", "trie.elc"
   end
 
   test do
     (testpath/"test.el").write <<-EOS.undent
-      (add-to-list 'load-path "#{share}/emacs/site-lisp/trie")
-      (add-to-list 'load-path "#{Formula["homebrew/emacs/heap-emacs"].share}/emacs/site-lisp/heap")
-      (add-to-list 'load-path "#{Formula["homebrew/emacs/tnfa-emacs"].share}/emacs/site-lisp/tNFA")
-      (add-to-list 'load-path "#{Formula["homebrew/emacs/queue-emacs"].share}/emacs/site-lisp/queue")
+      (add-to-list 'load-path "#{elisp}")
+      (add-to-list 'load-path "#{Formula["homebrew/emacs/heap-emacs"].opt_elisp}")
+      (add-to-list 'load-path "#{Formula["homebrew/emacs/tnfa-emacs"].opt_elisp}")
+      (add-to-list 'load-path "#{Formula["homebrew/emacs/queue-emacs"].opt_elisp}")
       (load "trie")
       (print (minibuffer-prompt-width))
     EOS
