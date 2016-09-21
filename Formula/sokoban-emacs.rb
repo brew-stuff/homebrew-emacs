07bc146767cb844f4a8e64a069b0972aef00dbee
Alex Dunn <dunn.alex@gmail.com>
2016-09-20 18:07:04 -0700

    sokoban-emacs: use secure url, clean up

--- Formula/sokoban-emacs.rb
+++ Formula/sokoban-emacs.rb
@@ -2,29 +2,20 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 
 class SokobanEmacs < EmacsFormula
   desc "Emacs version of Sokoban"
-  homepage "http://elpa.gnu.org/packages/sokoban.html"
-  url "http://elpa.gnu.org/packages/sokoban-1.4.tar"
+  homepage "https://elpa.gnu.org/packages/sokoban.html"
+  url "https://elpa.gnu.org/packages/sokoban-1.4.tar"
   sha256 "8127798c25bb19ae8d9a4d8d9d3e24b43a5a1ec45b0c79f1003a66991157d3f9"
 
-  depends_on :emacs => "19.34"
+  depends_on emacs: "19.34"
 
   def install
     byte_compile Dir["*.el"]
-    (share/"emacs/site-lisp/sokoban").install Dir["*.el"],
-                                              Dir["*.elc"],
-                                              "sokoban.levels"
-  end
-
-  def caveats; <<-EOS.undent
-    Add the following to your init file:
-
-    (require 'sokoban)
-  EOS
+    elisp.install Dir["*.el"], Dir["*.elc"], "sokoban.levels"
   end
 
   test do
     (testpath/"test.el").write <<-EOS.undent
-      (add-to-list 'load-path "#{share}/emacs/site-lisp/sokoban")
+      (add-to-list 'load-path "#{elisp}")
       (load "sokoban")
       (print (minibuffer-prompt-width))
     EOS
