31c0ba1c2d659b216e003767103d63f887ae1092
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-21 02:05:58 +0100

    spinner-emacs: add 1.7.1 bottle.

--- Formula/spinner-emacs.rb
+++ Formula/spinner-emacs.rb
@@ -7,6 +7,13 @@ class SpinnerEmacs < EmacsFormula
   sha256 "0199b591ba1c8897bc9bbe6de1f18c55e8a549bd150dbb90de7e4d0063fbbcba"
   head "https://github.com/Malabarba/spinner.el.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "f6390b22460814d97de5fde438e6d17a12bdeedfeb608db2096ad08be7fb01c9" => :sierra
+    sha256 "f6390b22460814d97de5fde438e6d17a12bdeedfeb608db2096ad08be7fb01c9" => :el_capitan
+    sha256 "f6390b22460814d97de5fde438e6d17a12bdeedfeb608db2096ad08be7fb01c9" => :yosemite
+  end
+
   depends_on :emacs
 
   def install
