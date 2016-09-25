c34628a6ad3f806c2989857415cf11d35ae5bf96
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 02:55:11 +0100

    debbugs: add 0.11 bottle.

--- Formula/debbugs.rb
+++ Formula/debbugs.rb
@@ -6,6 +6,13 @@ class Debbugs < EmacsFormula
   url "https://elpa.gnu.org/packages/debbugs-0.11.tar"
   sha256 "d670f48dc5138301d5f02f77d881144cd7d761fc89390c8d34edbbedd5d16983"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "593672178e5cacb64a5f002dd21ce4a7cf7e86fa127c03757e36d303f065e8cc" => :sierra
+    sha256 "593672178e5cacb64a5f002dd21ce4a7cf7e86fa127c03757e36d303f065e8cc" => :el_capitan
+    sha256 "593672178e5cacb64a5f002dd21ce4a7cf7e86fa127c03757e36d303f065e8cc" => :yosemite
+  end
+
   depends_on :emacs => "22.1"
   depends_on "homebrew/emacs/soap-client-emacs" if Emacs.version < Version.create("24.1")
 
