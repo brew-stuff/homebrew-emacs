70fb9f00af58d6d49e2559aee716b165950d95bc
ilovezfs <ilovezfs@icloud.com>
2016-10-01 19:19:57 -0700

    ggtags 0.8.12
    Closes #461.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/ggtags.rb
+++ Formula/ggtags.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class Ggtags < EmacsFormula
   desc "Emacs interface to GNU Global tagging system"
   homepage "https://github.com/leoliu/ggtags"
-  url "https://github.com/leoliu/ggtags/archive/0.8.11.tar.gz"
-  sha256 "5fd28f3f5d63d3325474b93f1a992f9f38bdb1279c3a2d3ba67b034d1728ecf4"
+  url "https://github.com/leoliu/ggtags/archive/0.8.12.tar.gz"
+  sha256 "428638328fed1884bfb5b5ea1c57b141ac78415f4f40aa98bef3faff58388d26"
   head "https://github.com/leoliu/ggtags.git"
 
   depends_on :emacs => "24.1"
