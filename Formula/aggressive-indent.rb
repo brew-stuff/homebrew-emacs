f0db60c5010de1359eb4a09a6ed250a299e43b6e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-13 10:56:06 +0100

    aggressive-indent: add 1.8.3 bottle.

--- Formula/aggressive-indent.rb
+++ Formula/aggressive-indent.rb
@@ -7,6 +7,13 @@ class AggressiveIndent < EmacsFormula
   sha256 "d272e4b93ef20cb21fb1ba1a3c88348af39bd2b5b1f397ef551c74502863df4a"
   head "https://github.com/Malabarba/aggressive-indent-mode.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ba490be18e32f1529179972b5f03bd729fd527df1c780c4681eaee61e64edd09" => :sierra
+    sha256 "ba490be18e32f1529179972b5f03bd729fd527df1c780c4681eaee61e64edd09" => :el_capitan
+    sha256 "ba490be18e32f1529179972b5f03bd729fd527df1c780c4681eaee61e64edd09" => :yosemite
+  end
+
   depends_on :emacs => "24.1"
   depends_on "homebrew/emacs/cl-lib" if Emacs.version < Version.create("24.3")
 
