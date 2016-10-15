aa68eb631b207f866b4e6a8fd46c907662b01ef6
Bob W. Hogg <rwhogg@linux.com>
2016-10-14 21:09:01 -0400

    less-css-mode 0.21 (new formula)

---
+++ Formula/less-css-mode.rb
@@ -0,0 +1,25 @@
+require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
+
+class LessCssMode < EmacsFormula
+  desc "Major mode for editing .less files in Emacs"
+  homepage "https://github.com/purcell/less-css-mode"
+  url "https://github.com/purcell/less-css-mode/archive/0.21.tar.gz"
+  sha256 "c0fcf73526b66e2eb1f0bfa6c7ca0f1aab9e3f7ba2b3c08252fab7ccf2da2b71"
+  head "https://github.com/purcell/less-css-mode.git"
+
+  depends_on :emacs => "22.2"
+
+  def install
+    byte_compile "less-css-mode.el"
+    elisp.install "less-css-mode.el", "less-css-mode.elc"
+  end
+
+  test do
+    (testpath/"test.el").write <<-EOS.undent
+      (add-to-list 'load-path "#{elisp}")
+      (load "less-css-mode")
+      (print (minibuffer-prompt-width))
+    EOS
+    assert_equal "0", shell_output("emacs -Q --batch -l #{testpath}/test.el").strip
+  end
+end
