500d323bbeab88955c21f4dacdb425da8f07e675
Alex Dunn <dunn.alex@gmail.com>
2016-10-28 18:22:40 -0700

    seq 2.19
    Closes #484.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/seq.rb
+++ Formula/seq.rb
@@ -3,13 +3,19 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class Seq < EmacsFormula
   desc "Sequence manipulation library for Emacs"
   homepage "https://elpa.gnu.org/packages/seq.html"
-  url "https://elpa.gnu.org/packages/seq-2.16.tar"
-  sha256 "6ac63594fa2d53e524b007283cd183100192fb9d8bbcfde17c3dd3b1966481b9"
-  head "https://github.com/NicolasPetton/seq.el.git"
+  url "https://elpa.gnu.org/packages/seq-2.19.tar"
+  sha256 "0a7e93f52bb7afd28d36c1362f7da9e301694e813f3b2699a6011265743c0b86"
 
-  depends_on :emacs
+  depends_on :emacs => "24.1"
+
+  # Submitted upstream via email on 2016-10-28
+  patch :p3 do
+    url "https://github.com/dunn/elpa/commit/195320c704cbc721f945d59fa54cd20d8e024a57.diff"
+    sha256 "f4967cac92cfe462eca073c27db1422299acae5d0a99cacac8c14555ab616142"
+  end
 
   def install
+    ert_run_tests "tests/seq-tests.el"
     byte_compile Dir["*.el"]
     elisp.install Dir["*.el"], Dir["*.elc"]
   end
