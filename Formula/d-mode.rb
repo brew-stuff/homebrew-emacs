1ff02fc2eb17151ebcfd7d013bb6b3f4df3099e6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-11 15:13:52 +0000

    d-mode: add 2.0.9 bottle.

--- Formula/d-mode.rb
+++ Formula/d-mode.rb
@@ -7,6 +7,13 @@ class DMode < EmacsFormula
   sha256 "6d8e2f3f201a829fcad82353a85a22c627d78c007da5311ee1e78e1ecf51ea88"
   head "https://github.com/Emacs-D-Mode-Maintainers/Emacs-D-Mode.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "509b93a1da0fd6802e67b8a8b518e647668b380112185f6fc9baa78f9722b439" => :sierra
+    sha256 "509b93a1da0fd6802e67b8a8b518e647668b380112185f6fc9baa78f9722b439" => :el_capitan
+    sha256 "509b93a1da0fd6802e67b8a8b518e647668b380112185f6fc9baa78f9722b439" => :yosemite
+  end
+
   depends_on :emacs => "23"
   depends_on "cask"
 
