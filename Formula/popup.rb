af34caa530dd4464a2bb8dccf46eb667a832af65
Alex Dunn <dunn.alex@gmail.com>
2016-11-11 18:23:10 -0800

    popup: don't use cask for tests
    Fixes #502.

--- Formula/popup.rb
+++ Formula/popup.rb
@@ -7,22 +7,18 @@ class Popup < EmacsFormula
   sha256 "8035782f642f346ba768fa408e4120a9368b0c86f43e3ecd62b527e391742ffb"
   head "https://github.com/auto-complete/popup-el.git"
 
-  # only tested on 24
   depends_on :emacs => "24.1"
-  depends_on "cask"
   depends_on "homebrew/emacs/cl-lib" if Emacs.version < Version.create("24.3")
 
   def install
-    system "make", "travis-ci", "CASK=#{Formula["cask"].bin}/cask"
-
+    ert_run_tests "tests/popup-test.el"
     byte_compile "popup.el"
-    (share/"emacs/site-lisp/popup").install "popup.el", "popup.elc"
-    doc.install "README.md"
+    elisp.install "popup.el", "popup.elc"
   end
 
   test do
     (testpath/"test.el").write <<-EOS.undent
-      (add-to-list 'load-path "#{share}/emacs/site-lisp/popup")
+      (add-to-list 'load-path "#{elisp}")
       (load "popup")
       (print popup-version)
     EOS
