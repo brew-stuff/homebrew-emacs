5cfe87e152e4bba3d6c2dd5a13f6413c99b31229
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-12 02:28:29 +0000

    popup: add 0.5.3 bottle.

--- Formula/popup.rb
+++ Formula/popup.rb
@@ -7,6 +7,13 @@ class Popup < EmacsFormula
   sha256 "8035782f642f346ba768fa408e4120a9368b0c86f43e3ecd62b527e391742ffb"
   head "https://github.com/auto-complete/popup-el.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "185a95d7bae875a306141dabb88bbc3f8729c6a3cf0073b816d6b42abfcd6690" => :sierra
+    sha256 "185a95d7bae875a306141dabb88bbc3f8729c6a3cf0073b816d6b42abfcd6690" => :el_capitan
+    sha256 "185a95d7bae875a306141dabb88bbc3f8729c6a3cf0073b816d6b42abfcd6690" => :yosemite
+  end
+
   depends_on :emacs => "24.1"
   depends_on "homebrew/emacs/cl-lib" if Emacs.version < Version.create("24.3")
 
