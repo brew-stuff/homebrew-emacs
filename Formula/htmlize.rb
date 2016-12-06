3b0e8f296da704d534736f05d3ead065373fb296
Alex Dunn <dunn.alex@gmail.com>
2016-12-05 18:37:50 -0800

    htmlize 1.49
    Closes #517.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/htmlize.rb
+++ Formula/htmlize.rb
@@ -3,10 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class Htmlize < EmacsFormula
   desc "Emacs package for converting text to HTML"
   homepage "http://www.us.xemacs.org/People/hrvoje.niksic/"
-  url "https://github.com/dunn/htmlize-mirror.git",
-      :tag => "release/1.47",
-      :revision => "aa6e2f6dba6fdfa200c7c55efe29ff63380eac8f"
-
+  url "https://github.com/hniksic/emacs-htmlize/archive/release/1.49.tar.gz"
+  sha256 "b450fa229bef77741b98b121a84ad27e2e0c530bbf87a589e06f3bab94a89682"
   head "https://github.com/dunn/htmlize-mirror.git"
 
   depends_on :emacs => "22.1"
@@ -17,11 +15,15 @@ class Htmlize < EmacsFormula
   end
 
   test do
+    (testpath/"test.txt").write "hello"
+
     (testpath/"test.el").write <<-EOS.undent
       (add-to-list 'load-path "#{elisp}")
       (load "htmlize")
-      (print htmlize-version)
+      (htmlize-file "#{testpath}/test.txt" "#{testpath}/out.html")
     EOS
-    assert_match version.to_s, shell_output("emacs -Q --batch -l #{testpath}/test.el").strip
+    system "emacs", "-Q", "--batch", "-l", testpath/"test.el"
+    assert_equal "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01//EN\">",
+                 File.read(testpath/"out.html").split("\n").first
   end
 end
