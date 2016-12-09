c5e6eb7c205a3d81c1f103e5ae41f838b93e0208
Bob W. Hogg <rwhogg@linux.com>
2016-12-08 19:38:37 -0800

    cython-mode 0.25.2

--- Formula/cython-mode.rb
+++ Formula/cython-mode.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class CythonMode < EmacsFormula
   desc "Emacs major mode for Cython"
   homepage "http://cython.org"
-  url "https://github.com/cython/cython/archive/0.25.1.tar.gz"
-  sha256 "1cda0c5b6ebe9a6df19993cabbe747e35e6f540d438e1a750f4fc8a17b3a7d37"
+  url "https://github.com/cython/cython/archive/0.25.2.tar.gz"
+  sha256 "206b6fc7e8cebe8e0529064a92e4f53f13a8f1d1f4dc173c32782ab134a2551e"
   head "https://github.com/cython/cython.git"
 
   depends_on :emacs
