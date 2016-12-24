028814a2c249f9269190e63d73733096fe5dec42
Alex Dunn <dunn.alex@gmail.com>
2016-12-23 17:03:02 -0800

    context-coloring 8.1.0
    Closes #530.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/context-coloring.rb
+++ Formula/context-coloring.rb
@@ -3,18 +3,23 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class ContextColoring < EmacsFormula
   desc "Emacs mode to color JS and Elisp code by scope"
   homepage "https://github.com/jacksonrayhamilton/context-coloring"
-  url "https://github.com/jacksonrayhamilton/context-coloring/archive/v8.0.1.tar.gz"
-  sha256 "06fc76c24bc738b595d2fff1c1fd41e1015a248d6bc9cdee5637b83d27d393b5"
+  url "https://github.com/jacksonrayhamilton/context-coloring/archive/v8.1.0.tar.gz"
+  sha256 "cc19110feca916f9be3cd697ac1e17b7a067f0d707289634033540328d550797"
   head "https://github.com/jacksonrayhamilton/context-coloring.git"
 
   depends_on :emacs => "24.3"
   depends_on "homebrew/emacs/js2-mode"
-  depends_on "homebrew/emacs/undercover" => :build
 
   def install
-    ert_run_tests Dir["test/*.el"]
-    byte_compile Dir["*.el"]
-    elisp.install "context-coloring.el", "context-coloring.elc"
+    ert_run_tests "context-coloring-test.el"
+
+    byte_compile "context-coloring-emacs-lisp.el",
+                 "context-coloring-javascript.el",
+                 "context-coloring.el"
+
+    elisp.install "context-coloring-emacs-lisp.el", "context-coloring-emacs-lisp.elc",
+                  "context-coloring-javascript.el", "context-coloring-javascript.elc",
+                  "context-coloring.el", "context-coloring.elc"
   end
 
   test do
