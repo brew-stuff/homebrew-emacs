49d6a04d914958db6e1ae410390cf9e9de42611d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-06 02:00:06 +0000

    cl-lib: add 0.6 bottle.

--- Formula/cl-lib.rb
+++ Formula/cl-lib.rb
@@ -6,6 +6,13 @@ class ClLib < EmacsFormula
   url "https://elpa.gnu.org/packages/cl-lib-0.6.el"
   sha256 "d34f8b68414f3a48dec41c0728381c3e9254f7179776ddffe90944f7878865cb"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "1d6129d35b62520f5765a3673b744e4414694b8b31858ed7eeb9eff1f00cfde4" => :sierra
+    sha256 "90a7cf3819e82ef479b8f6619b88fddfc1de7f3fb0a4fc7100a6f8f691b3b703" => :el_capitan
+    sha256 "90a7cf3819e82ef479b8f6619b88fddfc1de7f3fb0a4fc7100a6f8f691b3b703" => :yosemite
+  end
+
   depends_on :emacs => "21"
 
   def install
