fe0e3bfe085aa856d2d5a509db19d0fea7d1e765
ilovezfs <ilovezfs@icloud.com>
2017-01-17 06:03:48 -0800

    neotree 0.5.1

--- Formula/neotree.rb
+++ Formula/neotree.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class Neotree < EmacsFormula
   desc "Tree-style file browser for Emacs"
   homepage "https://github.com/jaypei/emacs-neotree"
-  url "https://github.com/jaypei/emacs-neotree/archive/0.5.tar.gz"
-  sha256 "208ba914cc8b2d65ecc69db1ada3066c61d9d5b541529d566e0161cf390bf330"
+  url "https://github.com/jaypei/emacs-neotree/archive/0.5.1.tar.gz"
+  sha256 "1d517543bcc3d7df92a1262410266febee47b9a294d9380183bf3fa735c6fc39"
   head "https://github.com/jaypei/emacs-neotree.git", :branch => "dev"
 
   bottle do
@@ -14,7 +14,7 @@ class Neotree < EmacsFormula
     sha256 "66b99c96b3b183db861e7e9805bd12a541df2c55b12cc50c2ef33f560896307b" => :yosemite
   end
 
-  depends_on :emacs
+  depends_on :emacs => "24.1"
 
   def install
     system "make", "test-batch"
