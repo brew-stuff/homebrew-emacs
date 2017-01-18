9c3db6726d6fc6ce1986541bba704dd473c27f3e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-18 03:13:56 +0000

    xpm-emacs: add 1.0.3_1 bottle.

--- Formula/xpm-emacs.rb
+++ Formula/xpm-emacs.rb
@@ -5,6 +5,13 @@ class XpmEmacs < EmacsFormula
   homepage "http://www.gnuvola.org/software/xpm/"
   url "https://elpa.gnu.org/packages/xpm-1.0.3.tar"
   sha256 "e45281a2a361790fc1a7b17e360ab2c04a6032026d911d79448f7dde475a9361"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9a0c4aa6d9be329992dcdcb3ba6c6fe5c9c02b14614bba9e854ca08305c76693" => :sierra
+    sha256 "0141324ad216a7362a7aa01a2b84966a7853f7dbc74730caf3f1cc15fcfc2bd3" => :el_capitan
+    sha256 "0141324ad216a7362a7aa01a2b84966a7853f7dbc74730caf3f1cc15fcfc2bd3" => :yosemite
+  end
+
   revision 1
 
   depends_on :emacs => "24.1"
