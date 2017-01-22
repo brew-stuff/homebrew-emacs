58ea6eced1575dfe573f1dcc5204905b7ef8a003
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-22 16:52:22 +0000

    git-modes: add 1.2.4 bottle.

--- Formula/git-modes.rb
+++ Formula/git-modes.rb
@@ -7,6 +7,13 @@ class GitModes < EmacsFormula
   sha256 "149d91b52ca222e360a9d22bdb6898243e8a3650580846e9ee927a2b81abb977"
   head "https://github.com/magit/git-modes.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "de1e3c8b8bd03c6eb34df8562bf236f1ca63a5b029cdd12a4ec9e15002a172d6" => :sierra
+    sha256 "35c84d898e53906342f652e9d929ff6f9e01a91455a6ae7e744e648444414efc" => :el_capitan
+    sha256 "35c84d898e53906342f652e9d929ff6f9e01a91455a6ae7e744e648444414efc" => :yosemite
+  end
+
   depends_on :emacs
 
   def install
