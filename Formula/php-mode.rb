fc5b854f43e0b79a5d3c5519c93d3bc59fae8592
Alex Dunn <dunn.alex@gmail.com>
2017-01-29 07:25:14 -0800

    php-mode 1.18.0
    Closes #555.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/php-mode.rb
+++ Formula/php-mode.rb
@@ -3,22 +3,13 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class PhpMode < EmacsFormula
   desc "Major mode for editing PHP files"
   homepage "https://github.com/ejmr/php-mode"
+  url "https://github.com/ejmr/php-mode/archive/v1.18.0.tar.gz"
+  sha256 "3ab66c6d8b84e601700c49b42850145b797fb31a5a49d273a4048d394d519c01"
   head "https://github.com/ejmr/php-mode.git"
-
-  stable do
-    url "https://github.com/ejmr/php-mode/archive/v1.17.0.tar.gz"
-    sha256 "4393e452e7ade8b7479a3b942b985a9495f6a961b443308718756038d21c231d"
-
-    patch do
-      url "https://github.com/ejmr/php-mode/commit/86eab8013c566eebcb77dadf5616b3e99e0eeb1d.diff"
-      sha256 "a0199f08ecd1eea8aa8d1ff76c8088cd95ebc846443c41ade1f3cd2e7dbbfed0"
-    end
-  end
 
   depends_on :emacs => "24.1"
 
   def install
-    ENV["TMPDIR"] = buildpath
     system "make", "test"
     elisp.install Dir["*.el"], Dir["*.elc"]
   end
