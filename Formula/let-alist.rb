cd81546d70d43ba57bf159dd0ad8fb12431d33fc
Alex Dunn <dunn.alex@gmail.com>
2017-02-02 17:39:03 -0800

    let-alist 1.0.5
    Closes #556.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/let-alist.rb
+++ Formula/let-alist.rb
@@ -2,18 +2,30 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 
 class LetAlist < EmacsFormula
   desc "Easily let-bind values of an assoc-list by their names"
-  homepage "http://elpa.gnu.org/packages/let-alist.html"
-  url "http://elpa.gnu.org/packages/let-alist-1.0.4.el"
-  sha256 "1da10a090b40feb682a35c36498ba0d23d1ea8787db64d8ca3067dbff712611c"
-  head "http://git.savannah.gnu.org/cgit/emacs/elpa.git/plain/packages/let-alist/let-alist.el"
+  homepage "https://elpa.gnu.org/packages/let-alist.html"
+  url "https://elpa.gnu.org/packages/let-alist-1.0.5.el"
+  sha256 "fc6b2900e461eb02c0be0365daaa130d3daf43a095ce994e0d8a8212ad4ceb64"
 
-  depends_on :emacs
+  depends_on :emacs => "24.1"
+
+  bottle :disable
 
   def install
-    mv "let-alist-#{version}.el", "let-alist.el" if build.stable?
-
+    mv "let-alist-#{version}.el", "let-alist.el"
     byte_compile "let-alist.el"
-    (share/"emacs/site-lisp/let-alist").install "let-alist.el",
-                                                "let-alist.elc"
+    elisp.install "let-alist.el", "let-alist.elc"
+  end
+
+  test do
+    (testpath/"test.el").write <<-EOS.undent
+      (add-to-list 'load-path "#{elisp}")
+      (load "let-alist")
+      (print
+        (let ((alist '((cat . meow)
+                       (dog . woof))))
+          (let-alist alist
+            .cat)))
+    EOS
+    assert_equal "meow", shell_output("emacs -Q --batch -l #{testpath}/test.el").strip
   end
 end
