389d3713cc92d09a595836c155ebe7b071ed4036
Alex Dunn <dunn.alex@gmail.com>
2017-02-18 22:56:01 +0000

    tern 0.21.0

--- Formula/tern.rb
+++ Formula/tern.rb
@@ -1,10 +1,11 @@
+require "language/node"
 require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 
 class Tern < EmacsFormula
   desc "JavaScript code analyzer"
   homepage "http://ternjs.net/"
-  url "https://github.com/ternjs/tern/archive/0.20.0.tar.gz"
-  sha256 "27d3bf6255b908fed1b995dc33466fba18fdf617b7b2aecbad5f505e33b3a2bc"
+  url "https://github.com/ternjs/tern/archive/0.21.0.tar.gz"
+  sha256 "5a232295630b681a25a20616caa7fcece22ed816793205ea73bd99984483ecdf"
   head "https://github.com/ternjs/tern.git"
 
   depends_on "node"
@@ -13,9 +14,7 @@ class Tern < EmacsFormula
   depends_on "dunn/emacs/cl-lib" if Emacs.version < Version.create("24.3")
 
   def install
-    ENV.prepend_path "PATH", "#{Formula["node"].opt_libexec}/npm/bin"
-    system "npm", "install"
-    system "npm", "test"
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
 
     lisps = %w[emacs/tern.el]
     lisps << "emacs/tern-auto-complete.el" if build.with? "auto-complete"
@@ -34,5 +33,16 @@ class Tern < EmacsFormula
       (print (minibuffer-prompt-width))
     EOS
     assert_equal "0", shell_output("emacs -Q --batch -l #{testpath}/test.el").strip
+
+    pid = fork do
+      exec bin/"tern", "--port", "4444"
+    end
+
+    sleep 2
+
+    system "curl", "localhost:4444"
+
+    Process.kill "TERM", pid
+    Process.wait pid
   end
 end
