4896a841ba82337d761e01dedac7254d84933048
Alex Dunn <dunn.alex@gmail.com>
2017-03-14 17:46:50 -0700

    load-dir 0.0.4

--- Formula/load-dir.rb
+++ Formula/load-dir.rb
@@ -2,34 +2,29 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 
 class LoadDir < EmacsFormula
   desc "Package for loading all Emacs Lisp files in a directory"
-  homepage "http://elpa.gnu.org/packages/load-dir.html"
-  url "http://elpa.gnu.org/packages/load-dir-0.0.3.el"
-  sha256 "62e8f3238ad3623dda424fcef36b4dc01ac074b91515d1403ed59efc0c6bb970"
+  homepage "https://elpa.gnu.org/packages/load-dir.html"
+  url "https://elpa.gnu.org/packages/load-dir-0.0.4.el"
+  sha256 "440549cc97aedb1488cd30d2b09baaaeba10d357d8389292fdaf4affa20fa913"
 
-  depends_on :emacs
+  bottle :disable
+
+  depends_on :emacs => "24.3"
 
   def install
     mv "load-dir-#{version}.el", "load-dir.el"
     byte_compile "load-dir.el"
-    (share/"emacs/site-lisp/load-dir").install "load-dir.el",
-                                               "load-dir.elc"
-  end
-
-  def caveats; <<-EOS.undent
-    Add the following to your init file:
-
-    (require 'load-dir)
-  EOS
+    elisp.install "load-dir.el", "load-dir.elc"
   end
 
   test do
     (testpath/"test.el").write <<-EOS.undent
-      (add-to-list 'load-path "#{share}/emacs/site-lisp/load-dir")
+      (add-to-list 'load-path "#{elisp}")
+      (require 'cl-extra)
       (load "load-dir")
-      (load-dir-one "#{share}/emacs/site-lisp/load-dir")
+      (load-dir-one "#{elisp}")
       (print load-dir-loaded)
     EOS
-    assert_equal "(\"#{share}/emacs/site-lisp/load-dir/load-dir\")",
+    assert_equal "(\"#{elisp}/load-dir\")",
                  shell_output("emacs -Q --batch -l #{testpath}/test.el").strip
   end
 end
