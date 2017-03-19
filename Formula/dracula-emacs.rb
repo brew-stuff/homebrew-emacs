8d7b04b2d47a6591826e47769db49513835d0bc4
Benjamin Reynolds <breyno127@gmail.com>
2017-03-19 18:12:43 -0500

    dracula-emacs 1.2.5 (new formula)
    Closes #581.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

---
+++ Formula/dracula-emacs.rb
@@ -0,0 +1,26 @@
+require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
+
+class DraculaEmacs < EmacsFormula
+  desc "Dark color theme available for a number of editors."
+  homepage "https://github.com/dracula/emacs"
+  url "https://github.com/dracula/emacs/archive/v1.2.5.tar.gz"
+  sha256 "bc78039c90d5d27f6b1ccb7b32fabb3ecbf0d485421bc63ae5d5d6836b9780b4"
+  head "https://github.com/dracula/emacs.git"
+
+  bottle :disable
+
+  depends_on :emacs => "24.1"
+
+  def install
+    elisp.install "dracula-theme.el"
+  end
+
+  test do
+    (testpath/"test.el").write <<-EOS.undent
+      (add-to-list 'custom-theme-load-path "#{elisp}")
+      (load-theme 'dracula t)
+      (print (minibuffer-prompt-width))
+    EOS
+    assert_equal "0", shell_output("emacs -Q --batch -l #{testpath}/test.el").strip
+  end
+end
