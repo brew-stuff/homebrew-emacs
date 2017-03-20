6e9dbc6d0f80c5747d670105510a0122a2502a95
Benjamin Reynolds <breyno127@gmail.com>
2017-03-19 20:33:25 -0500

    Rename formula, change bottle, and remove .elc

--- Formula/dracula-emacs.rb
+++ Formula/dracula-emacs.rb
@@ -1,18 +1,18 @@
 require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 
-class DraculaTheme < EmacsFormula
+class DraculaEmacs < EmacsFormula
   desc "A dark color theme available for a number of editors."
   homepage "https://github.com/dracula/emacs"
   url "https://github.com/dracula/emacs/archive/v1.2.5.tar.gz"
   sha256 "bc78039c90d5d27f6b1ccb7b32fabb3ecbf0d485421bc63ae5d5d6836b9780b4"
   head "https://github.com/dracula/emacs.git"
 
-  bottle :unneed
+  bottle :disable
 
   depends_on :emacs => "24.1"
 
   def install
-    elisp.install "dracula-theme.el", "dracula-theme.elc"
+    elisp.install "dracula-theme.el"
   end
 
   test do
