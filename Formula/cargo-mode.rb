de065be8a2c53528dbdeff9bdbc9025ec11bfefc
Alex Dunn <dunn.alex@gmail.com>
2017-03-31 08:42:58 -0700

    cargo-mode 0.4.0
    Closes #590.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/cargo-mode.rb
+++ Formula/cargo-mode.rb
@@ -3,23 +3,19 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class CargoMode < EmacsFormula
   desc "Emacs Minor Mode for Cargo, Rust's Package Manager."
   homepage "https://github.com/kwrooijen/cargo.el"
-  url "https://github.com/kwrooijen/cargo.el/archive/0.2.0.tar.gz"
-  sha256 "e4e966615db86d51e3f3a7dcfca24d21d4725bac4d9a1a9ef61bddcf6e95100b"
+  url "https://github.com/kwrooijen/cargo.el/archive/0.4.0.tar.gz"
+  sha256 "78bfeb12b224199517e7e154af5e877f83a0220526738798422afebaeb11fc51"
   head "https://github.com/kwrooijen/cargo.el.git"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "cccb8bfb6ff5f3db21088444a7aeb7232f8b41d5d6a1477ebe4bff584943b98b" => :sierra
-    sha256 "cccb8bfb6ff5f3db21088444a7aeb7232f8b41d5d6a1477ebe4bff584943b98b" => :el_capitan
-    sha256 "cccb8bfb6ff5f3db21088444a7aeb7232f8b41d5d6a1477ebe4bff584943b98b" => :yosemite
-  end
+  bottle :disable
 
-  depends_on emacs: "24.3"
+  depends_on :emacs => "24.3"
   depends_on "rust-mode"
 
   def install
     byte_compile "cargo.el", "cargo-process.el"
-    elisp.install "cargo.el", "cargo.elc", "cargo-process.el", "cargo-process.elc"
+    elisp.install "cargo.el", "cargo.elc",
+                  "cargo-process.el", "cargo-process.elc"
   end
 
   test do
