e565b29b53d967e897e9cf522db1fb3ff9aec469
Alex Dunn <dunn.alex@gmail.com>
2017-03-30 21:13:36 -0700

    deferred 0.5.0

--- Formula/deferred.rb
+++ Formula/deferred.rb
@@ -3,14 +3,22 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class Deferred < EmacsFormula
   desc "Simple asynchronous functions for Emacs Lisp "
   homepage "https://github.com/kiwanami/emacs-deferred"
-  url "https://github.com/kiwanami/emacs-deferred/archive/v0.4.0.tar.gz"
-  sha256 "1f3d615eff3b5199176ff7d84bc1908d48a936125c43c2deac5c073051a3aca5"
   head "https://github.com/kiwanami/emacs-deferred.git"
 
-  depends_on :emacs => "23.1"
+  stable do
+    url "https://github.com/kiwanami/emacs-deferred/archive/v0.5.0.tar.gz"
+    sha256 "6e587cc5e4505fa42ab1f0e9d8fc8d0c68e5feed257eaec1272cac6b94ac24c8"
+
+    patch do
+      url "https://github.com/kiwanami/emacs-deferred/commit/7bfe8428c92377140a0fff6d1bb310fa027c8672.diff"
+      sha256 "1fbbd48c5b4e268a5172c3b07e3f6cb6e2eb63df572c6d3f04cf5de122d42c76"
+    end
+  end
+
+  depends_on :emacs => "24.3"
 
   def install
-    byte_compile "deferred.el", "concurrent.el"
+    system "make", "compile"
     elisp.install "deferred.el", "deferred.elc",
                   "concurrent.el", "concurrent.elc"
   end
