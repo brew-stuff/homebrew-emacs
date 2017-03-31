b5243ab4307d9e65643a38ae761b590cd8f7e02b
Alex Dunn <dunn.alex@gmail.com>
2017-03-31 08:35:24 -0700

    circe 2.4

--- Formula/circe.rb
+++ Formula/circe.rb
@@ -3,14 +3,18 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class Circe < EmacsFormula
   desc "Emacs IRC client"
   homepage "https://github.com/jorgenschaefer/circe"
-  url "https://github.com/jorgenschaefer/circe/archive/v2.3.tar.gz"
-  sha256 "7f751c3c988f33c49027eb0bf673512499b1266150d1353bf1e5124746f8147f"
+  url "https://github.com/jorgenschaefer/circe/archive/v2.4.tar.gz"
+  sha256 "36e5d4a22ba8fce24da222eb7ea4100999f35a0d7a47b3e609942b1e874b9fd1"
   head "https://github.com/jorgenschaefer/circe.git"
 
   depends_on :emacs
   depends_on "dunn/emacs/cl-lib" if Emacs.version < Version.create("24.3")
 
+  depends_on "dunn/emacs/buttercup" => :build
+
   def install
+    system Formula["dunn/emacs/buttercup"].bin/"buttercup", "-L", "."
+
     byte_compile Dir["*.el"]
     elisp.install Dir["*.el"], Dir["*.elc"]
   end
