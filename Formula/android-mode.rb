b8212ac979880c20021ca31140cf746b3fd283b9
Alex Dunn <dunn.alex@gmail.com>
2017-03-30 20:41:13 -0700

    android-mode 0.5.0
    Closes #587.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/android-mode.rb
+++ Formula/android-mode.rb
@@ -3,11 +3,14 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class AndroidMode < EmacsFormula
   desc "Emacs minor mode for Android application development"
   homepage "https://github.com/remvee/android-mode"
-  url "https://github.com/remvee/android-mode/archive/0.4.0.tar.gz"
-  sha256 "8b88d08198e5b0e0a7bd9fb0ff3d04213a84f5f9573c20a5d8a4797542b23e97"
+  url "https://github.com/remvee/android-mode/archive/0.5.0.tar.gz"
+  sha256 "39d0acfe3cd23d3234012f213548411ce922f68f1e5edf7a66b8e682bf5edf45"
   head "https://github.com/remvee/android-mode.git"
 
+  bottle :disable
+
   depends_on :emacs => "22.1"
+  depends_on "dunn/emacs/cl-lib" if Emacs.version < Version.create("24.3")
 
   def install
     byte_compile "android-mode.el"
@@ -17,6 +20,7 @@ class AndroidMode < EmacsFormula
   test do
     (testpath/"test.el").write <<-EOS.undent
       (add-to-list 'load-path "#{elisp}")
+      (add-to-list 'load-path "#{Formula["dunn/emacs/cl-lib"].opt_elisp}")
       (load "android-mode")
       (print (minibuffer-prompt-width))
     EOS
