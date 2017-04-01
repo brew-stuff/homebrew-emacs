f2ee690377cea308b38c3ded4d4e14bd16637812
Alex Dunn <dunn.alex@gmail.com>
2017-04-01 07:24:07 -0700

    go-mode 1.5.0

--- Formula/go-mode.rb
+++ Formula/go-mode.rb
@@ -3,11 +3,11 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class GoMode < EmacsFormula
   desc "Emacs major mode for editing Go"
   homepage "https://github.com/dominikh/go-mode.el"
-  url "https://github.com/dominikh/go-mode.el/archive/v1.4.0.tar.gz"
-  sha256 "31365aa8b220ca67e6fb24fa197e99121a6db815690df731df2f56915805935d"
+  url "https://github.com/dominikh/go-mode.el/archive/v1.5.0.tar.gz"
+  sha256 "d54a4afac1658082da1f26bab486c346262e5b185e6463cfe316a3e5e57ab6a9"
   head "https://github.com/dominikh/go-mode.el"
 
-  depends_on :emacs
+  depends_on :emacs => "24.1"
   depends_on "dunn/emacs/cl-lib" if Emacs.version < Version.create("24.3")
 
   def install
