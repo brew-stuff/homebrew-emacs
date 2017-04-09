a2d43b84aac8ed54f69d21b864b556ae0d57c149
Alex Dunn <dunn.alex@gmail.com>
2017-04-08 17:15:36 -0700

    swiper 0.9.0

--- Formula/swiper.rb
+++ Formula/swiper.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class Swiper < EmacsFormula
   desc "Emacs isearch with an overview"
   homepage "https://github.com/abo-abo/swiper"
-  url "https://github.com/abo-abo/swiper/archive/0.8.0.tar.gz"
-  sha256 "582414b7e257019696e2b93bbdf6556383751e85ee6ecbf56fe1e85800e5d8a2"
+  url "https://github.com/abo-abo/swiper/archive/0.9.0.tar.gz"
+  sha256 "963b9be7494981266793f202fdbe246e040ad4a5c647e3c9d77ac47518a2c9c6"
   head "https://github.com/abo-abo/swiper.git"
 
   depends_on :emacs => "24.3"
