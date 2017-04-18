e2e38589f1a3c5a69c90815a3c46a44c3ccb09d0
Alex Dunn <dunn.alex@gmail.com>
2017-04-18 06:50:53 -0700

    async-emacs 1.9.1
    Closes #601.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/async-emacs.rb
+++ Formula/async-emacs.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class AsyncEmacs < EmacsFormula
   desc "Emacs library for asynchronous processing"
   homepage "https://github.com/jwiegley/emacs-async"
-  url "https://github.com/jwiegley/emacs-async/archive/v1.9.tar.gz"
-  sha256 "1abf379c971d8b9d60969aa53f743db97e2bf6587cc13a406c632ed3ccdc4f5b"
+  url "https://github.com/jwiegley/emacs-async/archive/v1.9.1.tar.gz"
+  sha256 "45f78bf7356ee80c6e1ac9971f3768d57acca602dabbbc20c1a61b90ec4a0b2a"
   head "https://github.com/jwiegley/emacs-async.git"
 
   depends_on :emacs
