eb7b7d1df25add4acc8308673073abd542877a07
Alex Dunn <dunn.alex@gmail.com>
2017-05-03 07:12:29 -0700

    async-emacs 1.9.2

--- Formula/async-emacs.rb
+++ Formula/async-emacs.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class AsyncEmacs < EmacsFormula
   desc "Emacs library for asynchronous processing"
   homepage "https://github.com/jwiegley/emacs-async"
-  url "https://github.com/jwiegley/emacs-async/archive/v1.9.1.tar.gz"
-  sha256 "45f78bf7356ee80c6e1ac9971f3768d57acca602dabbbc20c1a61b90ec4a0b2a"
+  url "https://elpa.gnu.org/packages/async-1.9.2.tar"
+  sha256 "dcb9bc817be1c478a73a039a2a4c129c3cca7ebcd228b3954e82737964ded69d"
   head "https://github.com/jwiegley/emacs-async.git"
 
   depends_on :emacs
