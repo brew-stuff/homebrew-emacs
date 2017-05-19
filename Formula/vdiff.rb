3a094371ab1119b98e5e605bc8ec2aeeaf103231
Alex Dunn <dunn.alex@gmail.com>
2017-05-19 13:35:25 -0700

    vdiff 0.2 (new formula)

---
+++ Formula/vdiff.rb
@@ -0,0 +1,42 @@
+require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
+
+class Vdiff < EmacsFormula
+  desc "Visual diff tool for Emacs, similar to vimdiff"
+  homepage "https://github.com/justbur/emacs-vdiff"
+  url "https://elpa.gnu.org/packages/vdiff-0.2.el"
+  sha256 "7cec49f361a38a51fa05467939e0d004326f93a3f102281dab52c2bd4d252710"
+  head "https://github.com/justbur/emacs-vdiff.git"
+
+  depends_on :emacs => "24.4"
+  depends_on "hydra-emacs"
+  depends_on "magit" => :optional
+
+  resource "vdiff-magit" do
+    url "https://github.com/justbur/emacs-vdiff-magit/archive/v0.3.1.tar.gz"
+    sha256 "c979548115a018dbe8f30b335ca452435ad8b6c220ac53019d5136608bdeb553"
+  end
+
+  def install
+    mv "vdiff-#{version}.el", "vdiff.el"
+
+    if build.with? "magit"
+      resource("vdiff-magit").stage do
+        byte_compile "vdiff-magit.el"
+        elisp.install "vdiff-magit.el", "vdiff-magit.elc"
+      end
+    end
+
+    byte_compile "vdiff.el"
+    elisp.install "vdiff.el", "vdiff.elc"
+  end
+
+  test do
+    (testpath/"test.el").write <<-EOS.undent
+      (add-to-list 'load-path "#{Formula["dunn/emacs/hydra-emacs"].opt_elisp}")
+      (add-to-list 'load-path "#{elisp}")
+      (load "vdiff")
+      (print (minibuffer-prompt-width))
+    EOS
+    assert_equal "0", shell_output("emacs -Q --batch -l #{testpath}/test.el").strip
+  end
+end
