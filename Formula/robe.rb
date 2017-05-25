8ebacf1477f970214feeaede6b29c63b686c9f61
Alex Dunn <dunn.alex@gmail.com>
2017-05-24 22:27:52 -0700

    robe 0.8.0
    Closes #617.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/robe.rb
+++ Formula/robe.rb
@@ -3,23 +3,26 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class Robe < EmacsFormula
   desc "Navigation, documentation lookup and completion for Ruby"
   homepage "https://github.com/dgutov/robe"
-  url "https://github.com/dgutov/robe/archive/0.7.9.tar.gz"
-  sha256 "f6b471dd4af841c3b79c14a0db57ba0a87209d8ca7d9845bbcfc82b20e9a79f4"
+  url "https://github.com/dgutov/robe/archive/0.8.0.tar.gz"
+  sha256 "ec3eb7cfe63b7fd8fca8f9e70c12d976b49bfc0870424e2e2ae74abe35ba8de9"
   head "https://github.com/dgutov/robe.git"
 
-  depends_on :emacs => "24.3"
+  depends_on :emacs => "24.4"
   depends_on :ruby => "1.9"
   depends_on "dunn/emacs/inf-ruby"
 
   def install
-    byte_compile Dir["*.el"]
-    elisp.install Dir["*.el"], Dir["*.elc"], "lib"
+    byte_compile "robe.el", "ac-robe.el", "company-robe.el"
+
+    elisp.install "robe.el", "ac-robe.el", "company-robe.el",
+                  "robe.elc", "ac-robe.elc", "company-robe.elc",
+                  "lib"
   end
 
   test do
     (testpath/"test.el").write <<-EOS.undent
-      (add-to-list 'load-path "#{Formula["inf-ruby"].opt_elisp}")
       (add-to-list 'load-path "#{elisp}")
+      (add-to-list 'load-path "#{Formula["inf-ruby"].opt_elisp}")
       (load "robe")
       (print (minibuffer-prompt-width))
     EOS
