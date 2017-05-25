a82767520ce0432a38f463d6577c69d8d0f6e948
Alex Dunn <dunn.alex@gmail.com>
2017-05-24 21:31:57 -0700

    soap-client-emacs 3.1.2

--- Formula/soap-client-emacs.rb
+++ Formula/soap-client-emacs.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class SoapClientEmacs < EmacsFormula
   desc "Emacs SOAP client"
   homepage "https://github.com/alex-hhh/emacs-soap-client"
-  url "https://elpa.gnu.org/packages/soap-client-3.1.1.tar"
-  sha256 "13b6d53e014d75a0cde728e3228bf546d48199353db6715c564e20f486484247"
+  url "https://github.com/alex-hhh/emacs-soap-client/archive/3.1.2.tar.gz"
+  sha256 "0c69d65a8cdc4aa5fad903c3ab7a752058d4ea5d7f3a8ff37ed92ecf40df95ac"
   head "https://github.com/alex-hhh/emacs-soap-client.git"
 
   depends_on :emacs => "23.1"
@@ -18,6 +18,7 @@ class SoapClientEmacs < EmacsFormula
   test do
     (testpath/"test.el").write <<-EOS.undent
       (add-to-list 'load-path "#{elisp}")
+      (add-to-list 'load-path "#{Formula["dunn/emacs/cl-lib"].opt_elisp}")
       (load "soap-client")
       (print (minibuffer-prompt-width))
     EOS
