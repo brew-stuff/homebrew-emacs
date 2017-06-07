b7db6df0bc8eae06b8a410110136d86f9d71d0c1
Alex Dunn <dunn.alex@gmail.com>
2017-06-06 20:02:41 -0700

    nlinum 1.7
    Closes #632.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/nlinum.rb
+++ Formula/nlinum.rb
@@ -2,29 +2,23 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 
 class Nlinum < EmacsFormula
   desc "Emacs mode for displaying line numbers in the margin"
-  homepage "http://elpa.gnu.org/packages/nlinum.html"
-  url "http://elpa.gnu.org/packages/nlinum-1.6.el"
-  sha256 "9cdd586dfd5078951b9ec1211071da688b5b09a2cd59d191f6cc01bcbae225c3"
+  homepage "https://elpa.gnu.org/packages/nlinum.html"
+  url "https://elpa.gnu.org/packages/nlinum-1.7.el"
+  sha256 "9f86c20724e3bd7bb393104351cd0982ae885e41a9985df9d84d27d1d46a8162"
 
-  depends_on :emacs
+  bottle :disable
+
+  depends_on :emacs => "23.1"
 
   def install
     mv "nlinum-#{version}.el", "nlinum.el"
     byte_compile "nlinum.el"
-    (share/"emacs/site-lisp/nlinum").install "nlinum.el",
-                                             "nlinum.elc"
-  end
-
-  def caveats; <<-EOS.undent
-    Add the following to your init file:
-
-    (require 'nlinum)
-  EOS
+    elisp.install "nlinum.el", "nlinum.elc"
   end
 
   test do
     (testpath/"test.el").write <<-EOS.undent
-      (add-to-list 'load-path "#{share}/emacs/site-lisp/nlinum")
+      (add-to-list 'load-path "#{elisp}")
       (load "nlinum")
       (nlinum-mode)
       (print nlinum--width)
