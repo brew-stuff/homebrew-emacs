c540b3ab5cdb6bdc625721b985ad9cd7b51d8800
Alex Dunn <dunn.alex@gmail.com>
2017-07-11 17:19:47 -0700

    wisi-emacs 1.1.5

--- Formula/wisi-emacs.rb
+++ Formula/wisi-emacs.rb
@@ -1,24 +1,19 @@
 require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 
 class WisiEmacs < EmacsFormula
-  desc "Indentation/navigation in Emacs using LALR parsers"
-  homepage "https://elpa.gnu.org/packages/wisi.html"
-  url "https://elpa.gnu.org/packages/wisi-1.1.4.tar"
-  sha256 "99507170b58aab866d7bbc63547751454227d68b196452f4bf7d5dbdcfc10bd8"
+  desc "Indentation/navigation in Emacs using a LALR parser"
+  homepage "http://www.nongnu.org/ada-mode/wisi/wisi.html"
+  url "https://elpa.gnu.org/packages/wisi-1.1.5.tar"
+  sha256 "448dee9cff401b9ddb471d37afe40118531ef3e534b05dca0070bf04a75f0de0"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "a78cea1fd5b1ae63b35b14c29ea97ea694ece20ce87a34f40a965592dd957004" => :sierra
-    sha256 "a78cea1fd5b1ae63b35b14c29ea97ea694ece20ce87a34f40a965592dd957004" => :el_capitan
-    sha256 "a78cea1fd5b1ae63b35b14c29ea97ea694ece20ce87a34f40a965592dd957004" => :yosemite
-  end
+  bottle :disable
 
   depends_on :emacs => "24.2"
   depends_on "dunn/emacs/cl-lib" if Emacs.version < Version.create("24.3")
 
   def install
-    byte_compile Dir["*.el"]
-    elisp.install Dir["*.el"], Dir["*.elc"]
+    byte_compile (Dir["*.el"] - ["wisi-pkg.el"])
+    elisp.install (Dir["*.el"] - ["wisi-pkg.el"]), Dir["*.elc"]
   end
 
   test do
