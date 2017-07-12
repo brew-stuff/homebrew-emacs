0b75f1ceb07fae640c33f4b314d0f0b88b7128a2
Alex Dunn <dunn.alex@gmail.com>
2017-07-11 17:22:45 -0700

    ada-mode 5.2.2

--- Formula/ada-mode.rb
+++ Formula/ada-mode.rb
@@ -3,30 +3,25 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class AdaMode < EmacsFormula
   desc "Emacs major mode for editing Ada sources"
   homepage "http://www.nongnu.org/ada-mode/"
-  url "https://elpa.gnu.org/packages/ada-mode-5.2.1.tar"
-  sha256 "64f03d662500335b27e60f69635d181843e3cfd16b07949cbb9e6f69ea462c25"
+  url "https://elpa.gnu.org/packages/ada-mode-5.2.2.tar"
+  sha256 "6dbf0ef0b16a9e4fc6020b2fcf4f5a1eb866f73bcb2d0cb5e76ae9b789cc1430"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "d91c0f7770d892d0a94be729da7c71b4c784e5e7214d9bada75f9496885b1077" => :sierra
-    sha256 "d91c0f7770d892d0a94be729da7c71b4c784e5e7214d9bada75f9496885b1077" => :el_capitan
-    sha256 "d91c0f7770d892d0a94be729da7c71b4c784e5e7214d9bada75f9496885b1077" => :yosemite
-  end
+  bottle :disable
 
   option "without-reference", "Build without the reference manual"
 
   depends_on :emacs => "24.2"
-  depends_on "dunn/emacs/wisi-emacs"
+  depends_on "dunn/emacs/wisi"
 
   resource "reference" do
-    url "http://stephe-leake.org/ada/arm_info-2012.2.tar.gz"
-    sha256 "e861098b37faf38289b0ada9543bb2f846285080512d86e052a4154d0a800e61"
+    url "http://stephe-leake.org/ada/arm_info-2012.3.tar.gz"
+    sha256 "e0961268ecf6bb3c852d88d9f6ff4a8518e98c607cd264f70bb9c38389458461"
   end
 
   def install
     # there's `make byte-compile` but it uses package.el
-    byte_compile Dir["*.el"]
-    elisp.install Dir["*.el"], Dir["*.elc"]
+    byte_compile (Dir["*.el"] - %w[ada-mode-pkg.el])
+    elisp.install (Dir["*.el"] - %w[ada-mode-pkg.el]), Dir["*.elc"]
 
     info.install Dir["*.info"]
     if build.with? "reference"
@@ -37,7 +32,7 @@ class AdaMode < EmacsFormula
   test do
     (testpath/"test.el").write <<-EOS.undent
       (add-to-list 'load-path "#{elisp}")
-      (add-to-list 'load-path "#{Formula["wisi-emacs"].opt_elisp}")
+      (add-to-list 'load-path "#{Formula["wisi"].opt_elisp}")
       (load "ada-mode")
       (print (ada-mode-version))
     EOS
