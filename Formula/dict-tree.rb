7f57f7497c943683443fdb7d5720f6baad5b4195
Alex Dunn <dunn.alex@gmail.com>
2017-08-18 06:59:52 -0700

    dict-tree 0.14
    Closes #659.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/dict-tree.rb
+++ Formula/dict-tree.rb
@@ -2,16 +2,11 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 
 class DictTree < EmacsFormula
   desc "Emacs implementation of the dictionary data structure"
-  homepage "http://www.dr-qubit.org/Emacs_data_structure_packages.html"
-  url "https://elpa.gnu.org/packages/dict-tree-0.12.8.el"
-  sha256 "457b6b3a0a103616ba54cfb48381e9a42f6a685a9164477ef88e25acb08b4a22"
+  homepage "http://www.dr-qubit.org/emacs_data-structures.html"
+  url "https://elpa.gnu.org/packages/dict-tree-0.14.el"
+  sha256 "58968bccc400a3fa7e28d6fb16f05202537c16e9abe72d633ef82d10ca9800cc"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "6c55b749791552d0b990f565b5b94ca571cae46356e7be92f3dd41a862f11c0c" => :sierra
-    sha256 "6c55b749791552d0b990f565b5b94ca571cae46356e7be92f3dd41a862f11c0c" => :el_capitan
-    sha256 "6c55b749791552d0b990f565b5b94ca571cae46356e7be92f3dd41a862f11c0c" => :yosemite
-  end
+  bottle :disable
 
   depends_on :emacs
   depends_on "dunn/emacs/heap-emacs"
