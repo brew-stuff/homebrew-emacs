598007eedc9e2b7f763897a21ed03eb2d96e176d
Alex Dunn <dunn.alex@gmail.com>
2017-10-11 20:51:59 -0700

    ahungry-emacs 1.6.0

--- Formula/ahungry-emacs.rb
+++ Formula/ahungry-emacs.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
 class AhungryEmacs < EmacsFormula
   desc "Bright and bold color theme for Emacs"
   homepage "https://github.com/ahungry/color-theme-ahungry"
-  url "https://elpa.gnu.org/packages/ahungry-theme-1.5.0.tar"
-  sha256 "2958fc543eeb2c305cc7ccc12375614d1948253cccfcb4be539c9dc615aebf77"
+  url "https://elpa.gnu.org/packages/ahungry-theme-1.6.0.tar"
+  sha256 "5ff3e769788117c0b88102ead89e0465f7facf48cb2081aaafc7be51ce3b1dac"
   head "https://github.com/ahungry/color-theme-ahungry.git"
 
   depends_on :emacs => "24.1"
