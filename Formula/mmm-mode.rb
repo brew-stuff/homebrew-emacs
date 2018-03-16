d65341beded8c0ed08e3dadc7a9c2e77de124134
Alex Dunn <dunn.alex@gmail.com>
2018-03-16 09:38:18 -0700

    mmm-mode 0.5.5

--- Formula/mmm-mode.rb
+++ Formula/mmm-mode.rb
@@ -1,15 +1,15 @@
-require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
+require File.expand_path("../Homebrew/emacs_formula", __dir__)
 
 class MmmMode < EmacsFormula
   desc "Enable Multiple Major Modes in Emacs buffers"
   homepage "https://github.com/purcell/mmm-mode"
-  url "https://github.com/purcell/mmm-mode/archive/0.5.4.tar.gz"
-  sha256 "db55529ba95f841826bee952e73e55b87d9c191004e7c985a9582eea2cd57c82"
+  url "https://github.com/purcell/mmm-mode/archive/0.5.5.tar.gz"
+  sha256 "0c7fec2371c0b84d461732219e1d86c88e6a76e42e430cd199e46406c7b3b130"
   head "https://github.com/purcell/mmm-mode.git"
 
   bottle :disable
 
-  depends_on EmacsRequirement
+  depends_on EmacsRequirement => "24.3"
   depends_on "autoconf" => :build
   depends_on "automake" => :build
 
