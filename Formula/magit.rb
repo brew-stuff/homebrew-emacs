0af1cbc2c216cde37e969820dc818197e6f75c4c
Alex Dunn <dunn.alex@gmail.com>
2018-03-30 09:16:16 -0700

    magit 2.12.0

--- Formula/magit.rb
+++ Formula/magit.rb
@@ -1,10 +1,10 @@
-require File.expand_path("../../Homebrew/emacs_formula", __FILE__)
+require File.expand_path("../Homebrew/emacs_formula", __dir__)
 
 class Magit < EmacsFormula
   desc "Emacs interface for Git"
   homepage "https://magit.vc/"
-  url "https://github.com/magit/magit/releases/download/2.11.0/magit-2.11.0.tar.gz"
-  sha256 "1f9ae73637257987a7c4ed60e78df0e14f1459c60b3ba4a10c4c265557f1b487"
+  url "https://github.com/magit/magit/releases/download/2.12.0/magit-2.12.0.tar.gz"
+  sha256 "e26949084481a851e7ac580c898f34a51f05c7dc7bed8531afe9b324184c7ccb"
   head "https://github.com/magit/magit.git", :shallow => false
 
   bottle :disable
@@ -22,23 +22,46 @@ class Magit < EmacsFormula
   end
 
   resource "gh-pulls" do
-    url "https://github.com/sigma/magit-gh-pulls/archive/0.5.2.tar.gz"
-    sha256 "95cea18d4d9b8b16c64d726c24343280fa50705b057790d6cee6019ef3471037"
+    url "https://github.com/sigma/magit-gh-pulls/archive/0.5.3.tar.gz"
+    sha256 "23d90e5aca234f856e7c6b622314dde03fa84910bc350da3b7e12b18796a4137"
+  end
+
+  resource "ghub" do
+    url "https://github.com/magit/ghub/archive/v2.0.0.tar.gz"
+    sha256 "6c77664c36258a5af756781c0c8c9cba8f069a001974a8c443422802661f2bff"
+  end
+
+  resource "popup" do
+    url "https://github.com/magit/magit-popup/archive/v2.12.3.tar.gz"
+    sha256 "69e46652e3d96457ed6e73998f40b57fd408051dd5482e7c727c72758cdce6cf"
   end
 
   resource "with-editor" do
-    url "https://github.com/magit/with-editor/archive/v2.6.0.tar.gz"
-    sha256 "8f26a619d745f0a47281fd389c6f947733752472eec26a2b1d5e7f641063ea04"
+    url "https://github.com/magit/with-editor/archive/v2.7.2.tar.gz"
+    sha256 "bb0addd744f443ed2b3494f3316ad6d7ec049460bdcbb0d90d8b01cdba1a11cb"
   end
 
   def install
+    resource("ghub").stage do
+      system "make", "all"
+      elisp.install "ghub.el", "ghub.elc"
+      info.install "ghub.info"
+    end
+
+    resource("popup").stage do
+      system "make", "all",
+             "LOAD_PATH=-L #{Formula["dunn/emacs/async-emacs"].opt_elisp} \
+                        -L #{Formula["dunn/emacs/dash-emacs"].opt_elisp}"
+      elisp.install "magit-popup.el", "magit-popup.elc"
+      info.install "magit-popup.info"
+    end
+
     resource("with-editor").stage do
       system "make", "all",
              "EFLAGS=-L #{Formula["dunn/emacs/async-emacs"].opt_elisp} \
                      -L #{Formula["dunn/emacs/dash-emacs"].opt_elisp}"
       elisp.install "with-editor.el", "with-editor.elc"
       info.install "with-editor.info"
-      doc.install Dir["with-editor.*"]
     end
 
     if build.with? "gh-pulls"
